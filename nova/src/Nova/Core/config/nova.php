877bac46041587d83abbbd637e7274e75239b34d
David VanScott <david.vanscott@gmail.com>
2013-02-18 13:01:36 -0500

    Tweak the nova config file

--- nova/src/Nova/Core/config/nova.php
+++ nova/src/Nova/Core/config/nova.php
@@ -1,5 +1,43 @@
 <?php
 
 return array(
-	'foo' => 'st24',
+	
+	/**
+	 * General application info
+	 */
+	'app_name'				=> 'Nova',
+	'app_version_full'		=> '3.0',
+	'app_version_major'		=> 3,
+	'app_version_minor'		=> 0,
+	'app_version_update'	=> 0,
+	
+	/**
+	 * Wiki info
+	 */
+	'wiki_name'				=> 'Thresher',
+	'wiki_version_full'		=> 'Release 3',
+	'wiki_version_major'	=> 2,
+	'wiki_version_minor'	=> 0,
+	'wiki_version_update'	=> 0,
+	
+	/**
+	 * Forum info
+	 */
+	'forum_name'			=> 'Mako',
+	'forum_version_full'	=> 'Release 1',
+	'forum_version_major'	=> 0,
+	'forum_version_minor'	=> 0,
+	'forum_version_update'	=> 0,
+	
+	/**
+	 * Update version path
+	 */
+	'version_check_path'	=> NOVAPATH.'src/Nova/Setup/assets/update/version.json',
+	//'version_check_path'	=> 'http://www.anodyne-productions.com/updates/version.json',
+	
+	/**
+	 * Should we be using dev data?
+	 */
+	'use_dev_data'			=> true,
+
 );
