221f5779e6c28c29f5aae91cda1a0901bfdb6b7d
David VanScott <david.vanscott@gmail.com>
2013-02-18 13:00:22 -0500

    Add local database settings

---
+++ app/config/local/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'host'      => 'localhost',
+			'database'  => 'nova3_install',
+			'username'  => 'nova',
+			'password'  => '',
+			'prefix'    => 'nova_',
+		),
+
+	),
+
+);
