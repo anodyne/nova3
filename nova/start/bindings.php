1bda30d01ec89233891e83d3e83afb0e9a159b9a
David VanScott <david.vanscott@gmail.com>
2013-02-19 13:33:42 -0500

    Remove the Citadel bindings from the bindings file

--- nova/start/bindings.php
+++ nova/start/bindings.php
@@ -32,35 +32,3 @@ $app->bind('location', function($app)
 {
 	return new Nova\Core\Lib\Location(false, false, false);
 });
-
-/*
-|--------------------------------------------------------------------------
-| Citadel Bindings for Sentry
-|--------------------------------------------------------------------------
-|
-| Because of the unique nature of Nova, we have created our own
-| implementations of the various Sentry components and wrapped them together
-| under the name Citadel. Here, we bind those components to Sentry so they
-| get used from the get-go.
-|
-*/
-
-$app->bind('sentry.hasher', function($app)
-{
-	return new Nova\Citadel\Hashing\CitadelHasher;
-});
-
-$app->bind('sentry.group', function($app)
-{
-	return new Nova\Citadel\Groups\Provider('AccessRoleModel');
-});
-
-$app->bind('sentry.user', function($app)
-{
-	return new Nova\Citadel\Users\Provider($app['sentry.hasher'], 'UserModel');
-});
-
-$app->bind('sentry.throttle', function($app)
-{
-	return new Nova\Citadel\Throttling\Provider($app['sentry.user'], 'UserSuspendModel');
-});
