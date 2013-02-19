50c10a15fc500aa67d7694e82d0e4f5d35422bc4
David VanScott <david.vanscott@gmail.com>
2013-02-19 16:24:32 -0500

    Update what Laravel loads for paths

--- nova/start/global.php
+++ nova/start/global.php
@@ -13,9 +13,9 @@
 
 ClassLoader::addDirectories(array(
 
-	app_path().'/controllers',
-	app_path().'/models',
-	app_path().'/database/seeds',
+	//app_path().'/controllers',
+	//app_path().'/models',
+	//app_path().'/database/seeds',
 
 ));
 
