db430e0543d4e4e726f02ab408e825282a9f8ab3
David VanScott <david.vanscott@gmail.com>
2013-02-23 13:16:45 -0500

    Work on the Sentry integration with the models

--- nova/src/Nova/Core/model/Access/Role.php
+++ nova/src/Nova/Core/model/Access/Role.php
@@ -6,12 +6,17 @@
  * @subpackage	Core
  * @category	Model
  * @author		Anodyne Productions
- * @copyright	2012 Anodyne Productions
+ * @copyright	2013 Anodyne Productions
  */
  
 namespace Nova\Core\Model\Access;
 
-class Role extends \Model {
+use Model;
+use UserModel;
+use AccessTaskModel;
+use Cartalyst\Sentry\Groups\GroupInterface;
+
+class Role extends Model implements GroupInterface {
 
 	const INACTIVE		= 1;
 	const USER			= 2;
@@ -22,50 +27,32 @@ class Role extends \Model {
 	
 	protected $table = 'roles';
 	
-	protected static $_properties = array(
-		'id' => array(
-			'type' => 'int',
-			'constraint' => 11,
-			'auto_increment' => true),
-		'name' => array(
-			'type' => 'string',
-			'constraint' => 255,
-			'null' => true),
-		'desc' => array(
-			'type' => 'text',
-			'null' => true),
-		'inherits' => array(
-			'type' => 'text',
-			'null' => true),
+	protected static $properties = array(
+		'id' => array('type' => 'int', 'constraint' => 11, 'auto_increment' => true),
+		'name' => array('type' => 'string', 'constraint' => 255, 'null' => true),
+		'desc' => array('type' => 'text', 'null' => true),
+		'inherits' => array('type' => 'text', 'null' => true),
 	);
-	
-	public static $_has_many = array(
-		'users' => array(
-			'model_to' => '\\Model_User',
-			'key_to' => 'role_id',
-			'key_from' => 'id',
-			'cascade_save' => false,
-			'cascade_delete' => false,
-		),
-	);
-	
-	public static $_many_many = array(
-		'tasks' => array(
-			'model_to' => '\\Model_Access_Task',
-			'key_to' => 'id',
-			'key_from' => 'id',
-			'key_through_from' => 'role_id',
-			'key_through_to' => 'task_id',
-			'table_through' => 'roles_tasks',
-			'cascade_save' => false,
-			'cascade_delete' => false,
-		),
-	);
+
+	/**
+	 * Has Many: Users
+	 */
+	public function users()
+	{
+		return $this->hasMany('UserModel');
+	}
+
+	/**
+	 * Belongs To Many: Tasks
+	 */
+	public function tasks()
+	{
+		return $this->belongsToMany('AccessTaskModel', 'roles_tasks');
+	}
 
 	/**
 	 * Get all the roles.
 	 *
-	 * @api
 	 * @param	array	Any items to remove from the final array
 	 * @return	array
 	 */
@@ -92,21 +79,76 @@ class Role extends \Model {
 	/**
 	 * Get all the tasks.
 	 *
-	 * @api
-	 * @return	array
+	 * @return	Collection
 	 */
 	public function getTasks()
 	{
-		$groups[] = $this->tasks;
-		
-		$inherited = explode(',', $this->inherits);
-		
-		foreach ($inherited as $i)
+		// Loop through this role's tasks
+		foreach ($this->tasks as $task)
 		{
-			//$groups[] = static::find($i)->getTasks();
-			\Debug::dump(static::find($i));
+			$groups[] = $task;
 		}
-		
-		return $groups;
+
+		// If there are inherited roles, loop through those too
+		if ( ! empty($this->inherits))
+		{
+			$inherited = explode(',', $this->inherits);
+
+			foreach ($inherited as $i)
+			{
+				// Get a new instance of the model
+				$instance = new static;
+
+				// Start a new Query Builder
+				$query = $instance->newQuery();	
+
+				// Put the tasks into the holding array
+				$groups[] = $query->find($i)->getTasks();
+			}
+		}
+
+		return $this->newCollection($groups);
+	}
+
+	/*
+	|--------------------------------------------------------------------------
+	| Sentry Group Interface Methods
+	|--------------------------------------------------------------------------
+	|
+	| Sentry provides an interface of methods that need to be implemented by
+	| the model. In Nova's case, some of these aren't applicable, and in those
+	| situations, we simply throw exceptions. In others, we do things the way
+	| we've chosen to setup our authorization system.
+	|
+	*/
+
+	/**
+	 * Returns the group's ID.
+	 *
+	 * @return	int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Returns the group's name.
+	 *
+	 * @return	string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * Returns permissions for the group included inherited permissions.
+	 *
+	 * @return	array
+	 */
+	public function getPermissions()
+	{
+		return $this->getTasks();
 	}
 }
--- nova/src/Nova/Core/model/User.php
+++ nova/src/Nova/Core/model/User.php
@@ -11,18 +11,26 @@
  
 namespace Nova\Core\Model;
 
+use Str;
 use Model;
 use Status;
+use Sentry;
+use Session;
 use AppModel;
 use LogModel;
+use Redirect;
+use Exception;
 use PostModel;
 use CharacterModel;
 use UserPrefsModel;
 use AccessRoleModel;
+use UserSuspendModel;
 use AppReviewerModel;
 use AnnouncementModel;
+use Cartalyst\Sentry\Users\UserInterface;
+use Cartalyst\Sentry\Groups\GroupInterface;
 
-class User extends Model {
+class User extends Model implements UserInterface {
 
 	protected $table = 'users';
 
@@ -107,6 +115,14 @@ class User extends Model {
 	public function preferences()
 	{
 		return $this->hasMany('UserPrefsModel');
+	}
+
+	/**
+	 * Has Many: Throttles
+	 */
+	public function throttles()
+	{
+		return $this->hasMany('UserSuspendModel');
 	}
 
 	/**
@@ -295,5 +311,448 @@ class User extends Model {
 		$query = $instance->newQuery();
 
 		return $query->where('status', $status)->get();
+	}
+
+	/*
+	|--------------------------------------------------------------------------
+	| Sentry User Interface Methods
+	|--------------------------------------------------------------------------
+	|
+	| Sentry provides an interface of methods that need to be implemented by
+	| the model. In Nova's case, some of these aren't applicable, and in those
+	| situations, we simply throw exceptions. In others, we do things the way
+	| we've chosen to setup our authorization system.
+	|
+	*/
+	
+	/**
+	 * Returns the user's ID.
+	 *
+	 * @return	int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * Returns the name for the user's login.
+	 *
+	 * @return	string
+	 */
+	public function getLoginName()
+	{
+		return 'email';
+	}
+
+	/**
+	 * Returns the user's login.
+	 *
+	 * @return	string
+	 */
+	public function getLogin()
+	{
+		return $this->email;
+	}
+
+	/**
+	 * Returns the user's password (hashed).
+	 *
+	 * @return	string
+	 */
+	public function getPassword()
+	{
+		return $this->password;
+	}
+
+	/**
+	 * Returns permissions for the user.
+	 *
+	 * @return	array
+	 */
+	public function getPermissions()
+	{
+		return $this->getMergedPermissions();
+	}
+
+	/**
+	 * Check if the user is activated.
+	 *
+	 * @return	bool
+	 */
+	public function isActivated()
+	{
+		return ((int) $this->status === Status::ACTIVE);
+	}
+
+	/**
+	 * Checks if the user is a super user - has access to everything regardless
+	 * of permissions.
+	 *
+	 * @return	bool
+	 */
+	public function isSuperUser()
+	{
+		return $this->isAdmin();
+	}
+
+	/**
+	 * Validates the user and throws a number of exceptions if validation fails.
+	 *
+	 * @throws	Exception
+	 */
+	public function validate()
+	{
+		throw new Exception("User validation is not supported in Citadel.");
+	}
+
+	/**
+	 * Delete the user.
+	 *
+	 * We don't ever actually delete a user, we only hide them.
+	 *
+	 * @return	bool
+	 */
+	public function delete()
+	{
+		$this->status = Status::REMOVED;
+		$this->save();
+
+		return true;
+	}
+
+	/**
+	 * Gets a code for when the user is persisted to a cookie or session which
+	 * identifies the user.
+	 *
+	 * @return	string
+	 */
+	public function getPersistCode()
+	{
+		$this->persist_hash = Str::random(42);
+
+		// Our code got hashed
+		$persistCode = $this->persist_hash;
+
+		$this->save();
+
+		return $persistCode;
+	}
+
+	/**
+	 * Checks the given persist code.
+	 *
+	 * @param	string	The persist code to check
+	 * @return	bool
+	 */
+	public function checkPersistCode($persistCode)
+	{
+		if ( ! $persistCode)
+		{
+			return false;
+		}
+
+		return $persistCode == $this->persist_hash;
+	}
+
+	/**
+	 * Get an activation code for the given user.
+	 *
+	 * @throws	Exception
+	 */
+	public function getActivationCode()
+	{
+		throw new Exception("Activation is not supported in Citadel.");
+	}
+
+	/**
+	 * Attempts to activate the given user by checking the activate code. If the
+	 * user is activated already, an exception is thrown.
+	 *
+	 * @throws	Exception
+	 */
+	public function attemptActivation($activationCode)
+	{
+		throw new Exception("Activation is not supported in Citadel.");
+	}
+
+	/**
+	 * Get a reset password code for the given user.
+	 *
+	 * @return	string
+	 */
+	public function getResetPasswordCode()
+	{
+		$this->reset_password_hash = Str::random(42);
+
+		// Our code got hashed
+		$resetCode = $this->reset_password_hash;
+
+		$this->save();
+
+		return $resetCode;
+	}
+
+	/**
+	 * Checks if the provided user reset password code is valid without actually
+	 * resetting the password.
+	 *
+	 * @param	string	The reset code to test
+	 * @return	bool
+	 */
+	public function checkResetPasswordCode($resetCode)
+	{
+		return ($this->reset_password_hash == $resetCode);
+	}
+
+	/**
+	 * Attemps to reset a user's password by matching the reset code generated
+	 * with the user's.
+	 *
+	 * @param	string	The reset code
+	 * @param	string	The new password
+	 * @return	bool
+	 */
+	public function attemptResetPassword($resetCode, $newPassword)
+	{
+		if ($this->checkResetPasswordCode($resetCode))
+		{
+			$this->password = $newPassword;
+			$this->reset_password_hash = null;
+			
+			return $this->save();
+		}
+
+		return false;
+	}
+
+	/**
+	 * Wipes out the data associated with resetting a password.
+	 *
+	 * @return	void
+	 */
+	public function clearResetPassword()
+	{
+		if ($this->reset_password_hash)
+		{
+			$this->reset_password_hash = null;
+			
+			$this->save();
+		}
+	}
+
+	/**
+	 * Returns the role the user is assigned.
+	 *
+	 * @param	bool	Get the full object?
+	 * @return	object|string
+	 */
+	public function getGroups($getObject = false)
+	{
+		if ($getObject)
+		{
+			return $this->role;
+		}
+
+		return $this->role->name;
+	}
+
+	/**
+	 * Adds the user to the given group
+	 *
+	 * @param	Cartalyst\Sentry\Groups\GroupInterface  $group
+	 * @return	bool
+	 */
+	public function addGroup(GroupInterface $group)
+	{
+		$this->role_id = $group->id;
+		$this->save();
+	}
+
+	/**
+	 * Removes the user from the given group.
+	 *
+	 * @param	Cartalyst\Sentry\Groups\GroupInterface  $group
+	 * @return	bool
+	 */
+	public function removeGroup(GroupInterface $group)
+	{
+		throw new Exception("Removing a user from a role is not supported in Citadel.");
+	}
+
+	/**
+	 * See if the user is in the given group.
+	 *
+	 * @param	Cartalyst\Sentry\Groups\GroupInterface  $group
+	 * @return	bool
+	 */
+	public function inGroup(GroupInterface $group)
+	{
+		return $this->hasRole($group->id, true);
+	}
+
+	/**
+	 * Returns an array of merged permissions for each group the user is in.
+	 *
+	 * @return	array
+	 */
+	public function getMergedPermissions()
+	{
+		// Get the roles from the session
+		$roles = Session::get('role');
+		
+		// If we don't have anything in the session, calculate the roles
+		if ($roles === null)
+		{
+			// Set up an empty array for storing all the role tasks
+			$roles = array();
+			
+			// Loop through the primary role's tasks and add them
+			foreach ($this->role->tasks as $task)
+			{
+				if ((isset($roles[$task->component][$task->action]) 
+						and $roles[$task->component][$task->action] < (int) $task->level)
+						or ! isset($roles[$task->component][$task->action]))
+				{
+					$roles[$task->component][$task->action] = (int) $task->level;
+				}
+			}
+			
+			// Get an array of inherited roles
+			$inherited = explode(',', $this->role->inherits);
+			
+			// Loop through the inherited roles
+			foreach ($inherited as $i)
+			{
+				// Make sure we aren't doing anything stupid
+				if ($i > 0 and $i !== null)
+				{
+					// Get the role
+					$r = AccessRoleModel::find($i);
+					
+					// Loop through the role's tasks and add them
+					foreach ($r->tasks as $t)
+					{
+						if ((isset($roles[$t->component][$t->action]) 
+								and $roles[$t->component][$t->action] < (int) $t->level)
+								or ! isset($roles[$t->component][$t->action]))
+						{
+							$roles[$t->component][$t->action] = (int) $t->level;
+						}
+					}
+				}
+			}
+
+			// Put the role info into the session
+			Session::put('role', $roles);
+		}
+		
+		return $roles;
+	}
+
+	/**
+	 * See if a user has access to the passed permission(s). Permissions are
+	 * merged from all groups the user belongs to and then are checked against
+	 * the passed permission(s).
+	 *
+	 * @param	string	Dot-notated component-action item
+	 * @return	bool
+	 */
+	public function hasAccess($permissions)
+	{
+		$mergedPermissions = $this->getMergedPermissions();
+
+		return (array_get($mergedPermissions, $permissions, false) !== false);
+	}
+
+	/**
+	 * Checks if the user has at least the given role or higher.
+	 *
+	 * @param	int		The role ID
+	 * @param	bool	Should it be a strict comparison (must equal) or not (greater than or equal)
+	 * @return	bool
+	 */
+	public function hasRole($role, $strict = false)
+	{
+		if ($strict)
+		{
+			return ((int) $this->role_id === (int) $role);
+		}
+
+		return ((int) $this->role_id >= (int) $role);
+	}
+
+	/**
+	 * Check if the user is allowed to access a page.
+	 *
+	 * @param	mixed	An access key or an array of keys
+	 * @param	bool	Should they be redirected
+	 * @return	bool
+	 */
+	public function allowed($key, $redirect = false)
+	{
+		// Check the login
+		if (Sentry::check() === false and $redirect === true)
+		{
+			//return Redirect::to('login/index/'.\Login\Controller_Login::NOT_LOGGED_IN);
+		}
+		else
+		{
+			// Get the current user
+			$user = Sentry::getUser();
+
+			// If we have a simple string, make it an array
+			if ( ! is_array($key))
+			{
+				$key = (array) $key;
+			}
+
+			// Create a temp array
+			$allowed = array();
+
+			// Loop through the array and see if they have access
+			foreach ($key as $k)
+			{
+				$allowed[] = $user->hasAccess($k);
+			}
+
+			if ($redirect === true and ! in_array(true, $allowed))
+			{
+				//return Redirect::to('admin/error/'.\Nova\Controller_Admin::NOT_ALLOWED);
+			}
+			
+			return (in_array(true, $allowed));
+		}
+
+		return false;
+	}
+
+	/**
+	 * Checks if the user has the given level
+	 *
+	 * @param	string	A dot-notated array key of the task
+	 * @param	int		The level to check for (default: 0)
+	 * @param	bool	Strict (exactly the level) or loose (at least the level)
+	 * @return	bool
+	 */
+	public function hasLevel($permissions, $level = 0, $strict = true)
+	{
+		// If they don't have permission, break out early
+		if ($this->hasAccess($permissions) === false)
+		{
+			return false;
+		}
+
+		// Get the full list of permissions
+		$mergedPermissions = $this->getMergedPermissions();
+
+		// Check to see if the user has AT LEAST a level
+		if ($strict)
+		{
+			return (array_get($mergedPermissions, $permissions, false) === $level);
+		}
+
+		// Check to see if the user has EXACTLY a level
+		return (array_get($mergedPermissions, $permissions, false) >= $level);
 	}
 }
