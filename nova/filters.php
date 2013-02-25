a1ace8a5ca812e58ff9d896fc06979bde2685b48
David VanScott <david.vanscott@gmail.com>
2013-02-25 18:23:04 -0500

    Make sure CSRF checking only happens on POST

--- nova/filters.php
+++ nova/filters.php
@@ -28,9 +28,12 @@ App::after(function($request, $response)
 
 Route::filter('csrf', function()
 {
-	if (Session::getToken() != Input::get('csrf_token'))
+	if (Request::getMethod() == 'POST')
 	{
-		throw new Illuminate\Session\TokenMismatchException;
+		if (Session::getToken() != Input::get('csrf_token'))
+		{
+			throw new Illuminate\Session\TokenMismatchException;
+		}
 	}
 });
 
