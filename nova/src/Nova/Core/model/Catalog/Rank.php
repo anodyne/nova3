bb5e5d9f2aad5d852946d9a865f3d33283f86fac
David VanScott <david.vanscott@gmail.com>
2013-03-11 16:14:41 -0400

    Cleanup the rank catalog model

--- nova/src/Nova/Core/model/Catalog/Rank.php
+++ nova/src/Nova/Core/model/Catalog/Rank.php
@@ -6,65 +6,45 @@
  * @subpackage	Core
  * @category	Model
  * @author		Anodyne Productions
- * @copyright	2012 Anodyne Productions
+ * @copyright	2013 Anodyne Productions
  */
  
 namespace Nova\Core\Model\Catalog;
 
-class Rank extends \Model implements \QuickInstallInterface {
+use Model;
+use Config;
+use Status;
+use Exception;
+use QuickInstallInterface;
+use FuelPHP\FileSystem\Directory;
+
+class Rank extends Model implements QuickInstallInterface {
 
 	protected $table = 'catalog_ranks';
 
-	protected static $_properties = array(
-		'id' => array(
-			'type' => 'int',
-			'constraint' => 11,
-			'auto_increment' => true),
-		'name' => array(
-			'type' => 'string',
-			'constraint' => 255,
-			'null' => true),
-		'location' => array(
-			'type' => 'string',
-			'constraint' => 255,
-			'null' => true),
-		'preview' => array(
-			'type' => 'string',
-			'constraint' => 50,
-			'default' => 'preview.png'),
-		'blank' => array(
-			'type' => 'string',
-			'constraint' => 50,
-			'default' => 'blank.png'),
-		'extension' => array(
-			'type' => 'string',
-			'constraint' => 5,
-			'default' => '.png'),
-		'status' => array(
-			'type' => 'tinyint',
-			'constraint' => 1,
-			'default' => \Status::ACTIVE),
-		'credits' => array(
-			'type' => 'text'),
-		'default' => array(
-			'type' => 'tinyint',
-			'constraint' => 1,
-			'default' => 0),
-		'genre' => array(
-			'type' => 'string',
-			'constraint' => 10,
-			'null' => true),
+	protected static $properties = array(
+		'id'			=> array('type' => 'int', 'constraint' => 11, 'auto_increment' => true),
+		'name'			=> array('type' => 'string'),
+		'location'		=> array('type' => 'string'),
+		'preview'		=> array('type' => 'string', 'constraint' => 50, 'default' => 'preview.png'),
+		'blank'			=> array('type' => 'string', 'constraint' => 50, 'default' => 'blank.png'),
+		'extension'		=> array('type' => 'string', 'constraint' => 5, 'default' => '.png'),
+		'status'		=> array('type' => 'tinyint', 'constraint' => 4, 'default' => Status::ACTIVE),
+		'credits'		=> array('type' => 'text', 'null' => true),
+		'default'		=> array('type' => 'tinyint', 'constraint' => 1, 'default' => 0),
+		'genre'			=> array('type' => 'string', 'constraint' => 10),
+		'created_at'	=> array('type' => 'datetime'),
+		'updated_at'	=> array('type' => 'datetime'),
 	);
 	
 	/**
 	 * Get all items from the catalog.
 	 *
-	 * @api
 	 * @param	string	Status to pull
 	 * @param	bool	Whether to limit to the current genre (true) or all genres (false)
-	 * @return	object
+	 * @return	Collection
 	 */
-	public static function getItems($status = \Status::ACTIVE, $limitToGenre = true)
+	public static function getItems($status = Status::ACTIVE, $limitToCurrentGenre = true)
 	{
 		// Get a new instance of the model
 		$instance = new static;
@@ -72,9 +52,9 @@ class Rank extends \Model implements \QuickInstallInterface {
 		// Start a new Query Builder
 		$query = $instance->newQuery();
 
-		if ($limitToGenre)
+		if ($limitToCurrentGenre)
 		{
-			$query->where('genre', \Config::get('nova.genre'));
+			$query->where('genre', Config::get('nova.genre'));
 		}
 
 		if ( ! empty($status))
@@ -88,9 +68,8 @@ class Rank extends \Model implements \QuickInstallInterface {
 	/**
 	 * Get the default rank catalog item.
 	 *
-	 * @api
 	 * @param	bool	Return just the location value (true) or the whole object (false)
-	 * @return	string|object
+	 * @return	string|Collection
 	 */
 	public static function getDefault($valueOnly = false)
 	{
@@ -110,66 +89,84 @@ class Rank extends \Model implements \QuickInstallInterface {
 		return $result;
 	}
 
+	/**
+	 * Install via QuickInstall.
+	 *
+	 * @param	string	A specific location to install
+	 * @return	void
+	 */
 	public static function install($location = null)
 	{
+		// Get the genre
+		$genre = Config::get('nova.genre');
+
 		if ($location === null)
 		{
-			// get the directory listing for the genre
-			$dir = \File::read_dir(APPPATH.'assets/common/'.\Config::get('nova.genre').'/ranks/');
+			// Map the ranks directory
+			$fs = new Directory(APPPATH."assets/common/{$genre}/ranks");
+			$dir = $fs->listFiles();
 
-			// get all the rank sets locations
+			// Get all the rank set locations
 			$ranks = static::getItems();
 
 			if (count($ranks) > 0)
 			{
-				// start by removing anything that's already installed
+				// Start by removing anything that's already installed
 				foreach ($ranks as $rank)
 				{
-					if (array_key_exists($rank->location.DS, $dir))
+					if (array_key_exists($rank->location.DIRECTORY_SEPARATOR, $dir))
 					{
-						unset($dir[$rank->location.DS]);
+						unset($dir[$rank->location.DIRECTORY_SEPARATOR]);
 					}
 				}
 			}
 				
-			// loop through the directories now
+			// Loop through the directories now
 			foreach ($dir as $key => $value)
 			{
-				// assign our path to a variable
-				$file = APPPATH.'assets/common/'.\Config::get('nova.genre').'/ranks/'.$key.'rank.json';
+				// Assign our path to a variable
+				$file = APPPATH."assets/common/{$genre}/ranks/{$key}rank.json";
 
-				// make sure the file exists first
+				// Make sure the file exists first
 				if (file_exists($file))
 				{
-					// get the contents and decode the JSON
+					// Get the contents and decode the JSON
 					$content = file_get_contents($file);
 					$data = json_decode($content);
 
-					// create the item
+					// Create the item
 					static::createItem($data);
 				}
 			}
 		}
 		else
 		{
-			// assign our path to a variable
-			$file = APPPATH.'assets/common/'.\Config::get('nova.genre').'/ranks/'.$location.'/rank.json';
+			// Assign our path to a variable
+			$file = APPPATH."assets/common/{$genre}/ranks/{$location}/rank.json";
 			
-			// make sure the file exists first
+			// Make sure the file exists first
 			if (file_exists($file))
 			{
-				// get the contents and decode the JSON
+				// Get the contents and decode the JSON
 				$content = file_get_contents($file);
 				$data = json_decode($content);
 				
-				// create the item
+				// Create the item
 				static::createItem($data);
 			}
 		}
 	}
 
+	/**
+	 * Uninstall via QuickInstall.
+	 *
+	 * @param	string	A specific location to uninstall
+	 * @return	void
+	 * @throws	Exception
+	 */
 	public static function uninstall($location)
 	{
-		throw new \Exception('Uninstall method is not implemented.');
+		throw new Exception('Uninstall method is not implemented.');
 	}
+
 }
