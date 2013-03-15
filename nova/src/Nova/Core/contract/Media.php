eb866a20979245d16ebad41b78af8639353d6c6b
David VanScott <david.vanscott@gmail.com>
2013-03-14 21:37:59 -0400

    Add the media interface (WIP)

---
+++ nova/src/Nova/Core/contract/Media.php
@@ -0,0 +1,15 @@
+<?php namespace Nova\Core\Contract;
+
+interface Media {
+	
+	public function addMedia();
+
+	public function getAllMedia();
+
+	public function getMediaInfo();
+
+	public function getMediaItem();
+
+	public function removeMedia();
+
+}
