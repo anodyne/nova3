a383b2cdc1065ef9be249a5c8bb65d5a85568a11
David VanScott <david.vanscott@gmail.com>
2013-03-28 23:08:43 -0400

    Update the HtmlServiceProvider

--- nova/src/Nova/Core/providers/HtmlServiceProvider.php
+++ nova/src/Nova/Core/providers/HtmlServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace Nova\Core\Providers;
 
 use Nova\Core\Lib\FormBuilder;
+use Illuminate\Html\HtmlBuilder;
 use Illuminate\Support\ServiceProvider;
 
 class HtmlServiceProvider extends ServiceProvider {
@@ -19,13 +20,36 @@ class HtmlServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
+		$this->registerHtmlBuilder();
+
+		$this->registerFormBuilder();
+	}
+
+	/**
+	 * Register the HTML builder instance.
+	 *
+	 * @return void
+	 */
+	protected function registerHtmlBuilder()
+	{
+		$this->app['html'] = $this->app->share(function($app)
+		{
+			return new HtmlBuilder($app['url']);
+		});
+	}
+
+	/**
+	 * Register the form builder instance.
+	 *
+	 * @return void
+	 */
+	protected function registerFormBuilder()
+	{
 		$this->app['form'] = $this->app->share(function($app)
 		{
-			$form = new FormBuilder($app['url'], $app['session']->getToken());
+			$form = new FormBuilder($app['html'], $app['url'], $app['session']->getToken());
 
-			$form->setSessionStore($app['session']);
-
-			return $form;
+			return $form->setSessionStore($app['session']);
 		});
 	}
 
@@ -36,7 +60,7 @@ class HtmlServiceProvider extends ServiceProvider {
 	 */
 	public function provides()
 	{
-		return array('form');
+		return array('html', 'form');
 	}
 
 }
