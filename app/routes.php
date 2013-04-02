e35e05e8efab4307be69ae0629b64add2ec2eb90
David VanScott <david.vanscott@gmail.com>
2013-04-02 17:05:15 -0400

    Update the docblock in the app routes file

--- app/routes.php
+++ app/routes.php
@@ -4,11 +4,6 @@
  *
  * Nova stores its routes in the core. You can create your own
  * routes here.
- *
- * @package		Nova
- * @category	File
- * @author		Anodyne Productions
- * @copyright	2012 Anodyne Productions
  */
 
 // Pull in the core routes
