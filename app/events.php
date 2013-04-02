e35e17e825255385d215b1676e1794742b55dc3a
David VanScott <david.vanscott@gmail.com>
2013-04-02 17:04:49 -0400

    Move the core events to nova/start

--- app/events.php
+++ app/events.php
@@ -5,11 +5,7 @@
  * Nova stores its events in the core. You can create your own
  * event listeners here, though you should do so with caution since events
  * can impact the entire system.
- *
- * @package		Nova
- * @category	File
- * @author		Anodyne Productions
- * @copyright	2013 Anodyne Productions
  */
 
-require NOVAPATH.'events.php';
+// Pull in the core events
+require NOVAPATH.'start/events.php';
