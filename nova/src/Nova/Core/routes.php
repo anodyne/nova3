26232090fcd49d79f9eb6675a6531afca2fa6ebd
David VanScott <david.vanscott@gmail.com>
2013-04-04 16:24:17 -0400

    Cleanup the routes

--- nova/src/Nova/Core/routes.php
+++ nova/src/Nova/Core/routes.php
@@ -5,10 +5,10 @@
  */
 
 // Pull in the core routes
-require_once SRCPATH.'Api/routes.php';
-require_once SRCPATH.'Forum/routes.php';
-require_once SRCPATH.'Setup/routes.php';
-require_once SRCPATH.'Wiki/routes.php';
+require SRCPATH.'Api/routes.php';
+require SRCPATH.'Forum/routes.php';
+require SRCPATH.'Setup/routes.php';
+require SRCPATH.'Wiki/routes.php';
 
 // Get the module list
 $modules = Cache::get('nova_module_list', array());
@@ -16,19 +16,14 @@ $modules = Cache::get('nova_module_list', array());
 // Loop through the modules and include their route files
 foreach ($modules as $m)
 {
-	if (file_exists(APPPATH."module/{$m}/routes.php"))
+	if (File::exists(APPPATH."module/{$m}/routes.php"))
 	{
-		include_once APPPATH."module/{$m}/routes.php";
+		include APPPATH."module/{$m}/routes.php";
 	}
 }
 
 // Pull in the test routes if we're local
 if (App::environment() == 'local')
 {
-	require_once SRCPATH.'Core/routes/test.php';
+	require SRCPATH.'Core/routes/test.php';
 }
-
-Route::get('temp/main/index', function()
-{
-	return 'Temporary main index page.';
-});
