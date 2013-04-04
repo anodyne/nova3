2031ebc3ed9dfa70b043399a5d1de4060edc7a4b
David VanScott <david.vanscott@gmail.com>
2013-04-04 08:18:13 -0400

    Cleanup the setup lang file

--- nova/src/Nova/Setup/lang/en/setup.php
+++ nova/src/Nova/Setup/lang/en/setup.php
@@ -36,33 +36,8 @@ return array(
 			),
 		),
 	),
-	
-	'error' => array(
-		'1' => "The system is already installed. If you want to re-install the system, you must first remove all the system data and database tables.",
-		'2' => "You must be a system administrator to change this RPG's genre!",
-		'no_genre' => "Uh-oh! I wasn't able to find a genre code in your Nova config file. This can happen if you have created your own genre file, have edited the Nova config file, or you experienced some file corruption with the download. To fix this, open the Nova config file located at <code>app/config/nova.php</code> and add the genre code before trying again.",
-		'not_logged_in' => "Oops! You aren't logged in and can't see the install options until you :login.",
-		'install_tables_exist' => "Uh oh! Looks like you've opted to do a fresh install, but you've got a previous version of Nova in this database with the same database table prefix (<code>:prefix</code>). Before you can continue, you'll need to change your database table prefix in the <nobr><code>app/config/production/db.php</code></nobr> file.",
-	),
-	
-	'install' => array(
-		'step1' => array(
-			'success' => "You're pretty good at this! The database tables have been created and some basic data put in to them. Now, just fill out the information below and I'll update the system with it.",
-			'failure' => "Uh oh! I ran in to a problem trying creating the database and put some basic data in the tables. For starters, make sure all your settings are right and try again. If you still can't install the system, drop us a line at our <a href='http://forums.anodyne-productions.com' target='_blank'>forums</a> for more help.",
-		),
-	),
 
 	'uninstall' => array(
 		'instructions' => "Whoa, hold up! Uninstalling Nova will remove all the data in the database tables (posts, logs, characters, etc.) as well as any MODs you've installed and cannot be undone. <strong>Make absolutely sure you want to do this before continuing.</strong>",
-		'success' => "Poof! I was able to successfully uninstall Nova 3. Now, you can go back to the Setup Center to re-install Nova 3 or upgrade from Nova 2.",
-		'failure' => "Uh oh! I wasn't able to uninstall Nova 3. Try again or if the problem continues, you can manually remove the database tables from the database.",
-		'no_tables' => "I couldn't find a Nova 3 installation to remove.",
-	),
-
-	'change' => array(
-		'default' => "You can use the sections below to modify your database for any changes you'd like to make. You can only add tables and fields, you cannot delete or modify existing tables or fields. In addition, you can only take these actions on Nova's tables, no other tables in your database.</p><p class='alert alert-danger'>Use extreme caution when modifying the database!",
-		'query' => "I can run any properly formatted MySQL query you have (like something that may have come with a MOD). Simply paste the query into the field below and I'll execute the query.</p><p class='alert alert-danger'><strong>Warning:</strong> MySQL queries can cause a lot of damage to your Nova database so make sure you trust whoever gave you the query and understand what the query is trying to do!",
-		'field' => "Need a new field in one of the Nova database tables? I can create a new field for you, all you need to do is tell me a little about the field you want to create.",
-		'table' => "Need a new database table for a MOD or something cool you're working on? I can create a new database table for you, all you need to do is tell me what you want to call the table. Don't worry about adding the table prefix, I'll do that before creating the table as well as an ID field for you. If you want to change the ID field, you'll have to do that from inside the database.",
 	),
 );
