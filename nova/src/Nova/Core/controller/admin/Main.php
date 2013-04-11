20ab475d9bdb58b5759d8bd6fafde340e4b287e1
David VanScott <david.vanscott@gmail.com>
2013-04-11 14:39:32 -0400

    Start working on the admin controller

---
+++ nova/src/Nova/Core/controller/admin/Main.php
@@ -0,0 +1,13 @@
+<?php namespace Nova\Core\Controller\Admin;
+
+use Sentry;
+use AdminBaseController;
+
+class Main extends AdminBaseController {
+
+	public function getIndex()
+	{
+		d(Sentry::getUser());
+	}
+
+}
