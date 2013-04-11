9d2a37b62a8fe24e846ac7be51b925226cb3b29d
David VanScott <david.vanscott@gmail.com>
2013-04-11 17:38:13 -0400

    Override the RedirectResponse class
    We do this because for some God-unknown reason, Symfony's HTTP Foundation
    sets content in the body that we don't want to display.

--- nova/src/Nova/Foundation/Routing/Redirector.php
+++ nova/src/Nova/Foundation/Routing/Redirector.php
@@ -10,22 +10,31 @@
  * @copyright	2013 Anodyne Productions
  */
 
+use Nova\Foundation\Http\RedirectResponse;
 use Illuminate\Routing\Redirector as LaravelRedirector;
 
 class Redirector extends LaravelRedirector {
 
 	/**
-	 * Create a new redirect response to the given path.
+	 * Create a new redirect response.
 	 *
 	 * @param  string  $path
 	 * @param  int     $status
 	 * @param  array   $headers
-	 * @param  bool    $secure
 	 * @return \Illuminate\Http\RedirectResponse
 	 */
-	public function to($path, $status = 302, $headers = array(), $secure = null)
+	protected function createRedirect($path, $status, $headers)
 	{
-		return parent::to($path, $status, $headers, $secure)->setContent('');
+		$redirect = new RedirectResponse($path, $status, $headers);
+
+		if (isset($this->session))
+		{
+			$redirect->setSession($this->session);
+		}
+
+		$redirect->setRequest($this->generator->getRequest());
+
+		return $redirect;
 	}
 
 }
