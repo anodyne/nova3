e6789c6ddabfeb488dc163f7340e1f80e23d43e6
David VanScott <david.vanscott@gmail.com>
2013-04-12 11:26:45 -0400

    BS3 fixes for some views

--- nova/src/Nova/Setup/views/components/page/setup/config/database.blade.php
+++ nova/src/Nova/Setup/views/components/page/setup/config/database.blade.php
@@ -4,36 +4,36 @@
 	<p>{{ $message }}</p>
 	
 	{{ Form::open(array('url' => 'setup/config/db/check')) }}
-		<div class="row-fluid">
-			<div class="span6">
+		<div class="row">
+			<div class="col-span-6">
 				<label class="control-label">Database Name</label>
 				<input type="text" class="span10" name="dbName" value="{{ Session::get('dbName', '') }}">
 				<p class="help-block">The name of the database you're installing Nova into</p>
 			</div>
 
-			<div class="span6">
+			<div class="col-span-6">
 				<label class="control-label">Database Host</label>
 				<input type="text" class="span10" name="dbHost" value="{{ Session::get('dbHost', 'localhost') }}">
 				<p class="help-block">You most likely won't need to change this</p>
 			</div>
 		</div>
 
-		<div class="row-fluid">
-			<div class="span6">
+		<div class="row">
+			<div class="col-span-6">
 				<label class="control-label">Username</label>
 				<input type="text" class="span10" name="dbUser" value="{{ Session::get('dbUser', '') }}">
 				<p class="help-block">Your database username</p>
 			</div>
 
-			<div class="span6">
+			<div class="col-span-6">
 				<label class="control-label">Password</label>
 				<input type="text" class="span10" name="dbPass" value="{{ Session::get('dbPass', '') }}">
 				<p class="help-block">Your database password</p>
 			</div>
 		</div>
 
-		<div class="row-fluid">
-			<div class="span6">
+		<div class="row">
+			<div class="col-span-6">
 				<label class="control-label">Table Prefix</label>
 				<input type="text" class="span10" name="prefix" value="{{ Session::get('prefix', 'nova_') }}">
 				<p class="help-block">The database table prefix to be used</p>
--- nova/src/Nova/Setup/views/components/page/setup/config/email.blade.php
+++ nova/src/Nova/Setup/views/components/page/setup/config/email.blade.php
@@ -6,42 +6,46 @@
 	<hr>
 	
 	{{ Form::open(array('url' => 'setup/config/email/write')) }}
-		<div class="control-group">
-			<label class="control-label">Email Driver</label>
-			<div class="controls">
-				<label class="radio inline"><input type="radio" name="driver" value="smtp" checked="checked"> SMTP</label>
-				<label class="radio inline"><input type="radio" name="driver" value="mail"> PHP Mail</label>
+		<div class="row">
+			<div class="col-span-12">
+				<div class="control-group">
+					<label class="control-label">Email Driver</label>
+					<div class="controls">
+						<label class="radio radio-inline"><input type="radio" name="driver" value="smtp" checked="checked"> SMTP</label>
+						<label class="radio radio-inline"><input type="radio" name="driver" value="mail"> PHP Mail</label>
+					</div>
+				</div>
 			</div>
 		</div>
 
-		<div class="row-fluid">
-			<div class="span6">
+		<div class="row">
+			<div class="col-span-6">
 				<label class="control-label">SMTP Host</label>
 				<input type="text" class="span10" name="hostname" value="{{ Session::get('hostname', '') }}">
 				<p class="help-block">The name of the SMTP server you'd like to use</p>
 			</div>
 
-			<div class="span3">
+			<div class="col-span-3">
 				<label class="control-label">SMTP Port</label>
 				<input type="text" class="span5" name="port" value="{{ Session::get('port', '25') }}">
 				<p class="help-block">The port to use for SMTP</p>
 			</div>
 
-			<div class="span3">
+			<div class="col-span-3">
 				<label class="control-label">Encryption Protocol</label>
 				<input type="text" class="span5" name="encryption" value="{{ Session::get('encryption', 'tls') }}">
 				<p class="help-block">The encryption protocol to be used</p>
 			</div>
 		</div>
 
-		<div class="row-fluid">
-			<div class="span6">
+		<div class="row">
+			<div class="col-span-6">
 				<label class="control-label">Username</label>
 				<input type="text" class="span10" name="username" value="{{ Session::get('username', '') }}">
 				<p class="help-block">Your SMTP username</p>
 			</div>
 
-			<div class="span6">
+			<div class="col-span-6">
 				<label class="control-label">Password</label>
 				<input type="text" class="span10" name="password" value="{{ Session::get('password', '') }}">
 				<p class="help-block">Your SMTP password</p>
