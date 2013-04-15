4fb63509a647ef936dd5b2c28fbc942db5b71b6e
David VanScott <david.vanscott@gmail.com>
2013-04-15 09:46:37 -0400

    Update the user model to comply with the Sentry user interface

--- nova/src/Nova/Core/model/User.php
+++ nova/src/Nova/Core/model/User.php
@@ -1,6 +1,7 @@
 <?php namespace Nova\Core\Model;
 
 use Str;
+use Date;
 use Model;
 use Event;
 use Config;
@@ -342,6 +343,16 @@ class User extends Model implements UserInterface {
 	}
 
 	/**
+	 * Returns the name for the user's password.
+	 *
+	 * @return string
+	 */
+	public function getPasswordName()
+	{
+		return 'password';
+	}
+
+	/**
 	 * Returns the user's password (hashed).
 	 *
 	 * @return	string
@@ -663,6 +674,112 @@ class User extends Model implements UserInterface {
 	}
 
 	/**
+	 * See if a user has access to the passed permission(s).
+	 * Permissions are merged from all groups the user belongs to
+	 * and then are checked against the passed permission(s).
+	 *
+	 * If multiple permissions are passed, the user must
+	 * have access to all permissions passed through, unless the
+	 * "all" flag is set to false.
+	 *
+	 * Super users DON'T have access no matter what.
+	 *
+	 * @param  string|array  $permissions
+	 * @param  bool $all
+	 * @return bool
+	 */
+	public function hasPermission($permissions, $all = true)
+	{
+		$mergedPermissions = $this->getMergedPermissions();
+
+		if ( ! is_array($permissions))
+		{
+			$permissions = (array) $permissions;
+		}
+
+		foreach ($permissions as $permission)
+		{
+			// We will set a flag now for whether this permission was
+			// matched at all.
+			$matched = true;
+
+			// Now, let's check if the permission ends in a wildcard "*" symbol.
+			// If it does, we'll check through all the merged permissions to see
+			// if a permission exists which matches the wildcard.
+			if ((strlen($permission) > 1) and ends_with($permission, '*'))
+			{
+				$matched = false;
+
+				foreach ($mergedPermissions as $mergedPermission => $value)
+				{
+					// Strip the '*' off the end of the permission.
+					$checkPermission = substr($permission, 0, -1);
+
+					// We will make sure that the merged permission does not
+					// exactly match our permission, but starts wtih it.
+					if ($checkPermission != $mergedPermission and starts_with($mergedPermission, $checkPermission) and $value == 1)
+					{
+						$matched = true;
+						break;
+					}
+				}
+			}
+
+			else
+			{
+				$matched = false;
+
+				foreach ($mergedPermissions as $mergedPermission => $value)
+				{
+					// This time check if the mergedPermission ends in wildcard "*" symbol.
+					if ((strlen($mergedPermission) > 1) and ends_with($mergedPermission, '*'))
+					{
+						$matched = false;
+
+						// Strip the '*' off the end of the permission.
+						$checkMergedPermission = substr($mergedPermission, 0, -1);
+
+						// We will make sure that the merged permission does not
+						// exactly match our permission, but starts wtih it.
+						if ($checkMergedPermission != $permission and starts_with($permission, $checkMergedPermission) and $value == 1)
+						{
+							$matched = true;
+							break;
+						}
+					}
+
+					// Otherwise, we'll fallback to standard permissions checking where
+					// we match that permissions explicitly exist.
+					elseif ($permission == $mergedPermission and $mergedPermissions[$permission] == 1)
+					{
+						$matched = true;
+						break;
+					}
+				}
+			}
+
+			// Now, we will check if we have to match all
+			// permissions or any permission and return
+			// accordingly.
+			if ($all === true and $matched === false)
+			{
+				return false;
+			}
+			elseif ($all === false and $matched === true)
+			{
+				return true;
+			}
+		}
+
+		if ($all === false)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
 	 * Returns if the user has access to any of the
 	 * given permissions.
 	 *
@@ -672,6 +789,17 @@ class User extends Model implements UserInterface {
 	public function hasAnyAccess(array $permissions)
 	{
 		return true;
+	}
+
+	/**
+	 * Records a login for the user.
+	 *
+	 * @return void
+	 */
+	public function recordLogin()
+	{
+		$this->last_login = Date::now();
+		$this->save();
 	}
 
 	/**
