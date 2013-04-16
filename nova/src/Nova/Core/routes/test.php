7550af60f56f8f41a095936c7e83400099abf2df
David VanScott <david.vanscott@gmail.com>
2013-04-15 21:56:45 -0400

    Test routes

--- nova/src/Nova/Core/routes/test.php
+++ nova/src/Nova/Core/routes/test.php
@@ -4,8 +4,8 @@ Route::group(array('prefix' => 'test'), function()
 {
 	Route::get('index', function()
 	{
-		$position = Position::find(1);
+		$user = User::find(1);
 
-		s($position->dept->id);
+		s($user->appReviews);
 	});
 });
