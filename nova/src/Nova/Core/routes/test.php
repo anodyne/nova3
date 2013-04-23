d5ae33ba560745ecf6f848e1df23995abde9e386
David VanScott <david.vanscott@gmail.com>
2013-04-23 16:18:31 -0400

    Test routes

--- nova/src/Nova/Core/routes/test.php
+++ nova/src/Nova/Core/routes/test.php
@@ -4,6 +4,8 @@ Route::group(array('prefix' => 'test'), function()
 {
 	Route::get('index', function()
 	{
-		//
+		Cache::forget('content_title_role');
+		Cache::forget('content_header_role');
+		Cache::forget('content_message_role');
 	});
 });
