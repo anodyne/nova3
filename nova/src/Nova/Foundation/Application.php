bf2a466fe6b6f566650e03ff95258b46b12f3ea2
David VanScott <david.vanscott@gmail.com>
2013-05-10 21:16:27 -0400

    Use the config loader instance out of the container

--- nova/src/Nova/Foundation/Application.php
+++ nova/src/Nova/Foundation/Application.php
@@ -1,7 +1,19 @@
 <?php namespace Nova\Foundation;
 
-use Illuminate\Filesystem\Filesystem;
-use Nova\Foundation\Config\CascadingFileLoader;
+/**
+ * The Application is extended from the Laravel Application because
+ * of a change to Laravel that hardcoded the config loader in. Instead
+ * of making the simple change in the Laravel core, we've been told
+ * we have to extend the Application class and do it ourselves.
+ * Absolute foolishness.
+ *
+ * @package		Nova
+ * @subpackage	Foundation
+ * @category	Class
+ * @author		Anodyne Productions
+ * @copyright	2013 Anodyne Productions
+ */
+
 use Illuminate\Foundation\Application as LaravelApplication;
 
 class Application extends LaravelApplication {
@@ -9,11 +21,11 @@ class Application extends LaravelApplication {
 	/**
 	 * Get the configuration loader instance.
 	 *
-	 * @return \Illuminate\Config\LoaderInterface
+	 * @return \Nova\Foundation\Config\LoaderInterface
 	 */
 	public function getConfigLoader()
 	{
-		return new CascadingFileLoader(new Filesystem, false);
+		return $this->make('config.loader');
 	}
 
 }
