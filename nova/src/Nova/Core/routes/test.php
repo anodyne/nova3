7a522990c9c48fb2914859e2551611c941c22422
David VanScott <david.vanscott@gmail.com>
2013-05-14 18:54:08 -0400

    Test routes

--- nova/src/Nova/Core/routes/test.php
+++ nova/src/Nova/Core/routes/test.php
@@ -6,7 +6,21 @@ Route::group(array('prefix' => 'test'), function()
 	{
 		//s(Config::get('cache'));
 		//Cache::put('test', 'foo', 5);
-		s(Cache::get('test'));
+		//s(Cache::get('test'));
+
+		return 'Done';
+	});
+
+	Route::get('sessions', function()
+	{
+		$user = User::find(1);
+
+		s($user->last_login);
+		s($user->updated_at);
+		s($user->role->updated_at);
+		
+		s($user->last_login->diffInMinutes($user->role->updated_at, false));
+		s($user->updated_at->diffInMinutes($user->role->updated_at, false));
 
 		return 'Done';
 	});
