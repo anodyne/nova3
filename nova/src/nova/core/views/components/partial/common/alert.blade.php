72c594d529650b98221632bbf448918189a37d79
David VanScott <david.vanscott@gmail.com>
2013-06-12 16:07:30 -0400

    Remove the close option on alerts

--- nova/src/nova/core/views/components/partial/common/alert.blade.php
+++ nova/src/nova/core/views/components/partial/common/alert.blade.php
@@ -1,5 +1,4 @@
 <div class="alert alert-block{{ (isset($class)) ? ' '.$class : '' }} fade in">
-	<a class="close" data-dismiss="alert" href="#">&times;</a>
 	{{ (isset($heading)) ? '<h4>'.$heading.'</h4>' : '' }}
 	<p>{{ $content }}</p>
 </div>
