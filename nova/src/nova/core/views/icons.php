24dd759196349d4e48a6f5440f8d279ce2e94138
David VanScott <david.vanscott@gmail.com>
2013-06-15 22:28:32 -0400

    Update the icon font

--- nova/src/nova/core/views/icons.php
+++ nova/src/nova/core/views/icons.php
@@ -5,6 +5,7 @@ return array(
 	'add' => '<span class="icn" data-icon="+"></span>',
 
 	'back' => '<span class="icn" data-icon="<"></span>',
+	'bookmark' => '<span class="icn" data-icon="b"></span>',
 
 	'categories' => '<span class="icn" data-icon=","></span>',
 	'check' => '<span class="icn" data-icon="4"></span>',
@@ -18,32 +19,40 @@ return array(
 
 	'filter' => '<span class="icn" data-icon="f"></span>',
 	'forward' => '<span class="icn" data-icon=">"></span>',
+	'favorite' => '<span class="icn" data-icon="&"></span>',
+	'favoriteHollow' => '<span class="icn" data-icon="7"></span>',
 
 	'image' => '<span class="icn" data-icon="m"></span>',
 	'info' => '<span class="icn" data-icon="i"></span>',
-	'inProgress' => '<span class="icn" data-icon="\'"></span>',
+	'inProgress' => '<span class="icn" data-icon="3"></span>',
 
 	'link' => '<span class="icn" data-icon="-"></span>',
 	'lock' => '<span class="icn" data-icon="("></span>',
+	'list' => '<span class="icn" data-icon="8"></span>',
+
+	'move' => '<span class="icn" data-icon="*"></span>',
+
+	'no' => '<span class="icn" data-icon="#"></span>',
 
 	'question' => '<span class="icn" data-icon="?"></span>',
 
-	'reject' => '<span class="icn" data-icon="2"></span>',
 	'remove' => '<span class="icn" data-icon="t"></span>',
+	'refresh' => '<span class="icn" data-icon="r"></span>',
 
 	'settings' => '<span class="icn" data-icon=")"></span>',
 	'share' => '<span class="icn" data-icon="a"></span>',
 	'star' => '<span class="icn" data-icon="5"></span>',
 	'starHollow' => '<span class="icn" data-icon="6"></span>',
-
-	'thumbsUp' => '<span class="icn" data-icon="."></span>',
-	'thumbsDown' => '<span class="icn" data-icon="/"></span>',
+	'search' => '<span class="icn" data-icon="s"></span>',
 
 	'user' => '<span class="icn" data-icon="u"></span>',
+	'upload' => '<span class="icn" data-icon="^"></span>',
 
-	'view' => '<span class="icn" data-icon="s"></span>',
+	'view' => '<span class="icn" data-icon="0"></span>',
 
 	'warning' => '<span class="icn" data-icon="!"></span>',
+
+	'yes' => '<span class="icn" data-icon="1"></span>',
 
 	/**
 	 * OLD
@@ -52,7 +61,6 @@ return array(
 
 	'groups' => 'zones.png',
 	
-	'reorder' => 'reorder-16x16.png',
 	'rules' => 'clipboard-task.png',
 	
 	'sections' => 'ui-group-box.png',
