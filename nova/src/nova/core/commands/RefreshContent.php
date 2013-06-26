ff27d70ede2c895f6d803d8464ff57d277c46caa
David VanScott <david.vanscott@gmail.com>
2013-06-26 16:31:52 -0400

    Add new artisan command to refresh site content

---
+++ nova/src/nova/core/commands/RefreshContent.php
@@ -0,0 +1,56 @@
+<?php namespace Nova\Core\Commands;
+
+use SiteContent;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class RefreshContent extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'nova:content';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Refresh the content from the data migration file.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
+		// Clear out the content table
+		SiteContent::where('key', '!=', '')->delete();
+
+		// Grab the content data file
+		$routes = include SRCPATH.'setup/database/migrations/data/content.php';
+
+		// Loop through the contents data file and insert the records
+		foreach ($routes as $r)
+		{
+			SiteContent::create($r);
+		}
+
+		$this->info('Site content refreshed!');
+	}
+
+}
