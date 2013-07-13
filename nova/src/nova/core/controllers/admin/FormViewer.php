9560427f7d21e3de72168287be6bd6784cf3a5be
David VanScott <david.vanscott@gmail.com>
2013-07-13 19:19:09 -0400

    Work on FormViewer

--- nova/src/nova/core/controllers/admin/FormViewer.php
+++ nova/src/nova/core/controllers/admin/FormViewer.php
@@ -4,12 +4,13 @@ use App;
 use Sentry;
 use NovaForm;
 use Redirect;
+use DynamicForm;
 use NovaFormData;
 use AdminBaseController;
 
 class FormViewer extends AdminBaseController {
 
-	public function getView($formKey = false, $id = false)
+	public function getView($formKey = false, $formMode = 'view', $id = false)
 	{
 		// Verify the user is allowed
 		Sentry::getUser()->allowed(['form.read', 'form.create', 'form.edit', 'form.delete'], true);
@@ -17,18 +18,16 @@ class FormViewer extends AdminBaseController {
 		// Set the JS view
 		//$this->_jsView = 'admin/formviewer/view_js';
 
-		// If we don't have a form, throw a 404
 		if ($formKey === false)
 		{
-			App::abort(404, "Page not found");
+			App::abort(404, lang('error.pageNotFound'));
 		}
 		else
 		{
 			// Get the form
 			$this->_data->form = $form = NovaForm::key($formKey)->first();
 
-			// If we don't have an ID, show all the records
-			if ($id === false)
+			if ($formMode == 'view')
 			{
 				// Set the view file
 				$this->_view = 'admin/formviewer/entries';
@@ -45,12 +44,97 @@ class FormViewer extends AdminBaseController {
 				$this->_view = 'admin/formviewer/entries_action';
 
 				// Get the entry data
-				$this->_data->entry = NovaFormData::key($formKey)->entry($id)->get();
+				//$this->_data->entry = NovaFormData::key($formKey)->entry($id)->get();
+
+				// Set the ID
+				$this->_data->id = $id;
 
 				// Set the action
-				$this->_data->action = ((int) $id === 0) ? 'create' : 'update';
+				$action = $this->_data->action = ($formMode == 'add') ? 'create' : 'update';
+
+				// Build the form
+				$this->_data->form = ($action == 'create')
+					? DynamicForm::setup($formKey, 0, true)->build()
+					: DynamicForm::setup($formKey, $id, true)->build();
 			}
 		}
+	}
+	public function postView($formKey = false)
+	{
+		// Set up the validation service
+		$validator = new FormValidator;
+
+		// If the validation fails, stop and go back
+		if ( ! $validator->passes())
+		{
+			// Set the flash message
+			$flashMessage = lang('Short.validate', lang('action.failed')).'. ';
+			$flashMessage.= implode(' ', $validator->getErrors()->all());
+
+			return Redirect::to('admin/form')
+				->with('flashStatus', 'danger')
+				->with('flashMessage', $flashMessage);
+		}
+
+		// Get the action
+		$action = e(Input::get('action'));
+
+		// Get the current user
+		$user = Sentry::getUser();
+
+		/**
+		 * Create the form entry.
+		 */
+		if ($user->hasAccess('form.read') and $action == 'create')
+		{
+			// Create the form
+			$form = NovaFormData::create(Input::all());
+
+			// Set the flash info
+			$flashStatus = ($form) ? 'success' : 'danger';
+			$flashMessage = ($form) 
+				? lang('Short.alert.success.create', lang('form'))
+				: lang('Short.alert.failure.create', lang('form'));
+		}
+
+		/**
+		 * Update the form.
+		 */
+		if ($user->hasAccess('form.update') and $action == 'update')
+		{
+			// Get the ID
+			$id = e(Input::get('id'));
+			$id = (is_numeric($id)) ? $id : false;
+
+			if ($id)
+			{
+				// Update the form
+				$form = NovaForm::where('id', $id)->update(Input::all());
+			}
+
+			// Set the flash info
+			$flashStatus = ($id) ? 'success' : 'danger';
+			$flashMessage = ($id) 
+				? lang('Short.alert.success.update', lang('form'))
+				: lang('Short.alert.failure.update', lang('form'));
+		}
+
+		/**
+		 * Delete the form.
+		 */
+		if ($user->hasAccess('form.delete') and $action == 'delete')
+		{
+			// Get the ID
+			$id = e(Input::get('id'));
+			$id = (is_numeric($id)) ? $id : false;
+
+			// Get the form
+			$form = NovaForm::find($id);
+		}
+
+		return Redirect::to("admin/formviewer/{$formKey}")
+			->with('flashStatus', $flashStatus)
+			->with('flashMessage', $flashMessage);
 	}
 
 }
--- nova/src/nova/core/views/components/page/admin/formviewer/entries_action.blade.php
+++ nova/src/nova/core/views/components/page/admin/formviewer/entries_action.blade.php
@@ -1,1 +1,12 @@
-<?php s($entry);?>
+{{ Form::open(['url' => 'admin/formviewer']) }}
+	{{ $form }}
+
+	<div class="row">
+		<div class="col-lg-12">
+			{{ Form::token() }}
+			{{ Form::hidden('id', $id) }}
+			{{ Form::hidden('action', $action) }}
+			{{ Form::button(lang('Action.submit'), ['type' => 'submit', 'class' => 'btn btn-primary']) }}
+		</div>
+	</div>
+{{ Form::close() }}
