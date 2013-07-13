e62de18687955f6c4564f86b8b3d2bb59eb5cb2b
David VanScott <david.vanscott@gmail.com>
2013-07-13 18:06:57 -0400

    Update the database seeder
    Add the form seeder. Add variables to control what does and does not
    get seeded during a run.

--- nova/src/nova/setup/database/seeds/DatabaseSeeder.php
+++ nova/src/nova/setup/database/seeds/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 class DatabaseSeeder extends Seeder {
 
+	protected $seedUsers = false;
+	protected $seedCharacters = false;
+	protected $seedApplications = false;
+	protected $seedForms = true;
+
 	/**
 	 * Run the database seeds.
 	 *
@@ -16,14 +21,29 @@ class DatabaseSeeder extends Seeder {
 			
 			Eloquent::unguard();
 			
-			$this->call('UserSeeder');
-			$this->command->info('User seeding completed');
+			if ($this->seedUsers)
+			{
+				$this->call('UserSeeder');
+				$this->command->info('User seeding completed');
+			}
 
-			$this->call('CharacterSeeder');
-			$this->command->info('Character seeding completed');
+			if ($this->seedCharacters)
+			{
+				$this->call('CharacterSeeder');
+				$this->command->info('Character seeding completed');
+			}
 
-			//$this->call('ApplicationSeeder');
-			//$this->command->info('ARC seeding completed');
+			if ($this->seedApplications)
+			{
+				$this->call('ApplicationSeeder');
+				$this->command->info('ARC seeding completed');
+			}
+
+			if ($this->seedForms)
+			{
+				$this->call('FormSeeder');
+				$this->command->info('Dynamic form seeding completed');
+			}
 		}
 		else
 		{
