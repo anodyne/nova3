125ce56b65cec6d5969d5cf26d1c44e1cedf56ef
David VanScott <david.vanscott@gmail.com>
2013-07-15 14:33:52 -0400

    Minor cleanup in the app config

--- nova/src/nova/core/config/app.php
+++ nova/src/nova/core/config/app.php
@@ -185,7 +185,7 @@ return array(
 		'Redirect'        => 'Illuminate\Support\Facades\Redirect',
 		'Redis'           => 'Illuminate\Support\Facades\Redis',
 		'Request'         => 'Illuminate\Support\Facades\Request',
-		//'Response'        => 'Illuminate\Support\Facades\Response',
+		'Response'        => 'Illuminate\Support\Facades\Response',
 		'Route'           => 'Illuminate\Support\Facades\Route',
 		'Schema'          => 'Illuminate\Support\Facades\Schema',
 		'Seeder'          => 'Illuminate\Database\Seeder',
