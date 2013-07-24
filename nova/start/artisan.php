3698a11615a6e3130c48aac5726a155bec010b83
David VanScott <david.vanscott@gmail.com>
2013-07-24 09:56:52 -0400

    Make sure that we're always reporting errors in Artisan

--- nova/start/artisan.php
+++ nova/start/artisan.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+
 /*
 |--------------------------------------------------------------------------
 | Register The Artisan Commands
