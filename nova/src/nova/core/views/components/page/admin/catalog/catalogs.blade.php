c333492976224f1373bced03e2932a231ae29a65
David VanScott <david.vanscott@gmail.com>
2013-07-25 17:54:24 -0400

    Change the order the catalogs are displayed in

--- nova/src/nova/core/views/components/page/admin/catalog/catalogs.blade.php
+++ nova/src/nova/core/views/components/page/admin/catalog/catalogs.blade.php
@@ -2,26 +2,6 @@
 	<div class="col-sm-6 col-lg-6">
 		<div class="thumbnail">
 			<div class="btn-group pull-right">
-				<a class="btn btn-small btn-default icn-size-16" href="{{ URL::to('admin/catalog/modules') }}">{{ $_icons['forward'] }}</a>
-			</div>
-
-			<h4>{{ lang('Modules') }}</h4>
-		</div>
-	</div>
-
-	<div class="col-sm-6 col-lg-6">
-		<div class="thumbnail">
-			<div class="btn-group pull-right">
-				<a class="btn btn-small btn-default icn-size-16" href="{{ URL::to('admin/catalog/widgets') }}">{{ $_icons['forward'] }}</a>
-			</div>
-
-			<h4>{{ lang('Widgets') }}</h4>
-		</div>
-	</div>
-
-	<div class="col-sm-6 col-lg-6">
-		<div class="thumbnail">
-			<div class="btn-group pull-right">
 				<a class="btn btn-small btn-default icn-size-16" href="{{ URL::to('admin/catalog/skins') }}">{{ $_icons['forward'] }}</a>
 			</div>
 
@@ -36,6 +16,26 @@
 			</div>
 
 			<h4>{{ lang('Ranks') }}</h4>
+		</div>
+	</div>
+	
+	<div class="col-sm-6 col-lg-6">
+		<div class="thumbnail">
+			<div class="btn-group pull-right">
+				<a class="btn btn-small btn-default icn-size-16" href="{{ URL::to('admin/catalog/modules') }}">{{ $_icons['forward'] }}</a>
+			</div>
+
+			<h4>{{ lang('Modules') }}</h4>
+		</div>
+	</div>
+
+	<div class="col-sm-6 col-lg-6">
+		<div class="thumbnail">
+			<div class="btn-group pull-right">
+				<a class="btn btn-small btn-default icn-size-16" href="{{ URL::to('admin/catalog/widgets') }}">{{ $_icons['forward'] }}</a>
+			</div>
+
+			<h4>{{ lang('Widgets') }}</h4>
 		</div>
 	</div>
 </div>
