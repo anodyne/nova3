da4d6d016ddadf8a7fd687276b4d47b2cdc34fc0
David VanScott <david.vanscott@gmail.com>
2013-07-25 17:54:12 -0400

    Some tweaks for validation in the rank catalog

--- nova/src/nova/core/views/components/page/admin/catalog/ranks_action.blade.php
+++ nova/src/nova/core/views/components/page/admin/catalog/ranks_action.blade.php
@@ -7,57 +7,84 @@
 {{ Form::model($rank, ['url' => 'admin/catalog/ranks']) }}
 	<div class="row">
 		<div class="col-sm-6 col-lg-4">
-			<div class="control-group">
+			<div class="control-group{{ ($errors->has('name')) ? ' has-error' : '' }}">
 				<label class="control-label">{{ lang('Name') }}</label>
-				<div class="controls">{{ Form::text('name') }}</div>
+				<div class="controls">
+					{{ Form::text('name', null, ['class' => 'input-with-feedback']) }}
+				</div>
 			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12">
+			{{ $errors->first('name', '<p class="help-block text-danger">:message</p>') }}
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-sm-6 col-lg-4">
-			<label class="control-label">{{ lang('Location') }}</label>
-			<div class="controls">{{ Form::text('location') }}</div>
+			<div class="{{ ($errors->has('location')) ? 'control-group has-error' : '' }}">
+				<label class="control-label">{{ lang('Location') }}</label>
+				<div class="controls">
+					{{ Form::text('location', null, ['class' => 'input-with-feedback']) }}
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-lg-12">
+			{{ $errors->first('location', '<p class="help-block text-danger">:message</p>') }}
 			<p class="help-block">{{ lang('short.admin.catalog.ranks.location') }}</p>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-sm-6 col-lg-4">
-			<label class="control-label">{{ lang('short.admin.catalog.ranks.previewImage') }}</label>
-			<div class="controls">{{ Form::text('preview') }}</div>
+			<div class="{{ ($errors->has('preview')) ? 'control-group has-error' : '' }}">
+				<label class="control-label">{{ lang('short.admin.catalog.ranks.previewImage') }}</label>
+				<div class="controls">
+					{{ Form::text('preview', null, ['class' => 'input-with-feedback']) }}
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-lg-12">
+			{{ $errors->first('preview', '<p class="help-block text-danger">:message</p>') }}
 			<p class="help-block">{{ lang('short.admin.catalog.ranks.previewImageHelp') }}</p>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-sm-6 col-lg-4">
-			<label class="control-label">{{ lang('short.admin.catalog.ranks.blankImage') }}</label>
-			<div class="controls">{{ Form::text('blank') }}</div>
+			<div class="{{ ($errors->has('blank')) ? 'control-group has-error' : '' }}">
+				<label class="control-label">{{ lang('short.admin.catalog.ranks.blankImage') }}</label>
+				<div class="controls">
+					{{ Form::text('blank', null, ['class' => 'input-with-feedback']) }}
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-lg-12">
+			{{ $errors->first('blank', '<p class="help-block text-danger">:message</p>') }}
 			<p class="help-block">{{ lang('short.admin.catalog.ranks.blankImageHelp') }}</p>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-sm-4 col-lg-2">
-			<label class="control-label">{{ lang('Extension') }}</label>
-			<div class="controls">{{ Form::select('extension', ['.png' => 'PNG', '.jpg' => 'JPG', '.jpeg' => 'JPEG', '.gif' => 'GIF', '.bmp' => 'BMP']) }}</div>
+			<div class="{{ ($errors->has('extension')) ? 'control-group has-error' : '' }}">
+				<label class="control-label">{{ lang('Extension') }}</label>
+				<div class="controls">
+					{{ Form::select('extension', ['.png' => 'PNG', '.jpg' => 'JPG', '.jpeg' => 'JPEG', '.gif' => 'GIF', '.bmp' => 'BMP'], null, ['class' => 'input-with-feedback']) }}
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-lg-12">
+			{{ $errors->first('extension', '<p class="help-block text-danger">:message</p>') }}
 			<p class="help-block">{{ lang('short.admin.catalog.ranks.extensionHelp') }}</p>
 		</div>
 	</div>
@@ -85,13 +112,18 @@
 
 	<div class="row">
 		<div class="col-sm-4 col-lg-2">
-			<div class="control-group">
+			<div class="control-group{{ ($errors->has('status')) ? ' has-error' : '' }}">
 				<label class="control-label">{{ lang('Display') }}</label>
 				<div class="controls">
 					<label class="radio-inline">{{ Form::radio('status', Status::ACTIVE) }} {{ lang('Yes') }}</label>
 					<label class="radio-inline">{{ Form::radio('status', Status::INACTIVE) }} {{ lang('No') }}</label>
 				</div>
 			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12">
+			{{ $errors->first('status', '<p class="help-block text-danger">:message</p>') }}
 		</div>
 	</div>
 
