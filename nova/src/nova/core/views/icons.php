14212c616c695936ebc6036cd8e7236caaef2e4f
David VanScott <david.vanscott@gmail.com>
2013-08-07 20:47:28 -0400

    Update the icon font

--- nova/src/nova/core/views/icons.php
+++ nova/src/nova/core/views/icons.php
@@ -11,8 +11,10 @@ return [
 	'check' => '<span class="icn" data-icon="4"></span>',
 	'clock' => '<span class="icn" data-icon="h"></span>',
 	'close' => '<span class="icn" data-icon="x"></span>',
+	'closeSmall' => '<span class="icn" data-icon="="></span>',
 
 	'duplicate' => '<span class="icn" data-icon="_"></span>',
+	'refresh' => '<span class="icn" data-icon="9"></span>',
 	
 	'edit' => '<span class="icn" data-icon="p"></span>',
 	'email' => '<span class="icn" data-icon="e"></span>',
@@ -21,6 +23,10 @@ return [
 	'forward' => '<span class="icn" data-icon=">"></span>',
 	'favorite' => '<span class="icn" data-icon="&"></span>',
 	'favoriteHollow' => '<span class="icn" data-icon="7"></span>',
+	'faceDisappointed' => '<span class="icn" data-icon="."></span>',
+	'faceSad' => '<span class="icn" data-icon="["></span>',
+	'faceHappy' => '<span class="icn" data-icon="%"></span>',
+	'faceExcited' => '<span class="icn" data-icon="$"></span>',
 
 	'image' => '<span class="icn" data-icon="m"></span>',
 	'info' => '<span class="icn" data-icon="i"></span>',
@@ -38,6 +44,8 @@ return [
 
 	'remove' => '<span class="icn" data-icon="t"></span>',
 	'refresh' => '<span class="icn" data-icon="r"></span>',
+	'reject' => '<span class="icn" data-icon=":"></span>',
+	'rank' => '<span class="icn" data-icon="@"></span>',
 
 	'settings' => '<span class="icn" data-icon=")"></span>',
 	'share' => '<span class="icn" data-icon="a"></span>',
@@ -46,7 +54,9 @@ return [
 	'search' => '<span class="icn" data-icon="s"></span>',
 
 	'user' => '<span class="icn" data-icon="u"></span>',
+	'user' => '<span class="icn" data-icon=";"></span>',
 	'upload' => '<span class="icn" data-icon="^"></span>',
+	'refresh' => '<span class="icn" data-icon="/"></span>',
 
 	'view' => '<span class="icn" data-icon="0"></span>',
 
