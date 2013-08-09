22b360db3e8cfd255f2b2cfa4762bb51d671ecc2
David VanScott <david.vanscott@gmail.com>
2013-08-09 13:59:20 -0400

    Minor tweak for responsive design

--- nova/src/nova/core/views/components/page/admin/admin/index.blade.php
+++ nova/src/nova/core/views/components/page/admin/admin/index.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
 @foreach ($_icons as $key => $icon)
-	<div class="col-lg-2">
+	<div class="col-6 col-sm-4 col-lg-2">
 		{{ Form::button($icon, ['class' => 'btn btn-default btn-block icn-size-16']) }}
 		<p class="text-muted text-center">{{ $key }}</p>
 	</div>
