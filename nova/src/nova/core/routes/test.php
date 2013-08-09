f25ea47834362be9f6177d373eec7213f2877455
David VanScott <david.vanscott@gmail.com>
2013-08-09 13:59:26 -0400

    Update the test routes

--- nova/src/nova/core/routes/test.php
+++ nova/src/nova/core/routes/test.php
@@ -2,46 +2,8 @@
 
 Route::group(array('prefix' => 'test'), function()
 {
-	Route::get('index', function()
+	Route::get('/', function()
 	{
-		$skin = SkinCatalog::find(1);
-
-		$file = $skin->getQuickInstallFile('options.json');
-
-		$search = array_filter($file->items, function($f)
-		{
-			return $f->key == 'banner_image';
-		});
-
-		sd(reset($search));
-	});
-
-	Route::get('partial', function()
-	{
-		echo partial('common/label', ['class' => 'label-info', 'value' => lang('short.create', lang('form'))]);
-	});
-
-	Route::get('forms', function()
-	{
-		echo DynamicForm::setup('character', 1, false)->build();
-	});
-
-	Route::get('email', function()
-	{
-		$users = User::active()->get();
-
-		$final = $users->filter(function($u)
-		{
-			$emailPref = $u->getPreferenceItem('email_format');
-
-			return ($emailPref == 'html');
-		})->toSimpleArray('id', 'email');
-
-		sd($final);
-	});
-
-	Route::get('location', function()
-	{
-		sd(Location::page('admin/form/forms'));
+		sd(User::searchCharacters('daniela')->get()->toArray());
 	});
 });
