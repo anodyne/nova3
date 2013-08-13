491e46c06050d2b0a534ad4c00d64a4c834a0608
David VanScott <david.vanscott@gmail.com>
2013-08-13 13:56:31 -0400

    Dev routes

--- nova/src/nova/core/routes/dev.php
+++ nova/src/nova/core/routes/dev.php
@@ -4,9 +4,6 @@ Route::group(array('prefix' => 'dev'), function()
 {
 	Route::get('/', function()
 	{
-		$user = \User::find(1);
-		$user->updateUserPreferences(['is_sysadmin' => (int) false]);
-
-		s($user->getPreferenceItem('is_sysadmin'));
+		sd(User::searchCharacters('more')->get()->toArray());
 	});
 });
