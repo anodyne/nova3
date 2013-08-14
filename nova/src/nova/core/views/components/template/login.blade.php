240a896824045875d83b7392b4e6609d24b73cbd
David VanScott <david.vanscott@gmail.com>
2013-08-14 08:25:31 -0400

    Fix some BS3 issues with the main log in screen

--- nova/src/nova/core/views/components/template/login.blade.php
+++ nova/src/nova/core/views/components/template/login.blade.php
@@ -1,12 +1,14 @@
 <div class="navbar navbar-fixed-top">
 	<div class="container">
-		<a href="{{ URL::to('/') }}" class="navbar-brand">{{ $settings->sim_name }}</a>
+		<div class="navbar-header">
+			<a href="{{ URL::to('/') }}" class="navbar-brand">{{ $settings->sim_name }}</a>
+		</div>
 	</div>
 </div>
 
 <div class="container">
 	<div class="row">
-		<div class="col-lg-6 col-offset-3">
+		<div class="col-sm-8 col-lg-6 col-sm-offset-2 col-lg-offset-3">
 			<div class="page-header">
 				<h1>{{ $header }}</h1>
 			</div>
