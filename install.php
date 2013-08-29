9d0bf38f5f097ade6e795708ace0a5520cc5d4a3
David VanScott <david.vanscott@gmail.com>
2013-08-29 16:15:38 -0400

    Tweak the presentation of the environment tests

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<title>Nova 3 Environment Tests</title>
 
-		<link rel="stylesheet" href="nova/src/Nova/Assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="nova/assets/css/bootstrap.min.css">
 		<style>
 			@import "http://fonts.googleapis.com/css?family=Open+Sans:300,400,600";
 
@@ -15,6 +15,13 @@
 			}
 			.glyphicon {
 				font-size: 32px;
+			}
+			h1, h2, h3 {
+				font-family: "Open Sans";
+				font-weight: 300;
+			}
+			h2, h3, th, strong {
+				font-weight: 600;
 			}
 		</style>
 	</head>
@@ -35,9 +42,19 @@
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
+					<?php if (version_compare(PHP_VERSION, '5.4.0', '>=')): ?>
+						<tr>
+					<?php else: ?>
+						<tr class="danger">
+					<?php endif;?>
 						<td><p class="lead">PHP 5.4+</p></td>
-						<td>Your host must be running at least version 5.4.0 in order for Nova 3 to run. If this test fails, you won't be able to install Nova 3 and will need to contact your host to see about correcting this issue.</td>
+						<td>
+							<p>Nova 3 requires at least PHP 5.4.0 in order to run.</p>
+
+							<?php if (version_compare(PHP_VERSION, '5.4.0', '<')): ?>
+								<p><strong>Your server is currently running PHP <?php echo PHP_VERSION;?>. Please contact your host about options for upgrading your account to run PHP 5.4.0 or higher.</strong></p>
+							<?php endif;?>
+						</td>
 						<td class="text-center">
 							<?php if (version_compare(PHP_VERSION, '5.4.0', '>=')): ?>
 								<i class="glyphicon glyphicon-ok text-success"></i>
@@ -46,9 +63,20 @@
 							<?php endif;?>
 						</td>
 					</tr>
-					<tr>
+
+					<?php if (defined('PDO::ATTR_DRIVER_NAME')): ?>
+						<tr>
+					<?php else: ?>
+						<tr class="danger">
+					<?php endif;?>
 						<td><p class="lead">PDO</p></td>
-						<td>In order to connect to the database where Nova's data is stored, you must have the PDO driver installed. Most installations of PHP 5.4 come with this installed. If this test fails, you won't be able to install Nova 3 and will need to contact your host to see about correcting this issue.</td>
+						<td>
+							<p>Nova 3 requires a PDO driver to be installed. Most installations of PHP 5.4 come with this installed.</p>
+
+							<?php if ( ! defined('PDO::ATTR_DRIVER_NAME')): ?>
+								<p><strong>Without a PDO driver, Nova 3 will be unable to connect to your database. Please contact your host about getting a PDO driver (to match whatever type of database you have) installed for your account.</strong></p>
+							<?php endif;?>
+						</td>
 						<td class="text-center">
 							<?php if (defined('PDO::ATTR_DRIVER_NAME')): ?>
 								<i class="glyphicon glyphicon-ok text-success"></i>
@@ -57,9 +85,20 @@
 							<?php endif;?>
 						</td>
 					</tr>
-					<tr>
+					
+					<?php if (function_exists('mcrypt_module_open')): ?>
+						<tr>
+					<?php else: ?>
+						<tr class="danger">
+					<?php endif;?>
 						<td><p class="lead">MCrypt</p></td>
-						<td>For security purposes, passwords are hashed using PHP's MCrypt extension. Without this, you won't be able to properly hash passwords. If this test fails, you will need to contact your host to see about correcting the issue.</td>
+						<td>
+							<p>For security purposes, passwords are hashed using PHP's MCrypt extension. Without this, you won't be able to properly hash passwords.</p>
+
+							<?php if ( ! function_exists('mcrypt_module_open')): ?>
+								<p><strong>Please contact your host about getting the MCrypt extension activated for your account.</strong></p>
+							<?php endif;?>
+						</td>
 						<td class="text-center">
 							<?php if (function_exists('mcrypt_module_open')): ?>
 								<i class="glyphicon glyphicon-ok text-success"></i>
@@ -82,9 +121,13 @@
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
+					<?php if (extension_loaded('apc') and ini_get('apc.enabled')): ?>
+						<tr>
+					<?php else: ?>
+						<tr class="warning">
+					<?php endif;?>
 						<td><p class="lead">APC</p></td>
-						<td>In order to speed up the execution of some of Nova's pages, things are cached. While the cache will work without APC, you can speed up Nova by enabling APC caching if your server supports it.</td>
+						<td><p>In order to speed up the execution of many of Nova's pages, views and content from the database are cached. While the cache will work without APC, you can speed up Nova by enabling APC caching if your server supports it.</p></td>
 						<td class="text-center">
 							<?php if (extension_loaded('apc') and ini_get('apc.enabled')): ?>
 								<i class="glyphicon glyphicon-ok text-success"></i>
