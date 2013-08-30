55b10f85539f5c2ed2628d5a9cab388338a708aa
David VanScott <david.vanscott@gmail.com>
2013-08-30 08:28:43 -0400

    Fix a style issue with alerts

--- nova/views/components/partial/common/alert.blade.php
+++ nova/views/components/partial/common/alert.blade.php
@@ -1,4 +1,3 @@
 <div class="alert{{ (isset($class)) ? ' '.$class : ' alert-warning' }} fade in">
-	{{ (isset($heading)) ? '<h4>'.$heading.'</h4>' : '' }}
-	<p>{{ $content }}</p>
+	{{ $content }}
 </div>
