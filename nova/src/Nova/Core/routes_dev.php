c497be4e5df63505090f4660fd491ddc5d7d2ee7
David VanScott <david.vanscott@gmail.com>
2013-08-31 14:20:54 -0400

    Updated the dev routes

--- nova/src/Nova/Core/routes_dev.php
+++ nova/src/Nova/Core/routes_dev.php
@@ -4,6 +4,24 @@ Route::group(array('prefix' => 'dev'), function()
 {
 	Route::get('/', function()
 	{
-		sd(User::searchCharacters('more')->get()->toArray());
+		$form = NovaForm::key('character')->first();
+
+		// Get the form fields
+		$formFields[0] = lang('short.selectOne', langConcat('form field'));
+		$formFields+= $form->fields()->active()->orderAsc('label')->get()->toSimpleArray('id', 'label');
+
+		$tab = $form->tabs()->find(2);
+
+		$newField = $form->fields()->getModel()->newInstance([
+			'form_id' => 4,
+			'type' => 'text',
+			'label' => 'Foo',
+		])->save();
+
+		//s($form);
+		//s($formFields);
+		//s($tab);
+		//s($form->tabs->toSimpleArray('id', 'name'));
+		s($newField);
 	});
 });
