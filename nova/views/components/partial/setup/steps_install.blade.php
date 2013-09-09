63157ff9b71baad3aa388aaf5510b2cd9a4199c2
David VanScott <david.vanscott@gmail.com>
2013-09-09 16:16:34 -0400

    Fix the step indicator icon sizes

--- nova/views/components/partial/setup/steps_install.blade.php
+++ nova/views/components/partial/setup/steps_install.blade.php
@@ -1,14 +1,14 @@
 @if (Request::segment(3) === null)
-	<span class="icn icn32 step tip-below" data-title="Step 1" data-icon="1"></span>
-	<span class="icn icn32 step tip-below" data-title="Step 2" data-icon="1"></span>
+	<span class="icn icn-size-32 step tip-below" data-title="Step 1" data-icon="1"></span>
+	<span class="icn icn-size-32 step tip-below" data-title="Step 2" data-icon="1"></span>
 @endif
 
 @if (Request::segment(3) == 'settings')
-	<span class="icn icn32 step step-complete tip-below" data-title="Step 1" data-icon="2"></span>
-	<span class="icn icn32 step step-active tip-below" data-title="Step 2" data-icon="1"></span>
+	<span class="icn icn-size-32 step step-complete tip-below" data-title="Step 1" data-icon="2"></span>
+	<span class="icn icn-size-32 step step-active tip-below" data-title="Step 2" data-icon="1"></span>
 @endif
 
 @if (Request::segment(3) == 'finalize')
-	<span class="icn icn32 step step-complete tip-below" data-title="Step 1" data-icon="2"></span>
-	<span class="icn icn32 step step-complete tip-below" data-title="Step 2" data-icon="2"></span>
+	<span class="icn icn-size-32 step step-complete tip-below" data-title="Step 1" data-icon="2"></span>
+	<span class="icn icn-size-32 step step-complete tip-below" data-title="Step 2" data-icon="2"></span>
 @endif
