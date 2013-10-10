bb99ccd90d312b476203659dfda19681ff7f1559
David VanScott <david.vanscott@gmail.com>
2013-10-10 16:16:18 -0400

    Make sure that modules can use the lang features

--- nova/src/Nova/Extensions/Laravel/Translation/TranslationCascadingFileLoader.php
+++ nova/src/Nova/Extensions/Laravel/Translation/TranslationCascadingFileLoader.php
@@ -1,5 +1,6 @@
 <?php namespace Nova\Extensions\Laravel\Translation;
 
+use Cache;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Translation\LoaderInterface;
 
@@ -24,7 +25,7 @@ class TranslationCascadingFileLoader implements LoaderInterface {
 	 *
 	 * @var array
 	 */
-	protected $hints = array();
+	protected $hints = [];
 
 	/**
 	 * Create a new file loader instance.
@@ -37,58 +38,65 @@ class TranslationCascadingFileLoader implements LoaderInterface {
 		$this->path = $path;
 		$this->files = $files;
 
-		// We're going to set the paths to the different lang locations
-		// in the core so that we have more control over it and that people
-		// can't create problems by changing these paths.
-		$this->path = array(
-			'app'		=> APPPATH.'lang',
-			'core'		=> NOVAPATH.'lang',
-		);
+		// Set the path to the app lang directory
+		$this->path['app'] = APPPATH."lang";
+
+		// Get the modules out of the cache
+		$modules = Cache::get('nova.modules');
+
+		if (is_array($modules))
+		{
+			foreach ($modules as $module)
+			{
+				if ($this->files->isDirectory(APPPATH."src/Modules/{$module}/lang"))
+					$this->path[$module] = APPPATH."src/Modules/{$module}/lang";
+			}
+		}
+
+		// Set the path to the core lang directory
+		$this->path['app'] = NOVAPATH."lang";
 	}
 
 	/**
 	 * Load the messages for the given locale.
 	 *
-	 * @param  string  $locale
-	 * @return array
+	 * @param	string	$locale
+	 * @param	string	$group
+	 * @param	string	$namespace
+	 * @return	array
 	 */
 	public function load($locale, $group, $namespace = null)
 	{
 		if (is_null($namespace) or $namespace == '*')
-		{
 			return $this->loadPath($this->path, $locale, $group);
-		}
+		
 		else
-		{
 			return $this->loadNamespaced($locale, $group, $namespace);
-		}
 	}
 
 	/**
 	 * Load a namespaced translation group.
 	 *
-	 * @param  string  $locale
-	 * @param  string  $group
-	 * @param  string  $namespace
-	 * @return array
+	 * @param	string	$locale
+	 * @param	string	$group
+	 * @param	string	$namespace
+	 * @return	array
 	 */
 	protected function loadNamespaced($locale, $group, $namespace)
 	{
 		if (isset($this->hints[$namespace]))
-		{
 			return $this->loadPath($this->hints[$namespace], $locale, $group);
-		}
 
-		return array();
+		return [];
 	}
 
 	/**
 	 * Load a locale from a given path.
 	 *
-	 * @param  string  $path
-	 * @param  string  $locale
-	 * @param  string  $group
-	 * @return array
+	 * @param	string	$path
+	 * @param	string	$locale
+	 * @param	string	$group
+	 * @return	array
 	 */
 	protected function loadPath($path, $locale, $group)
 	{
@@ -98,9 +106,7 @@ class TranslationCascadingFileLoader implements LoaderInterface {
 			$file = "{$p}/{$locale}/{$group}.php";
 
 			if ($this->files->exists($file))
-			{
 				$items[$location] = $this->files->getRequire($file);
-			}
 		}
 
 		// Flip the array of items and recursively merge them
@@ -112,9 +118,9 @@ class TranslationCascadingFileLoader implements LoaderInterface {
 	/**
 	 * Add a new namespace to the loader.
 	 *
-	 * @param  string  $namespace
-	 * @param  string  $hint
-	 * @return void
+	 * @param	string	$namespace
+	 * @param	string	$hint
+	 * @return	void
 	 */
 	public function addNamespace($namespace, $hint)
 	{
