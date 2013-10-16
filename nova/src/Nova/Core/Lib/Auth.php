bd5155f17c83ba992344f79b51bf8f9ac828fc09
David VanScott <david.vanscott@gmail.com>
2013-10-15 21:31:46 -0400

    Update the auth interface and class

--- nova/src/Nova/Core/Lib/Auth.php
+++ nova/src/Nova/Core/Lib/Auth.php
@@ -1,5 +1,6 @@
 <?php namespace Nova\Core\Lib;
 
+use Date;
 use Setup;
 use Sentry;
 use Session;
@@ -12,11 +13,22 @@ class Auth implements NovaAuthInterface {
 		$this->installed = Setup::installed(false);
 	}
 
+	/**
+	 * Authenticate a user and remember them forever.
+	 *
+	 * @param	array	$credentials	Credentials to use for authenticating
+	 * @return	User
+	 */
 	public function authenticateAndRemember(array $credentials)
 	{
 		return Sentry::authenticateAndRemember($credentials);
 	}
 
+	/**
+	 * Check if a user is logged in.
+	 *
+	 * @return	bool
+	 */
 	public function check()
 	{
 		if (is_bool($this->installed) and $this->installed)
@@ -25,16 +37,49 @@ class Auth implements NovaAuthInterface {
 		return false;
 	}
 
+	/**
+	 * Get the cookie.
+	 *
+	 * @return	Cookie
+	 */
 	public function getCookie()
 	{
 		return Sentry::getCookie()->getCookie();
 	}
 
+	/**
+	 * Get the remaining time of the log in suspension.
+	 *
+	 * @param	string	$email	Email address to check
+	 * @return	string
+	 */
+	public function getSuspendedTimeRemaining($email)
+	{
+		// Get the throttle record
+		$throttle = $this->getThrottleProvider()->findByLogin($email);
+
+		// Get the suspended date into a usable format
+		$suspendedAt = Date::instance($throttle->suspended_at)
+			->addMinutes($throttle->getSuspensionTime());
+
+		return $suspendedAt->diffInMinutes(Date::now('UTC'));
+	}
+
+	/**
+	 * Get the throttle provider.
+	 *
+	 * @return	ThrottleProviderInterface
+	 */
 	public function getThrottleProvider()
 	{
 		return Sentry::getThrottleProvider();
 	}
 
+	/**
+	 * Get the current user.
+	 *
+	 * @return	User
+	 */
 	public function getUser()
 	{
 		if (is_bool($this->installed) and $this->installed)
@@ -43,11 +88,21 @@ class Auth implements NovaAuthInterface {
 		return false;
 	}
 
+	/**
+	 * Get the user provider.
+	 *
+	 * @return	UserProviderInterface
+	 */
 	public function getUserProvider()
 	{
 		return Sentry::getUserProvider();
 	}
 
+	/**
+	 * Log out.
+	 *
+	 * @return	void
+	 */
 	public function logout()
 	{
 		// Do the logout
