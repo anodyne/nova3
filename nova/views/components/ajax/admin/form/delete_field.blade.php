2e410f1dd4e644bac6656c38ba3d3618d15f06ae
David VanScott <david.vanscott@gmail.com>
2013-10-18 10:42:56 -0400

    Update the locations of the ajax view files

--- nova/views/components/ajax/admin/form/delete_field.blade.php
+++ nova/views/components/ajax/admin/form/delete_field.blade.php
@@ -1,7 +1,6 @@
 <p>{{ lang('short.deleteConfirm', lang('field'), $name) }}</p>
 
 {{ Form::open(['url' => 'admin/form/fields/'.$formKey]) }}
-	{{ Form::token() }}
 	{{ Form::hidden('id', $id) }}
 	{{ Form::hidden('formAction', 'delete') }}
 	{{ Form::button(lang('Action.delete'), ['type' => 'submit', 'class' => 'btn btn-danger']) }}
