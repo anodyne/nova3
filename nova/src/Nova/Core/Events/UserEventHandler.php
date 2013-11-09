3a53dcd397eb3459b68ef082a1d2c62f8ff59d56
David VanScott <david.vanscott@gmail.com>
2013-11-08 20:49:45 -0500

    Fix some issues with the user event handler

--- nova/src/Nova/Core/Events/UserEventHandler.php
+++ nova/src/Nova/Core/Events/UserEventHandler.php
@@ -2,7 +2,9 @@
 
 use URL;
 use Request;
+use FormModel;
 use UserModel;
+use FormDataModel;
 use UserMailer as Mailer;
 use SettingsRepositoryInterface;
 
@@ -19,6 +21,28 @@ class UserEventHandler extends \BaseEventHandler {
 
 	public function onUserCreated(UserModel $user, $input)
 	{
+		// Create user preferences for the user
+		$user->createUserPreferences();
+
+		/**
+		 * Fill the user rows for the dynamic form with blank data for editing later.
+		 */
+		$form = FormModel::key('user')->first();
+		
+		if ($form->fields->count() > 0)
+		{
+			foreach ($form->fields as $field)
+			{
+				FormDataModel::create([
+					'form_id' 	=> $form->id,
+					'field_id' 	=> $field->id,
+					'data_id' 	=> $user->id,
+					'value' 	=> '',
+				]);
+			}
+		}
+
+		// Send the email to the user
 		$this->mailer->created([
 			'to'		=> $user->email,
 			'content'	=> lang('email.content.user.create', 
@@ -29,6 +53,8 @@ class UserEventHandler extends \BaseEventHandler {
 							$input['password']),
 			'subject'	=> lang('email.subject.user.create'),
 		]);
+
+		// Create a system event
 	}
 
 	public function onUserResetPassword(UserModel $user)
