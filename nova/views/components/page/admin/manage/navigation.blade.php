a1485889002e88fc567d5b3f023013ddc30195a1
David VanScott <david.vanscott@gmail.com>
2013-11-09 17:30:57 -0500

    Site navigation management work

--- nova/views/components/page/admin/manage/navigation.blade.php
+++ nova/views/components/page/admin/manage/navigation.blade.php
@@ -52,6 +52,37 @@
 							</div>
 						</div>
 					</div>
+				@else
+					@foreach ($item as $i)
+						<div class="row">
+							<div class="col-xs-12 col-sm-10 col-lg-8">
+								<p><strong>{{ $i->name }}</strong></p>
+								<p class="text-muted text-small">{{ $i->url }}</p>
+							</div>
+							<div class="col-xs-12 col-sm-2 col-lg-4">
+								<div class="visible-lg">
+									<div class="btn-toolbar pull-right">
+										<div class="btn-group">
+											<a href="{{ URL::to('admin/navigation/'.$i->id) }}" class="btn btn-sm btn-default icn-size-16">{{ $_icons['edit'] }}</a>
+										</div>
+										<div class="btn-group">
+											<a href="#" class="btn btn-sm btn-danger icn-size-16 js-navigation-action" data-action="delete" data-id="{{ $i->id }}">{{ $_icons['remove'] }}</a>
+										</div>
+									</div>
+								</div>
+								<div class="hidden-lg">
+									<div class="row">
+										<div class="col-xs-12">
+											<p><a href="{{ URL::to('admin/navigation/'.$i->id) }}" class="btn btn-lg btn-block btn-default icn-size-16">{{ $_icons['edit'] }}</a></p>
+										</div>
+										<div class="col-xs-12">
+											<p><a href="#" class="btn btn-lg btn-block btn-danger icn-size-16 js-navigation-action" data-action="delete" data-id="{{ $i->id }}">{{ $_icons['remove'] }}</a></p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					@endforeach
 				@endif
 			@endforeach
 			</div>
