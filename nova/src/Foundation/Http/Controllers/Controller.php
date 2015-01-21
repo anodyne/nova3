42358ed3ee653be066f95aa08ed9ba355860e8a1
David VanScott <david.vanscott@gmail.com>
2015-01-21 08:09:47 -0500

    Base controller updates
    Pull in the app instance and set the page variable

--- nova/src/Foundation/Http/Controllers/Controller.php
+++ nova/src/Foundation/Http/Controllers/Controller.php
@@ -8,10 +8,15 @@ abstract class Controller extends IlluminateController {
 
 	use DispatchesCommands, ValidatesRequests;
 
+	protected $app;
+
 	public function __construct()
 	{
+		// Grab the application
+		$this->app = app();
+
 		// Let's bind some common stuff to every single view
-		view()->share('_page', false);
+		view()->share('_page', app('PageRepository')->getByRoute($this->app['request']->route()));
 		view()->share('_icons', false);
 		view()->share('_currentUser', app('nova.user'));
 	}
