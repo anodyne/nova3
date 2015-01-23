f0e4dcd54d3bf17626929e867a285c0cf9a143ed
David VanScott <david.vanscott@gmail.com>
2015-01-22 21:44:43 -0500

    Use the icon helper and config

--- nova/src/Setup/views/pages/setup/index.blade.php
+++ nova/src/Setup/views/pages/setup/index.blade.php
@@ -13,15 +13,15 @@
 		<div class="col-md-6">
 			<div class="thumbnail text-center">
 				<h1>Fresh Install</h1>
-				<div class="icn icn-size-xlg text-error" data-icon="d"></div>
-				<p><a href="{{ route('setup.install') }}" class="btn btn-primary btn-lg btn-block">Install {{ config('nova.app.name')." ".config('nova.app.version.full') }}</a></p>
+				<div>{!! icon($_icons['new'], 'xlg') !!}</div>
+				<p><a href="{{ route('setup.install') }}" class="btn btn-primary btn-lg btn-block">Install {{ config('nova.app.name') }}</a></p>
 			</div>
 		</div>
 
 		<div class="col-md-6">
 			<div class="thumbnail text-center">
 				<h1>Upgrade from Nova 2</h1>
-				<div class="icn icn-size-xlg text-error" data-icon="u"></div>
+				<div>{!! icon($_icons['update'], 'xlg') !!}</div>
 				<p><a href="{{ route('setup.config.email') }}" class="btn btn-primary btn-lg btn-block">Start Upgrade</a></p>
 			</div>
 		</div>
--- nova/src/Setup/views/partials/steps.blade.php
+++ nova/src/Setup/views/partials/steps.blade.php
@@ -23,24 +23,24 @@ if ($type == 'install')
 
 <ol>
 	@if ($type == 'install')
-		<li{!! $classes[1] !!}><span class="icn icn-size-sm" data-icon="1"></span>Database Connection</li>
-		<li{!! $classes[2] !!}><span class="icn icn-size-sm" data-icon="2"></span>Email Settings</li>
-		<li{!! $classes[3] !!}><span class="icn icn-size-sm" data-icon="3"></span>Install {{ config('nova.app.name') }}</li>
-		<li{!! $classes[4] !!}><span class="icn icn-size-sm" data-icon="4"></span>Create User</li>
-		<li{!! $classes[5] !!}><span class="icn icn-size-sm" data-icon="5"></span>Finalize</li>
+		<li{!! $classes[1] !!}>{!! icon($_icons['1']) !!}Database Connection</li>
+		<li{!! $classes[2] !!}>{!! icon($_icons['2']) !!}Email Settings</li>
+		<li{!! $classes[3] !!}>{!! icon($_icons['3']) !!}Install {{ config('nova.app.name') }}</li>
+		<li{!! $classes[4] !!}>{!! icon($_icons['4']) !!}Create User</li>
+		<li{!! $classes[5] !!}>{!! icon($_icons['5']) !!}Finalize</li>
 	@endif
 
 	@if ($type == 'update')
-		<li{!! $classes[1] !!}><span class="icn icn-size-sm" data-icon="1"></span>Backup</li>
-		<li{!! $classes[2] !!}><span class="icn icn-size-sm" data-icon="2"></span>Update {{ config('nova.app.name') }}</li>
-		<li{!! $classes[5] !!}><span class="icn icn-size-sm" data-icon="3"></span>Finalize</li>
+		<li{!! $classes[1] !!}>{!! icon($_icons['1']) !!}Backup</li>
+		<li{!! $classes[2] !!}>{!! icon($_icons['2']) !!}Update {{ config('nova.app.name') }}</li>
+		<li{!! $classes[5] !!}>{!! icon($_icons['3']) !!}Finalize</li>
 	@endif
 
 	@if ($type == 'migrate')
-		<li{!! $classes[1] !!}><span class="icn icn-size-sm" data-icon="1"></span>Database Connection</li>
-		<li{!! $classes[2] !!}><span class="icn icn-size-sm" data-icon="2"></span>Email Settings</li>
-		<li{!! $classes[3] !!}><span class="icn icn-size-sm" data-icon="3"></span>Install {{ config('nova.app.name') }}</li>
-		<li{!! $classes[4] !!}><span class="icn icn-size-sm" data-icon="4"></span>Create User</li>
-		<li{!! $classes[5] !!}><span class="icn icn-size-sm" data-icon="5"></span>Finalize</li>
+		<li{!! $classes[1] !!}>{!! icon($_icons['1']) !!}Database Connection</li>
+		<li{!! $classes[2] !!}>{!! icon($_icons['2']) !!}Email Settings</li>
+		<li{!! $classes[3] !!}>{!! icon($_icons['3']) !!}Install {{ config('nova.app.name') }}</li>
+		<li{!! $classes[4] !!}>{!! icon($_icons['4']) !!}Create User</li>
+		<li{!! $classes[5] !!}>{!! icon($_icons['5']) !!}Finalize</li>
 	@endif
 </ol>
