2f7c8f58a0661b460f0ded2e2356553723c56fe9
David VanScott <david.vanscott@gmail.com>
2015-01-22 21:43:42 -0500

    Add the icons config file

---
+++ nova/config/icons.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+	'setup' => [
+		'1' => '1',
+		'2' => '2',
+		'3' => '3',
+		'4' => '4',
+		'5' => '5',
+		'6' => '6',
+		'user' => 'a',
+		'checkmark' => 'k',
+		'checkmark-box' => '7',
+		'done' => 'd',
+		'info' => 'i',
+		'migrate' => 'e',
+		'new' => 'n',
+		'refresh' => 'r',
+		'update' => 'u',
+		'warning' => '!',
+	],
+
+];
