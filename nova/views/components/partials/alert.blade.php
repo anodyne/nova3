24101f458e0835858f42457c0b9c955eba652baf
David VanScott <david.vanscott@gmail.com>
2015-01-22 21:44:51 -0500

    Add headers to the alerts

--- nova/views/components/partials/alert.blade.php
+++ nova/views/components/partials/alert.blade.php
@@ -1,3 +1,7 @@
 <div class="alert alert-{{ $level }}">
+	@if ($header)
+		<h4>{{ $header }}</h4>
+	@endif
+
 	{!! $content !!}
 </div>
--- nova/views/components/partials/flash.blade.php
+++ nova/views/components/partials/flash.blade.php
@@ -1,6 +1,7 @@
 @if (Session::has('flash.message'))
 	<?php $level = Session::get('flash.level');?>
 	<?php $content = Session::get('flash.message');?>
+	<?php $header = Session::get('flash.header');?>
 @endif
 
-{!! alert($level, $content) !!}
+{!! alert($level, $content, $header) !!}
