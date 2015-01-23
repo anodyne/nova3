f63a70845dddce8c00d05eb9487bf83fbf03d84d
David VanScott <david.vanscott@gmail.com>
2015-01-22 22:01:54 -0500

    Use the locator in the welcome controller

--- nova/src/Foundation/Http/Controllers/WelcomeController.php
+++ nova/src/Foundation/Http/Controllers/WelcomeController.php
@@ -1,10 +1,12 @@
 <?php namespace Nova\Foundation\Http\Controllers;
 
+use Locate;
+
 class WelcomeController extends Controller {
 
 	public function index()
 	{
-		return view('nova.views.components.pages.welcome');
+		return view(Locate::page('welcome'));
 	}
 
 }
