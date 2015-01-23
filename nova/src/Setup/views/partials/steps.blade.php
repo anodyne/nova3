3185a616b32b7fc32314d4cc00e84a2d1417a191
David VanScott <david.vanscott@gmail.com>
2015-01-22 21:11:36 -0500

    Working on the setup steps partial

--- nova/src/Setup/views/partials/steps.blade.php
+++ nova/src/Setup/views/partials/steps.blade.php
@@ -23,10 +23,24 @@ if ($type == 'install')
 
 <ol>
 	@if ($type == 'install')
-		<li{!! $classes[1] !!}>Database Connection</li>
-		<li{!! $classes[2] !!}>Email Settings</li>
-		<li{!! $classes[3] !!}>Install Nova</li>
-		<li{!! $classes[4] !!}>Create User</li>
-		<li{!! $classes[5] !!}>Finalize</li>
+		<li{!! $classes[1] !!}><span class="icn icn-size-sm" data-icon="1"></span>Database Connection</li>
+		<li{!! $classes[2] !!}><span class="icn icn-size-sm" data-icon="2"></span>Email Settings</li>
+		<li{!! $classes[3] !!}><span class="icn icn-size-sm" data-icon="3"></span>Install {{ config('nova.app.name') }}</li>
+		<li{!! $classes[4] !!}><span class="icn icn-size-sm" data-icon="4"></span>Create User</li>
+		<li{!! $classes[5] !!}><span class="icn icn-size-sm" data-icon="5"></span>Finalize</li>
+	@endif
+
+	@if ($type == 'update')
+		<li{!! $classes[1] !!}><span class="icn icn-size-sm" data-icon="1"></span>Backup</li>
+		<li{!! $classes[2] !!}><span class="icn icn-size-sm" data-icon="2"></span>Update {{ config('nova.app.name') }}</li>
+		<li{!! $classes[5] !!}><span class="icn icn-size-sm" data-icon="3"></span>Finalize</li>
+	@endif
+
+	@if ($type == 'migrate')
+		<li{!! $classes[1] !!}><span class="icn icn-size-sm" data-icon="1"></span>Database Connection</li>
+		<li{!! $classes[2] !!}><span class="icn icn-size-sm" data-icon="2"></span>Email Settings</li>
+		<li{!! $classes[3] !!}><span class="icn icn-size-sm" data-icon="3"></span>Install {{ config('nova.app.name') }}</li>
+		<li{!! $classes[4] !!}><span class="icn icn-size-sm" data-icon="4"></span>Create User</li>
+		<li{!! $classes[5] !!}><span class="icn icn-size-sm" data-icon="5"></span>Finalize</li>
 	@endif
 </ol>
