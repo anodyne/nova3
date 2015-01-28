f32db47ab798e99bae6604b998805e6f34d21e68
David VanScott <david.vanscott@gmail.com>
2015-01-28 11:06:45 -0500

    Do the check for Nova being installed

--- nova/src/Foundation/Http/Middleware/Installed.php
+++ nova/src/Foundation/Http/Middleware/Installed.php
@@ -6,6 +6,11 @@ class Installed {
 
 	public function handle($request, Closure $next)
 	{
+		if ( ! app('cache')->get('nova.installed'))
+		{
+			return redirect()->route('setup.env');
+		}
+
 		return $next($request);
 	}
 
