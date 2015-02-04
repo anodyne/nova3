30a4e50ae0d4f35e4165edb4387343ef4db790d1
David VanScott <david.vanscott@gmail.com>
2015-02-04 16:20:05 -0500

    Add the login pages for install

--- nova/src/Setup/database/data/pages.php
+++ nova/src/Setup/database/data/pages.php
@@ -8,6 +8,28 @@ $pages = [
 		'default_resource'	=> "Nova\\Core\\Game\\Http\\Controllers\\HomeController@home",
 		'protected'			=> (int) true,
 	],
+
+	[
+		'verb'				=> "GET",
+		'name'				=> "login",
+		'uri'				=> "login",
+		'default_resource'	=> "Nova\\Core\\Login\\LoginController@index",
+		'protected'			=> (int) true,
+	],
+	[
+		'verb'				=> "POST",
+		'name'				=> "login.do",
+		'uri'				=> "login",
+		'default_resource'	=> "Nova\\Core\\Login\\LoginController@login",
+		'protected'			=> (int) true,
+	],
+	[
+		'verb'				=> "GET",
+		'name'				=> "logout",
+		'uri'				=> "logout",
+		'default_resource'	=> "Nova\\Core\\Login\\LoginController@logout",
+		'protected'			=> (int) true,
+	],
 ];
 
 $collections = [];
