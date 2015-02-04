ca97513ec83af496d9027d6aaf7d7e3a2b5c2fdc
David VanScott <david.vanscott@gmail.com>
2015-02-04 17:28:59 -0500

    RWD tweaks

--- nova/views/components/templates/login.blade.php
+++ nova/views/components/templates/login.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
 	<div class="row">
-		<div class="col-md-6 col-md-offset-3">
+		<div class="col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
 			{!! $content or '' !!}
 		</div>
 	</div>
