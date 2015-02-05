b4dafda140936a3c9de7e99607807607ef13c150
David VanScott <david.vanscott@gmail.com>
2015-02-05 16:19:34 -0500

    Work on the main template

--- nova/views/components/structures/main.blade.php
+++ nova/views/components/structures/main.blade.php
@@ -1,39 +1,21 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
-		<title>{{ $pageTitle }} &bull; {{ $siteName }}</title>
+		<meta charset="utf-8">
+		<meta name="author" content="Anodyne Productions">
+		<meta name="description" content="{{ $_page->description }}">
+		<meta name="viewport" content="width=device-width">
+
+		<title>{{ $_page->present()->title }} &bull; {{ $siteName }}</title>
 		
-		<link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
-		<link href="//fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700" rel="stylesheet">
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
-
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 400;
-				font-family: 'Open Sans';
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-				font-family: "Open Sans Condensed";
-				font-weight: 300;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-
-			.data-table{margin:15px 0}.data-table>.row{margin-left:0;margin-right:0;border-top:1px solid #ddd}.data-table>.row p{margin:10px 0}.data-table.data-table-striped>.row:nth-child(odd){background:#ededed}.data-table.data-table-bordered>.row{border-left:1px solid #ddd;border-right:1px solid #ddd}.data-table.data-table-bordered>.row:first-child{-webkit-border-radius:5px 5px 0 0;-moz-border-radius:5px 5px 0 0;border-radius:5px 5px 0 0}.data-table.data-table-bordered>.row:last-child{border-bottom:1px solid #ddd;-webkit-border-radius:0 0 5px 5px;-moz-border-radius:0 0 5px 5px;border-radius:0 0 5px 5px}.data-table.data-table-bordered>.row:nth-child(1):nth-last-child(1){border-bottom:1px solid #ddd;-webkit-border-radius:5px;-moz-border-radius:5px;border-radius:5px 5px 5px 5px}
-		</style>
+		{!! HTML::style('nova/views/design/css/base.style.css') !!}
 	</head>
 	<body>
 		{!! $template or '' !!}
+
+		<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		{!! $javascript or '' !!}
 	</body>
 </html>
--- nova/views/components/templates/main.blade.php
+++ nova/views/components/templates/main.blade.php
@@ -1,3 +1,32 @@
 <div class="container">
-	{!! $content or '' !!}
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="{{ route('home') }}" class="navbar-brand">{{ config('nova.app.name') }}</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="{{ route('login') }}">Log In</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<main>
+		@if ($_page->header())
+			<div class="page-header">
+				<h1>{!! $_page->present()->header !!}</h1>
+			</div>
+		@endif
+
+		@if ($_page->message())
+			{!! $_page->present()->message !!}
+		@endif
+
+		{!! $content or '' !!}
+	</main>
+
+	<footer>
+		{!! $footer or '' !!}
+	</footer>
 </div>
