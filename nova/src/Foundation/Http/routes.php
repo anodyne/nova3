11a95dfe1b61c2fc65c7969aff0f4615bfa6df3e
David VanScott <david.vanscott@gmail.com>
2015-02-05 16:21:36 -0500

    Update the routes

--- nova/src/Foundation/Http/routes.php
+++ nova/src/Foundation/Http/routes.php
@@ -7,12 +7,12 @@ if ($pages->count() > 0)
 {
 	foreach ($pages as $page)
 	{
-		$options['as'] = $page->name;
+		$options['as'] = $page->key;
 		$options['uses'] = ($page->resource) ?: $page->default_resource;
 
 		call_user_func_array([$router, strtolower($page->verb)], [$page->uri, $options]);
 	}
 }
 
-Route::get('admin/pages', 'Nova\Core\Pages\Http\Controllers\PageController@index');
-Route::get('admin/pages/{pageId}/edit', 'Nova\Core\Pages\Http\Controllers\PageController@edit');
+//Route::get('admin/pages', 'Nova\Core\Pages\Http\Controllers\PageController@index');
+//Route::get('admin/pages/{pageId}/edit', 'Nova\Core\Pages\Http\Controllers\PageController@edit');
