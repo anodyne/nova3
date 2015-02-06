ca18e10f17376631aac27b66e3ac75d8db7c01fb
David VanScott <david.vanscott@gmail.com>
2015-02-06 11:20:40 -0500

    Service provider work

--- nova/src/Foundation/Providers/NovaServiceProvider.php
+++ nova/src/Foundation/Providers/NovaServiceProvider.php
@@ -6,14 +6,15 @@ use ReflectionClass;
 use Illuminate\Support\ClassLoader,
 	Illuminate\Support\ServiceProvider;
 use League\CommonMark\CommonMarkConverter;
-use Nova\Core\Pages\Services\Compilers\PageCompiler;
+use Nova\Core\Pages\Services\Compilers\PageCompiler,
+	Nova\Core\Settings\Services\Compilers\SettingCompiler;
 use Nova\Foundation\Services\FlashNotifierService,
 	Nova\Foundation\Services\MarkdownParserService,
 	Nova\Foundation\Services\Locator\LocatorService,
 	Nova\Foundation\Services\Themes\Theme as BaseTheme,
 	Nova\Foundation\Services\Themes\MissingThemeImplementationException,
 	Nova\Foundation\Services\PageCompiler\CompilerEngine,
-	Nova\Foundation\Services\PageCompiler\Compilers\SettingCompiler;
+	Nova\Foundation\Services\PageCompiler\Compilers\IconCompiler;
 
 class NovaServiceProvider extends ServiceProvider {
 
@@ -54,33 +55,24 @@ class NovaServiceProvider extends ServiceProvider {
 	{
 		$this->app->singleton('nova.locator', function($app)
 		{
-			return new LocatorService($app['nova.user']);
-			//return new LocatorService($app['nova.user'], $app->make('SettingsRepository'));
+			return new LocatorService($app['nova.user'], $app['nova.settings']);
 		});
 	}
 
 	protected function createPageCompilerEngine()
 	{
-		$this->app->singleton('nova.page.compiler.page', function($app)
-		{
-			return new PageCompiler;
-		});
-
-		$this->app->singleton('nova.page.compiler.setting', function($app)
-		{
-			return new SettingCompiler;
-		});
-
 		$this->app->singleton('nova.page.compiler', function($app)
 		{
 			$engine = new CompilerEngine;
-			$engine->registerCompiler('setting', $app['nova.page.compiler.setting']);
-			$engine->registerCompiler('page', $app['nova.page.compiler.page']);
+
+			$engine->registerCompiler('setting', new SettingCompiler);
+			$engine->registerCompiler('page', new PageCompiler);
+			$engine->registerCompiler('icon', new IconCompiler);
 
 			return $engine;
 		});
 
-		// Add a page parser handler in a service provider
+		// Add a page compiler in a service provider
 		// $this->app['nova.page.compiler']->registerCompiler('foo', new Foo);
 	}
 
@@ -94,6 +86,16 @@ class NovaServiceProvider extends ServiceProvider {
 
 	protected function registerBindings()
 	{
+		$this->app->singleton('nova.settings', function($app)
+		{
+			if ($app['nova.setup']->isInstalled())
+			{
+				return $app['SettingRepository']->getAllSettings();
+			}
+
+			return false;
+		});
+
 		$this->app->bind('nova.character.creator', function($app)
 		{
 			return new CharacterCreator(
@@ -128,7 +130,7 @@ class NovaServiceProvider extends ServiceProvider {
 		$this->aliases = $this->app['config']['app.aliases'];
 
 		// Set the items being bound
-		$bindings = ['Character', 'Page', 'PageContent', 'System', 'User'];
+		$bindings = ['Character', 'Page', 'PageContent', 'Setting', 'System', 'User'];
 
 		foreach ($bindings as $binding)
 		{
