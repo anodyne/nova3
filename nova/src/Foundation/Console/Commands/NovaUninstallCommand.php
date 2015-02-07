f3ce98a0c5c829f503cc699e3207819dfd82c327
David VanScott <david.vanscott@gmail.com>
2015-02-06 20:37:08 -0500

    Add uninstall command

---
+++ nova/src/Foundation/Console/Commands/NovaUninstallCommand.php
@@ -0,0 +1,59 @@
+<?php namespace Nova\Foundation\Console\Commands;
+
+use File, Cache, Artisan;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class NovaUninstallCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'nova:uninstall';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Remove Nova entirely';
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$this->info("Clearing the cache...");
+		Cache::forget('nova.installed');
+		Cache::flush();
+
+		$this->info("Removing cached routes...");
+		Artisan::call('route:clear');
+
+		$this->info("Removing the database...");
+		Artisan::call('migrate:reset', ['--force' => true]);
+
+		$this->info("Removing generated config files...");
+		File::delete(app('path.config').'/app.php');
+		File::delete(app('path.config').'/database.php');
+		File::delete(app('path.config').'/mail.php');
+		File::delete(app('path.config').'/session.php');
+
+		// Remove the SQLite database if it's there
+		if (File::exists(config('database.connections.sqlite.database')))
+		{
+			$this->info("Removing SQLite database file...");
+			File::delete(config('database.connections.sqlite.database'));
+		}
+
+		$this->info("");
+		$this->info(config('nova.app.name').' has been uninstalled!');
+		$this->info("");
+	}
+
+}
