ba44429c464b3e1a495ecc74dffe649f0025176d
David VanScott <david.vanscott@gmail.com>
2015-02-21 17:04:07 -0500

    Update the page content

--- nova/src/Setup/database/data/pages_content.php
+++ nova/src/Setup/database/data/pages_content.php
@@ -1,42 +1,24 @@
 <?php
 
 return [
-	[
-		'page_id' => 1,
-		'type' => 'header',
-		'key' => 'home.header',
-		'value' => "Welcome to the {% setting:sim_name %}!"
-	],
-	[
-		'page_id' => 1,
-		'type' => 'title',
-		'key' => 'home.title',
-		'value' => "Home"
-	],
-	[
-		'page_id' => 1,
-		'type' => 'message',
-		'key' => 'home.message',
-		'value' => "This is my first message. And here is a {% page:home:link %} to the homepage using the new page compiler engine!"
-	],
+	['page_id' => 1, 'type' => 'header', 'key' => 'home.header', 'value' => "Welcome to the {% setting:sim_name %}!"],
+	['page_id' => 1, 'type' => 'title', 'key' => 'home.title', 'value' => "Home"],
+	['page_id' => 1, 'type' => 'message', 'key' => 'home.message', 'value' => "This is my first message. And here is a {% page:home:link %} to the homepage using the new page compiler engine!"],
 
-	[
-		'page_id' => 9,
-		'type' => 'header',
-		'key' => 'admin.pages.header',
-		'value' => "Page Manager"
-	],
-	[
-		'page_id' => 9,
-		'type' => 'title',
-		'key' => 'admin.pages.title',
-		'value' => "Page Manager"
-	],
+	['page_id' => 2, 'type' => 'header', 'key' => 'login.header', 'value' => "Log In"],
+	['page_id' => 2, 'type' => 'title', 'key' => 'login.title', 'value' => "Log In"],
 
-	[
-		'page_id' => 10,
-		'type' => 'title',
-		'key' => 'admin.pages.edit.title',
-		'value' => "Edit Page"
-	],
+	['page_id' => 3, 'type' => 'header', 'key' => 'logout.header', 'value' => "Logout"],
+	['page_id' => 3, 'type' => 'title', 'key' => 'logout.title', 'value' => "Logout"],
+
+	['page_id' => 4, 'type' => 'header', 'key' => 'password.remind.header', 'value' => "Send Password Reminder"],
+	['page_id' => 4, 'type' => 'title', 'key' => 'password.remind.title', 'value' => "Send Password Reminder"],
+
+	['page_id' => 6, 'type' => 'header', 'key' => 'password.reset.header', 'value' => "Reset Password"],
+	['page_id' => 6, 'type' => 'title', 'key' => 'password.reset.title', 'value' => "Reset Password"],
+
+	['page_id' => 8, 'type' => 'header', 'key' => 'admin.pages.header', 'value' => "Page Manager"],
+	['page_id' => 8, 'type' => 'title', 'key' => 'admin.pages.title', 'value' => "Page Manager"],
+
+	['page_id' => 9, 'type' => 'title', 'key' => 'admin.pages.edit.title', 'value' => "Edit Page"],
 ];
