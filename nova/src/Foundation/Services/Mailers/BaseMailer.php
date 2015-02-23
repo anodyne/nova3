6454423e11c064b4d3915c1ca641a52fbea3202e
David VanScott <david.vanscott@gmail.com>
2015-02-22 23:37:37 -0500

    Add the base mailer

---
+++ nova/src/Foundation/Services/Mailers/BaseMailer.php
@@ -0,0 +1,56 @@
+<?php namespace Nova\Foundation\Services\Mailers;
+
+use Illuminate\Contracts\Mail\Mailer;
+use Nova\Foundation\Services\Locator\LocatorInterface;
+
+class BaseMailer {
+
+	protected $mailer;
+	protected $locator;
+
+	public function __construct(Mailer $mailer, LocatorInterface $locator)
+	{
+		$this->mailer = $mailer;
+		$this->locator = $locator;
+	}
+
+	final public function raw($text, array $payload)
+	{
+		// Build the message callback
+		$message = $this->buildMessage($payload);
+
+		// Pass the email off to the queue
+		return $this->mailer->raw($text, $message);
+	}
+
+	final public function send($view, array $payload)
+	{
+		// Build the message callback
+		$message = $this->buildMessage($payload);
+
+		// Pass the email off to the queue
+		return $this->mailer->queue($this->locator->email($view), $payload, $message);
+	}
+
+	protected function buildMessage(array $payload)
+	{
+		return function($message) use ($payload)
+		{
+			$calls = ['to', 'cc', 'bcc', 'from', 'replyTo'];
+
+			foreach ($calls as $call)
+			{
+				if (array_key_exists($call, $payload))
+				{
+					call_user_func_array([$message, $call], [$payload[$call]]);
+				}
+			}
+
+			if (array_key_exists('subject', $payload))
+			{
+				$message->subject("[] {$payload['subject']}");
+			}
+		};
+	}
+
+}
