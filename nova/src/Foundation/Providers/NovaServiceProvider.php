06fae7d31209d1d8cdc7650ad191b78ef5a646db
David VanScott <david.vanscott@gmail.com>
2015-03-10 19:49:28 -0400

    Setup the mailer
    Also, let’s add a bunch of comments while we’re at it to give a better
    idea of what’s happening and why.

--- nova/src/Foundation/Providers/NovaServiceProvider.php
+++ nova/src/Foundation/Providers/NovaServiceProvider.php
@@ -36,6 +36,7 @@ class NovaServiceProvider extends ServiceProvider {
 		if ($this->app['nova.setup']->isInstalled())
 		{
 			$this->setupTheme();
+			$this->setupMailer();
 		}
 	}
 
@@ -58,6 +59,12 @@ class NovaServiceProvider extends ServiceProvider {
 		}
 	}
 
+	/**
+	 * Build the locator that's at the heart-and-soul of seamless substitution.
+	 * Developers can add additional locations to search by calling the
+	 * registerSearchPath method on the nova.locator class in the IOC. Adding
+	 * a search path will insert it in the center of the search stack.
+	 */
 	protected function createLocator()
 	{
 		$this->app->bind(
@@ -72,6 +79,12 @@ class NovaServiceProvider extends ServiceProvider {
 		//$this->app['nova.locator']->registerSearchPath('extensions/Anodyne/Awards/views');
 	}
 
+	/**
+	 * Build the page compiler engine and add some of the compilers we want to
+	 * have out of the box. Developers can add additional compilers from their
+	 * service providers by calling the registerCompiler method on the
+	 * nova.page.compiler class in the IOC.
+	 */
 	protected function createPageCompilerEngine()
 	{
 		$this->app->singleton('nova.page.compiler', function($app)
@@ -89,6 +102,12 @@ class NovaServiceProvider extends ServiceProvider {
 		// $this->app['nova.page.compiler']->registerCompiler('foo', new Foo);
 	}
 
+	/**
+	 * Get the current user out of the Auth class so we have quick and easy
+	 * access to it instead of having to rely on calling Auth::user every time
+	 * we want the current user. This also has the added benefit of being far
+	 * more flexible in the event we change how the user is handled.
+	 */
 	protected function getCurrentUser()
 	{
 		$this->app->singleton('nova.user', function($app)
@@ -97,6 +116,11 @@ class NovaServiceProvider extends ServiceProvider {
 		});
 	}
 
+	/**
+	 * This registers all of the bindings with the IOC that Nova uses all over
+	 * the place. Developers can easily override any of these in their own
+	 * service providers.
+	 */
 	protected function registerBindings()
 	{
 		$this->app->singleton('nova.settings', function($app)
@@ -134,6 +158,11 @@ class NovaServiceProvider extends ServiceProvider {
 		});
 	}
 
+	/**
+	 * Bind all of the repository interfaces to their appropriate concrete
+	 * class and make sure we alias them at the same time. This allows for
+	 * quick and easy use of the repositories out of the IOC.
+	 */
 	protected function setRepositoryBindings()
 	{
 		// Build a list of repositories that should be built
@@ -147,6 +176,24 @@ class NovaServiceProvider extends ServiceProvider {
 		}
 	}
 
+	/**
+	 * Set the mailer's default from address and name to the values stored in
+	 * the settings table. This allows admins to change the defaults without
+	 * having to edit any PHP files.
+	 */
+	protected function setupMailer()
+	{
+		config(['mail.from.address' => $this->app['nova.settings']->mail_default_address]);
+		config(['mail.from.name' => $this->app['nova.settings']->mail_default_name]);
+	}
+
+	/**
+	 * Setup the theme class that will handle all of the rendering of the theme.
+	 * In some cases, the theme could have its own class to override what the
+	 * core is doing, and if it does, we need to make sure it implements the
+	 * necessary interfaces. Otherwise, we'll just fallback to the base theme
+	 * class in the core.
+	 */
 	protected function setupTheme()
 	{
 		// Get the theme name
