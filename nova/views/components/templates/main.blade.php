43f8d1228a42f0c2cc2b719f5e6a3ff53102c4c0
David VanScott <david.vanscott@gmail.com>
2015-03-11 19:52:28 -0400

    Update the main structure and template

--- nova/views/components/templates/main.blade.php
+++ nova/views/components/templates/main.blade.php
@@ -1,55 +1,19 @@
 <div class="container">
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a href="{{ route('home') }}" class="navbar-brand">{{ $siteName }}</a>
-			</div>
-			<div id="navbar" class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ route('setup.home') }}">Setup Center</a></li>
-
-					@if (Auth::check())
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin <span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="{{ route('admin.pages') }}">Page Manager</a></li>
-								<li><a href="#">Additional Page Contents</a></li>
-								<li><a href="#">Menus</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::check())
-						<li><a href="{{ route('logout') }}">Log Out</a></li>
-					@else
-						<li><a href="{{ route('login') }}">Log In</a></li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
+	{!! $nav or '' !!}
 
 	<main>
 		@if (Session::has('flash.level'))
 			{!! flash() !!}
 		@endif
 		
-		@if ($header)
+		@if ($_page->header())
 			<div class="page-header">
-				<h1>{!! $header !!}</h1>
+				<h1>{!! $_page->present()->header !!}</h1>
 			</div>
 		@endif
 
-		@if ($message)
-			{!! $message !!}
+		@if ($_page->message())
+			{!! $_page->present()->message !!}
 		@endif
 
 		{!! $content or '' !!}
