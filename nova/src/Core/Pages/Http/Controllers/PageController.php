7562740220f5c95b85ee554289205775edee4e80
David VanScott <david.vanscott@gmail.com>
2015-03-14 13:41:54 -0400

    Working on the page controller

--- nova/src/Core/Pages/Http/Controllers/PageController.php
+++ nova/src/Core/Pages/Http/Controllers/PageController.php
@@ -24,6 +24,33 @@ class PageController extends BaseController {
 		$this->data->pages = $this->repo->all();
 	}
 
+	public function create()
+	{
+		$this->view = 'admin/pages/create';
+		$this->jsView = 'admin/pages/create_js';
+
+		$this->data->httpVerbs = [
+			'GET' => 'GET',
+			'POST' => 'POST',
+			'PUT' => 'PUT',
+			'DELETE' => 'DELETE',
+		];
+	}
+
+	public function store()
+	{
+		// Create the page
+		$page = $this->repo->create(Input::all());
+
+		// Fire the event
+		event('nova.page.created', [$page]);
+
+		// Set the flash message
+		Flash::success("Page has been created.");
+
+		return redirect()->route('admin.pages');
+	}
+
 	public function edit($pageId)
 	{
 		$this->view = 'admin/pages/edit';
@@ -35,6 +62,25 @@ class PageController extends BaseController {
 			'PUT' => 'PUT',
 			'DELETE' => 'DELETE',
 		];
+	}
+
+	public function update($pageId)
+	{
+		# code...
+	}
+
+	public function checkPageKey()
+	{
+		$this->isAjax = true;
+
+		$count = $this->repo->countRouteKeys(Input::get('key'));
+
+		if ($count > 0)
+		{
+			return json_encode(['code' => 0]);
+		}
+
+		return json_encode(['code' => 1]);
 	}
 
 }
