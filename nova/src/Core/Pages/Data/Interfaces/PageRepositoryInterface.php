730e3be0b73566530c79addc1235b1e2063f2400
David VanScott <david.vanscott@gmail.com>
2015-03-14 13:41:13 -0400

    Work on the page repo and interface

--- nova/src/Core/Pages/Data/Interfaces/PageRepositoryInterface.php
+++ nova/src/Core/Pages/Data/Interfaces/PageRepositoryInterface.php
@@ -5,6 +5,7 @@ use Nova\Foundation\Data\Interfaces\BaseRepositoryInterface;
 
 interface PageRepositoryInterface extends BaseRepositoryInterface {
 
+	public function countRouteKeys($key);
 	public function create(array $data);
 	public function getByRouteKey(Route $route);
 	public function getByRouteUri(Route $route);
--- nova/src/Core/Pages/Data/Repositories/PageRepository.php
+++ nova/src/Core/Pages/Data/Repositories/PageRepository.php
@@ -14,9 +14,40 @@ class PageRepository extends BaseRepository implements PageRepositoryInterface {
 		$this->model = $model;
 	}
 
+	public function countRouteKeys($key)
+	{
+		$keys = $this->countBy('key', $key);
+
+		return (int) $keys;
+	}
+
 	public function create(array $data)
 	{
-		return $this->model->create($data);
+		$page = $this->model->create($data[$data['type']]);
+
+		if (array_key_exists('content', $data))
+		{
+			// Grab the content repo out of the IOC
+			$contentRepo = app('PageContentRepository');
+
+			foreach ($data['content'] as $type => $value)
+			{
+				// Build the content data
+				$content = [
+					'type'	=> $type,
+					'key'	=> "{$page->key}.{$type}",
+					'value'	=> $value,
+				];
+
+				// Create the content item
+				$contentItem = $contentRepo->create($content);
+
+				// Attach the content record to the page
+				$page->pageContents()->save($contentItem);
+			}
+		}
+
+		return $page;
 	}
 
 	public function getByRouteKey(Route $route)
