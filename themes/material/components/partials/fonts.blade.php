477bdfa5002567659d3eeda27405b5ff9b9540e9
David VanScott <david.vanscott@gmail.com>
2015-03-18 13:02:40 -0400

    Add MD theme

---
+++ themes/material/components/partials/fonts.blade.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,400,400italic,500,500italic,700,700italic">
+<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300italic,400,400italic,700,700italic">
+<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto+Slab:300,400,700">
