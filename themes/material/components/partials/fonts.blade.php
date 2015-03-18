b560936dfe71c09608371418ddea585d048a9bd6
David VanScott <david.vanscott@gmail.com>
2015-03-18 13:53:11 -0400

    Playing around with the material theme

--- themes/material/components/partials/fonts.blade.php
+++ themes/material/components/partials/fonts.blade.php
@@ -1,3 +1,2 @@
 <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,400,400italic,500,500italic,700,700italic">
 <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300italic,400,400italic,700,700italic">
-<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto+Slab:300,400,700">
