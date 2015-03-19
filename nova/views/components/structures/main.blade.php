2340d6ed6ffaa4f2c87d24e63e772e839e9dc81a
David VanScott <david.vanscott@gmail.com>
2015-03-19 15:47:25 -0400

    Do some JS work in the main structure

--- nova/views/components/structures/main.blade.php
+++ nova/views/components/structures/main.blade.php
@@ -48,10 +48,25 @@
 		<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
 		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 		<script>
+			// Setup the CSRF token on Ajax requests
 			$.ajaxSetup({
 				headers: {
 					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 				}
+			});
+
+			// Destroy all modals when they're hidden
+			$('.modal').on('hidden.bs.modal', function()
+			{
+				$('.modal').removeData('bs.modal');
+			});
+
+			$(function()
+			{
+				$('.js-tooltip-top').tooltip({ placement: 'top' });
+				$('.js-tooltip-bottom').tooltip({ placement: 'bottom' });
+				$('.js-tooltip-left').tooltip({ placement: 'left' });
+				$('.js-tooltip-right').tooltip({ placement: 'right' });
 			});
 		</script>
 		{!! $javascript or '' !!}
