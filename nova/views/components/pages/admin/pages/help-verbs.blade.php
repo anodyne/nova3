48bcd36cd0fa203ba746bfb96e3303bf66637002
David VanScott <david.vanscott@gmail.com>
2015-03-20 11:45:03 -0400

    Add a help modal for HTTP verbs

---
+++ nova/views/components/pages/admin/pages/help-verbs.blade.php
@@ -0,0 +1,15 @@
+<p>HTTP defines methods (often referred to as <em>verbs</em>) to indicate the desired action to be performed on a specific resource.</p>
+
+<dl>
+	<dt>GET</dt>
+	<dd>Requests a representation of the specified resource. Requests using GET should only retrieve data and should have no other effect.</dd>
+
+	<dt>POST</dt>
+	<dd>Requests that the server accept the entity enclosed in the request as a new resource. This is most often used for creating new data on the server or in the database.</dd>
+
+	<dt>PUT</dt>
+	<dd>If the URI refers to an already existing resource, it's modified with the data passed through the request. This is most often used for updating existing pieces of data.</dd>
+
+	<dt>DELETE</dt>
+	<dd>Deletes the specified resource.</dd>
+</dl>
