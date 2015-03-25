f95df164410673c645dd92a54d5b76b145e27fff
David VanScott <david.vanscott@gmail.com>
2015-03-24 21:09:06 -0400

    Cleanup some files

--- nova/views/components/js/admin/pages/pages-js.blade.php
+++ nova/views/components/js/admin/pages/pages-js.blade.php
@@ -1,5 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.js"></script>
-{!! HTML::script('nova/assets/js/app/pages.js') !!}
+{!! HTML::script('nova/assets/js/angular/pages.js') !!}
 
 <script>
 	var baseUrl = "{{ Request::root() }}";
