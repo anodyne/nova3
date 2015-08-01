1b53239f7e0105384962bc6d5d20e1200424230f
David VanScott <david.vanscott@gmail.com>
2015-08-01 09:49:36 -0400

    Update the API routes

--- nova/src/Foundation/Http/api.php
+++ nova/src/Foundation/Http/api.php
@@ -7,4 +7,7 @@ Route::group(['prefix' => 'api'], function()
 
 	Route::get('page-contents', 'Nova\Core\Pages\Http\Api\PageContentController@index');
 	Route::get('page-contents/{contentId}', 'Nova\Core\Pages\Http\Api\PageContentController@show');
+
+	Route::get('access/permissions', 'Nova\Core\Access\Http\Api\PermissionController@index');
+	Route::get('access/permissions/{permissionId}', 'Nova\Core\Access\Http\Api\PermissionController@show');
 });
