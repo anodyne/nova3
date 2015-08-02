adb28d59275c7d8374fc57d98604a89761ca648f
David VanScott <david.vanscott@gmail.com>
2015-08-02 16:44:29 -0400

    Add a first name method to the user presenter

--- nova/src/Core/Users/Data/Presenters/UserPresenter.php
+++ nova/src/Core/Users/Data/Presenters/UserPresenter.php
@@ -4,6 +4,13 @@ use Laracasts\Presenter\Presenter;
 
 class UserPresenter extends Presenter {
 
+	public function firstName()
+	{
+		if ($this->entity->nickname) return $this->entity->nickname;
+
+		return explode(' ', $this->entity->name)[0];
+	}
+
 	public function name()
 	{
 		if ($this->entity->nickname) return $this->entity->nickname;
