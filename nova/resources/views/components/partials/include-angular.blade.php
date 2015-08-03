0eb5ef069760ce6f21210c5517518671e5e0307c
David VanScott <david.vanscott@gmail.com>
2015-08-03 16:06:16 -0400

    Update the Angular include

--- nova/resources/views/components/partials/include-angular.blade.php
+++ nova/resources/views/components/partials/include-angular.blade.php
@@ -1,1 +1,3 @@
 <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.3/angular.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.3/angular-sanitize.js"></script>
+{!! HTML::script('nova/resources/js/angular/angular-toArrayFilter.js') !!}
