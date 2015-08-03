5396f6af471a896a12c8b2cf2d4450a7179a6cba
David VanScott <david.vanscott@gmail.com>
2015-08-03 16:04:31 -0400

    Clean up the rolePermissions Angular controller

--- nova/resources/js/angular/rolePermissions.js
+++ nova/resources/js/angular/rolePermissions.js
@@ -1,23 +1,8 @@
-var app = angular.module('app', []).config(function($interpolateProvider)
+var app = angular.module('app', ['ngSanitize', 'angular-toArrayFilter']).config(function($interpolateProvider)
 {
 	// Change the start and end symbols so we don't clash with Blade
 	$interpolateProvider.startSymbol('{%').endSymbol('%}');
 });
-
-var uniqueItems = function (data, key)
-{
-	var result = [];
-
-	for (var i = 0; i < data.length; i++)
-	{
-		var value = data[i][key];
-
-		if (result.indexOf(value) == -1)
-			result.push(value);
-	}
-
-	return result;
-}
 
 app.controller('PermissionsLoadingController', function ($scope)
 {
@@ -57,16 +42,6 @@ app.controller('PermissionsController', function ($scope, $http, $window, filter
 			}
 		}, function (value)
 		{
-			var selected;
-
-			$scope.count = function (prop, value)
-			{
-				return function (el)
-				{
-					return el[prop] == value;
-				};
-			};
-
 			// We're done loading the page now
 			$scope.$emit('unload');
 		}, true);
