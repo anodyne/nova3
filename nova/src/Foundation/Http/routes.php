1366beb07a4e2136231e4c2e7b60f6bb3aff63bb
David VanScott <david.vanscott@gmail.com>
2015-10-08 23:56:21 -0400

    Only allow valid HTTP verbs

--- nova/src/Foundation/Http/routes.php
+++ nova/src/Foundation/Http/routes.php
@@ -5,11 +5,22 @@ $pages = app('PageRepository')->all();
 
 if ($pages->count() > 0)
 {
+	// Allowed verbs
+	$allowedVerbs = ['get', 'post', 'put', 'delete'];
+
 	foreach ($pages as $page)
 	{
-		$options['as'] = $page->key;
-		$options['uses'] = ($page->resource) ?: $page->default_resource;
+		// Get the verb and format it
+		$verb = strtolower($page->verb);
 
-		call_user_func_array([$router, strtolower($page->verb)], [$page->uri, $options]);
+		// Make sure the page is using one of the allowed verbs
+		if (in_array($verb, $allowedVerbs))
+		{
+			// Build the options
+			$options['as'] = $page->key;
+			$options['uses'] = ($page->resource) ?: $page->default_resource;
+
+			call_user_func_array([$router, $verb], [$page->uri, $options]);
+		}
 	}
 }
