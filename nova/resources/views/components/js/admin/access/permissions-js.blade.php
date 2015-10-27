e62f3a19c6ef83841c80cab610efa599395c7c6b
David VanScott <david.vanscott@gmail.com>
2015-10-27 14:54:24 -0400

    Convert the permissions manager to use Vue

--- nova/resources/views/components/js/admin/access/permissions-js.blade.php
+++ nova/resources/views/components/js/admin/access/permissions-js.blade.php
@@ -1,8 +1,38 @@
-{!! partial('include-angular') !!}
-{!! HTML::script('nova/resources/js/angular/rolePermissions.js') !!}
-
 <script>
-	var baseUrl = "{{ Request::root() }}";
+	var vm = new Vue({
+		el: "#app",
+		data: {
+			loading: true,
+			loadingWithError: false,
+			baseUrl: "{{ Request::root() }}",
+			display_name: "",
+			name: "",
+			permissions: []
+		},
+		methods: {
+			resetFilters: function()
+			{
+				this.$set("display_name", "")
+				this.$set("name", "")
+			}
+		},
+		ready: function()
+		{
+			this.$http.get(this.baseUrl + '/api/access/permissions', function (data, status, request)
+			{
+				this.$set('permissions', data.data)
+			}).error(function (data, status, request)
+			{
+				this.$set('loadingWithError', true)
+			})
+		}
+	});
+
+	vm.$watch('permissions', function (newValue, oldValue)
+	{
+		if (newValue.length > 0)
+			this.$set('loading', false)
+	})
 
 	$(document).on('click', '.js-permissionAction', function(e)
 	{
--- nova/resources/views/components/pages/admin/access/permissions.blade.php
+++ nova/resources/views/components/pages/admin/access/permissions.blade.php
@@ -1,99 +1,102 @@
-<div ng-controller="PermissionsLoadingController">
-	<div ng-show="loading">
+<div v-show="loading">
+	<div v-show="!loadingWithError">
 		<h4 class="text-center">{!! HTML::image('nova/resources/images/ajax-loader.gif') !!}</h4>
 	</div>
+	<div v-else>
+		{!! alert('danger', "There was an error retrieving your permissions from the database. This can be caused by a wrong URL or an issue with the database. Please try again.", "Error!") !!}
+	</div>
+</div>
 
-	<div ng-cloak>
-		<div class="visible-xs visible-sm">
+<div v-else>
+	<phone-tablet>
+		@if ($_user->can('access.create'))
+			<p><a href="{{ route('admin.access.permissions.create') }}" class="btn btn-success btn-lg btn-block">Add a Permission</a></p>
+		@endif
+
+		<p><a href="{{ route('admin.access.roles') }}" class="btn btn-default btn-lg btn-block">Manage Roles</a></p>
+	</phone-tablet>
+	<desktop>
+		<div class="btn-toolbar">
 			@if ($_user->can('access.create'))
-				<p><a href="{{ route('admin.access.permissions.create') }}" class="btn btn-success btn-lg btn-block">Add a Permission</a></p>
+				<div class="btn-group">
+					<a href="{{ route('admin.access.permissions.create') }}" class="btn btn-success">Add a Permission</a>
+				</div>
 			@endif
 
-			<p><a href="{{ route('admin.access.roles') }}" class="btn btn-default btn-lg btn-block">Manage Roles</a></p>
+			<div class="btn-group">
+				<a href="{{ route('admin.access.roles') }}" class="btn btn-default">Manage Roles</a>
+			</div>
 		</div>
-		<div class="visible-md visible-lg">
-			<div class="btn-toolbar">
-				@if ($_user->can('access.create'))
-					<div class="btn-group">
-						<a href="{{ route('admin.access.permissions.create') }}" class="btn btn-success">Add a Permission</a>
-					</div>
-				@endif
+	</desktop>
 
-				<div class="btn-group">
-					<a href="{{ route('admin.access.roles') }}" class="btn btn-default">Manage Roles</a>
+	<div class="row">
+		<div class="col-md-3 col-md-push-9">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Filter Permissions</h3>
+				</div>
+				<div class="panel-body">
+					<div class="form-group">
+						<label class="control-label">By Name</label>
+						{!! Form::text('searchName', null, ['class' => 'form-control', 'v-model' => 'display_name']) !!}
+					</div>
+
+					<div class="form-group">
+						<label class="control-label">By Key</label>
+						{!! Form::text('searchKey', null, ['class' => 'form-control', 'v-model' => 'name']) !!}
+					</div>
+				</div>
+
+				<div class="panel-footer">
+					<phone-tablet>
+						<a class="btn btn-default btn-lg btn-block" @click="resetFilters">Reset Filters</a>
+					</phone-tablet>
+					<desktop>
+						<a class="btn btn-default btn-block" @click="resetFilters">Reset Filters</a>
+					</desktop>
 				</div>
 			</div>
 		</div>
 
-		<div class="row" id="permissions" ng-controller="PermissionsController">
-			<div class="col-md-3 col-md-push-9">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h3 class="panel-title">Filter Permissions</h3>
+		<div class="col-md-9 col-md-pull-3">
+			<div class="data-table data-table-bordered data-table-striped">
+				<div class="row" v-for="permission in permissions | filterBy display_name in 'display_name' | filterBy name in 'name'">
+					<div class="col-md-9">
+						<p class="lead"><strong>{% permission.display_name %}</strong></p>
+						<p><strong>Key:</strong> {% permission.name %}</p>
+						<p v-show="permission.roles != ''"><strong>Included in Role(s):</strong> {%! permission.roles !%}</p>
 					</div>
-					<div class="panel-body">
-						<div class="form-group">
-							<label class="control-label">By Name</label>
-							{!! Form::text('searchName', null, ['class' => 'form-control', 'ng-model' => 'search.display_name']) !!}
-						</div>
+					<div class="col-md-3">
+						<phone-tablet>
+							<div class="row">
+								@if ($_user->can('access.edit'))
+									<div class="col-sm-6">
+										<p><a href="{% permission.links.edit %}" class="btn btn-default btn-lg btn-block">Edit</a></p>
+									</div>
+								@endif
 
-						<div class="form-group">
-							<label class="control-label">By Key</label>
-							{!! Form::text('searchKey', null, ['class' => 'form-control', 'ng-model' => 'search.name']) !!}
-						</div>
-					</div>
-
-					<div class="panel-footer">
-						<div class="visible-xs visible-sm">
-							<a class="btn btn-default btn-lg btn-block" ng-click="search = {}">Reset Filters</a>
-						</div>
-						<div class="visible-md visible-lg">
-							<a class="btn btn-default btn-block" ng-click="search = {}">Reset Filters</a>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="col-md-9 col-md-pull-3">
-				<div class="data-table data-table-bordered data-table-striped">
-					<div class="row" ng-repeat="permission in permissions | toArray | filter:search">
-						<div class="col-md-9">
-							<p class="lead"><strong>{% permission.display_name %}</strong></p>
-							<p><strong>Key:</strong> {% permission.name %}</p>
-							<p ng-hide="permission.roles == ''"><strong>Included in Role(s):</strong> <span ng-bind-html="permission.roles"></span></p>
-						</div>
-						<div class="col-md-3">
-							<div class="visible-xs visible-sm">
-								<div class="row">
-									@if ($_user->can('access.edit'))
-										<div class="col-sm-6">
-											<p><a href="{% permission.links.edit %}" class="btn btn-default btn-lg btn-block">Edit</a></p>
-										</div>
-									@endif
-
-									@if ($_user->can('access.remove'))
-										<div class="col-sm-6" ng-hide="permission.protected">
-											<p><a href="#" data-id="{% permission.id %}" data-action="remove" class="btn btn-danger btn-lg btn-block js-permissionAction">Remove</a></p>
-										</div>
-									@endif
-								</div>
+								@if ($_user->can('access.remove'))
+									<div class="col-sm-6" v-show="!permission.protected">
+										<p><a href="#" data-id="{% permission.id %}" data-action="remove" class="btn btn-danger btn-lg btn-block js-permissionAction">Remove</a></p>
+									</div>
+								@endif
 							</div>
-							<div class="visible-md visible-lg">
-								<div class="btn-toolbar pull-right">
-									@if ($_user->can('access.edit'))
-										<div class="btn-group">
-											<a href="{% permission.links.edit %}" class="btn btn-default">Edit</a>
-										</div>
-									@endif
+						</phone-tablet>
+						<desktop>
+							<div class="btn-toolbar pull-right">
+								@if ($_user->can('access.edit'))
+									<div class="btn-group">
+										<a href="{% permission.links.edit %}" class="btn btn-default">Edit</a>
+									</div>
+								@endif
 
-									@if ($_user->can('access.remove'))
-										<div class="btn-group" ng-hide="permission.protected">
-											<a href="#" data-id="{% permission.id %}" data-action="remove" class="btn btn-danger js-permissionAction">Remove</a>
-										</div>
-									@endif
-								</div>
+								@if ($_user->can('access.remove'))
+									<div class="btn-group" v-show="!permission.protected">
+										<a href="#" data-id="{% permission.id %}" data-action="remove" class="btn btn-danger js-permissionAction">Remove</a>
+									</div>
+								@endif
 							</div>
-						</div>
+						</desktop>
 					</div>
 				</div>
 			</div>
