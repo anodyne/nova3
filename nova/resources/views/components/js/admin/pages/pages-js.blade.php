81ca0ae8daf3013b87bbcfa8c47d28ecec340ced
David VanScott <david.vanscott@gmail.com>
2015-10-27 14:54:04 -0400

    Convert page manage to use Vue

--- nova/resources/views/components/js/admin/pages/pages-js.blade.php
+++ nova/resources/views/components/js/admin/pages/pages-js.blade.php
@@ -1,8 +1,42 @@
-{!! partial('include-angular') !!}
-{!! HTML::script('nova/resources/js/angular/pages.js') !!}
-
 <script>
-	var baseUrl = "{{ Request::root() }}";
+	var vm = new Vue({
+		el: "#app",
+		data: {
+			loading: true,
+			loadingWithError: false,
+			baseUrl: "{{ Request::root() }}",
+			name: "",
+			key: "",
+			uri: "",
+			verbs: [],
+			pages: []
+		},
+		methods: {
+			resetFilters: function()
+			{
+				this.$set("name", "")
+				this.$set("key", "")
+				this.$set("uri", "")
+				this.$set("verbs", [])
+			}
+		},
+		ready: function()
+		{
+			this.$http.get(this.baseUrl + '/api/pages', function (data, status, request)
+			{
+				this.$set('pages', data.data)
+			}).error(function (data, status, request)
+			{
+				this.$set('loadingWithError', true)
+			})
+		}
+	});
+
+	vm.$watch('pages', function (newValue, oldValue)
+	{
+		if (newValue.length > 0)
+			this.$set('loading', false)
+	})
 
 	$(document).on('click', '.js-pageAction', function(e)
 	{
--- nova/resources/views/components/pages/admin/pages/pages.blade.php
+++ nova/resources/views/components/pages/admin/pages/pages.blade.php
@@ -1,114 +1,126 @@
-<div ng-controller="PagesLoadingController">
-	<div ng-show="loading">
+<div v-show="loading">
+	<div v-show="!loadingWithError">
 		<h4 class="text-center">{!! HTML::image('nova/resources/images/ajax-loader.gif') !!}</h4>
 	</div>
+	<div v-else>
+		{!! alert('danger', "There was an error retrieving your pages from the database. This can be caused by a wrong URL or an issue with the database. Please try again.", "Error!") !!}
+	</div>
+</div>
 
-	<div ng-cloak>
-		<div class="visible-xs visible-sm">
+<div v-else>
+	<phone-tablet>
+		@can('create', $page)
+			<p><a href="{{ route('admin.pages.create') }}" class="btn btn-success btn-lg btn-block">Add a Page</a></p>
+		@endcan
+
+		<p><a href="{{ route('admin.content') }}" class="btn btn-default btn-lg btn-block">Manage Page Content</a></p>
+	</phone-tablet>
+	<desktop>
+		<div class="btn-toolbar">
 			@can('create', $page)
-				<p><a href="{{ route('admin.pages.create') }}" class="btn btn-success btn-lg btn-block">Add a Page</a></p>
+				<div class="btn-group">
+					<a href="{{ route('admin.pages.create') }}" class="btn btn-success">Add a Page</a>
+				</div>
 			@endcan
 
-			<p><a href="{{ route('admin.content') }}" class="btn btn-default btn-lg btn-block">Manage Page Content</a></p>
+			<div class="btn-group">
+				<a href="{{ route('admin.content') }}" class="btn btn-default">Manage Page Content</a>
+			</div>
 		</div>
-		<div class="visible-md visible-lg">
-			<div class="btn-toolbar">
-				@can('create', $page)
-					<div class="btn-group">
-						<a href="{{ route('admin.pages.create') }}" class="btn btn-success">Add a Page</a>
-					</div>
-				@endcan
+	</desktop>
 
-				<div class="btn-group">
-					<a href="{{ route('admin.content') }}" class="btn btn-default">Manage Page Content</a>
+	<div class="row">
+		<div class="col-md-3 col-md-push-9">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Filter Pages</h3>
+				</div>
+				<div class="panel-body">
+					<div class="form-group">
+						<label class="control-label">By HTTP Verb</label>
+						<div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="GET" v-model="verbs"> GET</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="POST" v-model="verbs"> POST</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="PUT" v-model="verbs"> PUT</label>
+							</div>
+							<div class="checkbox">
+								<label><input type="checkbox" value="DELETE" v-model="verbs"> DELETE</label>
+							</div>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label class="control-label">By Name</label>
+						{!! Form::text('searchName', null, ['class' => 'form-control', 'v-model' => 'name']) !!}
+					</div>
+
+					<div class="form-group">
+						<label class="control-label">By Key</label>
+						{!! Form::text('searchKey', null, ['class' => 'form-control', 'v-model' => 'key']) !!}
+					</div>
+
+					<div class="form-group">
+						<label class="control-label">By URI</label>
+						{!! Form::text('searchUri', null, ['class' => 'form-control', 'v-model' => 'uri']) !!}
+					</div>
+				</div>
+
+				<div class="panel-footer">
+					<phone-tablet>
+						<a class="btn btn-default btn-lg btn-block" @click="resetFilters">Reset Filters</a>
+					</phone-tablet>
+					<desktop>
+						<a class="btn btn-default btn-block" @click="resetFilters">Reset Filters</a>
+					</desktop>
 				</div>
 			</div>
 		</div>
 
-		<div class="row" id="pages" ng-controller="PagesController">
-			<div class="col-md-3 col-md-push-9">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h3 class="panel-title">Filter Pages</h3>
+		<div class="col-md-9 col-md-pull-3">
+			<div class="data-table data-table-bordered data-table-striped">
+				<div class="row" v-for="page in pages | filterBy name in 'name' | filterBy key in 'key' | filterBy uri in 'uri' | filterByCheckboxes verbs 'verb'">
+					<div class="col-md-9">
+						<p class="lead"><strong>{% page.name %}</strong></p>
+						<p><strong>Key:</strong> {% page.key %}</p>
+						<p><strong>URI:</strong> <code>{% page.uri %}</code></p>
+						<p><strong>Verb:</strong> <span class="label label-default">{% page.verb %}</span></p>
 					</div>
-					<div class="panel-body">
-						<div class="form-group">
-							<label class="control-label">By HTTP Verb</label>
-							<div id="verbCheckboxes" ng-repeat="verb in verbsGroup">
-								<div class="checkbox">
-									<label><input type="checkbox" ng-model="useVerbs[verb]"> {% verb %}</label>
-								</div>
+					<div class="col-md-3">
+						<phone-tablet>
+							<div class="row">
+								@can('edit', $page)
+									<div class="col-sm-6">
+										<p><a href="{% page.links.edit %}" class="btn btn-default btn-lg btn-block">Edit</a></p>
+									</div>
+								@endcan
+
+								@can('remove', $page)
+									<div class="col-sm-6" v-show="!page.protected">
+										<p><a href="#" data-id="{% page.id %}" data-action="remove" class="btn btn-danger btn-lg btn-block js-pageAction">Remove</a></p>
+									</div>
+								@endcan
 							</div>
-						</div>
+						</phone-tablet>
+						<desktop>
+							<div class="btn-toolbar pull-right">
+								@can('edit', $page)
+									<div class="btn-group">
+										<a href="{% page.links.edit %}" class="btn btn-default">Edit</a>
+									</div>
+								@endcan
 
-						<div class="form-group">
-							<label class="control-label">By Name</label>
-							{!! Form::text('searchName', null, ['class' => 'form-control', 'ng-model' => 'search.name']) !!}
-						</div>
-
-						<div class="form-group">
-							<label class="control-label">By Key</label>
-							{!! Form::text('searchKey', null, ['class' => 'form-control', 'ng-model' => 'search.key']) !!}
-						</div>
-
-						<div class="form-group">
-							<label class="control-label">By URI</label>
-							{!! Form::text('searchUri', null, ['class' => 'form-control', 'ng-model' => 'search.uri']) !!}
-						</div>
-					</div>
-
-					<div class="panel-footer">
-						<div class="visible-xs visible-sm">
-							<a class="btn btn-default btn-lg btn-block" ng-click="search = {}; useVerbs = {}">Reset Filters</a>
-						</div>
-						<div class="visible-md visible-lg">
-							<a class="btn btn-default btn-block" ng-click="search = {}; useVerbs = {}">Reset Filters</a>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="col-md-9 col-md-pull-3">
-				<div class="data-table data-table-bordered data-table-striped">
-					<div class="row" ng-repeat="page in filteredPages | filter:search">
-						<div class="col-md-9">
-							<p class="lead"><strong>{% page.name %}</strong></p>
-							<p><strong>Key:</strong> {% page.key %}</p>
-							<p><strong>URI:</strong> <code>{% page.uri %}</code></p>
-							<p><strong>Verb:</strong> <span class="label label-default">{% page.verb %}</span></p>
-						</div>
-						<div class="col-md-3">
-							<div class="visible-xs visible-sm">
-								<div class="row">
-									@can('edit', $page)
-										<div class="col-sm-6">
-											<p><a href="{% page.links.edit %}" class="btn btn-default btn-lg btn-block">Edit</a></p>
-										</div>
-									@endcan
-
-									@can('remove', $page)
-										<div class="col-sm-6" ng-hide="page.protected">
-											<p><a href="#" data-id="{% page.id %}" data-action="remove" class="btn btn-danger btn-lg btn-block js-pageAction">Remove</a></p>
-										</div>
-									@endcan
-								</div>
+								@can('remove', $page)
+									<div class="btn-group" v-show="!page.protected">
+										<a href="#" data-id="{% page.id %}" data-action="remove" class="btn btn-danger js-pageAction">Remove</a>
+									</div>
+								@endcan
 							</div>
-							<div class="visible-md visible-lg">
-								<div class="btn-toolbar pull-right">
-									@can('edit', $page)
-										<div class="btn-group">
-											<a href="{% page.links.edit %}" class="btn btn-default">Edit</a>
-										</div>
-									@endcan
-
-									@can('remove', $page)
-										<div class="btn-group" ng-hide="page.protected">
-											<a href="#" data-id="{% page.id %}" data-action="remove" class="btn btn-danger js-pageAction">Remove</a>
-										</div>
-									@endcan
-								</div>
-							</div>
-						</div>
+						</desktop>
 					</div>
 				</div>
 			</div>
