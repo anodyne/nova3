d316042ed32d0a6d26677b3dcd23ea3410c3baa2
David VanScott <david.vanscott@gmail.com>
2015-10-30 09:16:05 -0400

    Vue 1.0.3

--- nova/resources/views/components/partials/include-vue.blade.php
+++ nova/resources/views/components/partials/include-vue.blade.php
@@ -1,5 +1,5 @@
-<script src="//cdnjs.cloudflare.com/ajax/libs/vue/1.0.0/vue.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/vue-resource/0.1.16/vue-resource.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.3/vue.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.1.16/vue-resource.min.js"></script>
 {!! HTML::script('nova/resources/js/vue-components.js') !!}
 {!! HTML::script('nova/resources/js/vue-filters.js') !!}
 <script>
