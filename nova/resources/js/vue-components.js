ba6800182f60aa160513a1995d1e4320a48172fc
David VanScott <david.vanscott@gmail.com>
2015-10-31 13:34:10 -0400

    Cloak the components during start up

--- nova/resources/js/vue-components.js
+++ nova/resources/js/vue-components.js
@@ -1,27 +1,27 @@
 Vue.component('desktop-lg', {
-	template: '<div class="visible-lg-block"><slot></slot></div>'
+	template: '<div class="visible-lg-block" v-cloak><slot></slot></div>'
 })
 
 Vue.component('desktop-sm', {
-	template: '<div class="visible-md-block"><slot></slot></div>'
+	template: '<div class="visible-md-block" v-cloak><slot></slot></div>'
 })
 
 Vue.component('desktop', {
-	template: '<div class="visible-md-block visible-lg-block"><slot></slot></div>'
+	template: '<div class="visible-md-block visible-lg-block" v-cloak><slot></slot></div>'
 })
 
 Vue.component('phone-tablet', {
-	template: '<div class="visible-xs-block visible-sm-block"><slot></slot></div>'
+	template: '<div class="visible-xs-block visible-sm-block" v-cloak><slot></slot></div>'
 })
 
 Vue.component('phone', {
-	template: '<div class="visible-xs-block"><slot></slot></div>'
+	template: '<div class="visible-xs-block" v-cloak><slot></slot></div>'
 })
 
 Vue.component('tablet', {
-	template: '<div class="visible-sm-block"><slot></slot></div>'
+	template: '<div class="visible-sm-block" v-cloak><slot></slot></div>'
 })
 
 Vue.component('tablet-desktop', {
-	template: '<div class="visible-sm-block visible-md-block visible-lg-block"><slot></slot></div>'
+	template: '<div class="visible-sm-block visible-md-block visible-lg-block" v-cloak><slot></slot></div>'
 })
