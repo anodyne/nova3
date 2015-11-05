30f66a739d4c64cd607892d908bfdabc1ae79049
David VanScott <david.vanscott@gmail.com>
2015-11-04 22:12:41 -0500

    Update to 1.0.4 and build in the mixin

--- nova/resources/views/components/partials/include-vue.blade.php
+++ nova/resources/views/components/partials/include-vue.blade.php
@@ -1,13 +1,14 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.3/vue.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.1.16/vue-resource.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.4/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.1.16/vue-resource.js"></script>
 {!! HTML::script('nova/resources/js/vue-components.js') !!}
 {!! HTML::script('nova/resources/js/vue-filters.js') !!}
 <script>
 	Vue.config.delimiters = ['{%', '%}']
 	Vue.config.unsafeDelimiters = ['{%!', '!%}']
 
-	var vm = new Vue({
-		el: "#app",
+	var vm = new Vue(
+	{
+		el: '#app',
 		mixins: [vue]
-	});
+	})
 </script>
