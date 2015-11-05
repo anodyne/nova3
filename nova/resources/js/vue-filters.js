ad5ead346020caf7d82705057a71ecb8a11ad611
David VanScott <david.vanscott@gmail.com>
2015-11-04 22:11:52 -0500

    Minor updates to the coding style

--- nova/resources/js/vue-filters.js
+++ nova/resources/js/vue-filters.js
@@ -1,10 +1,12 @@
 Vue.filter('filterByCheckboxes', function (value, choices, filterKey)
 {
-	var choicesArr = $.map(choices, function (val, key) {
+	var choicesArr = $.map(choices, function (val, key)
+	{
 		return [val]
 	})
 	
-	return value.filter(function (item) {
+	return value.filter(function (item)
+	{
 		if (choicesArr.length > 0)
 			return $.inArray(item[filterKey], choicesArr) > -1
 		
