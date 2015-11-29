548bdf17f3e8121db140f515e212b50753cf61a9
David VanScott <david.vanscott@gmail.com>
2015-11-28 21:59:53 -0500

    Better wording on the HTTP verbs pop up

--- nova/resources/views/components/pages/admin/pages/help-verbs.blade.php
+++ nova/resources/views/components/pages/admin/pages/help-verbs.blade.php
@@ -1,15 +1,15 @@
-<p>HTTP defines methods (often referred to as <em>verbs</em>) to indicate the desired action to be performed on a specific resource.</p>
+<p>HTTP defines methods (often referred to as <em>verbs</em>) to indicate action to be taken on a specific resource.</p>
 
 <dl>
 	<dt>GET</dt>
-	<dd>Requests a representation of the specified resource. Requests using GET should only retrieve data and should have no other effect.</dd>
+	<dd>Simply put, a GET request, well, <em>gets</em> data from a server. In most gasts, a GET request should only retrieve data and have no other effect.</dd>
 
 	<dt>POST</dt>
-	<dd>Requests that the server accept the entity enclosed in the request as a new resource. This is most often used for creating new data on the server or in the database.</dd>
+	<dd>A POST request actually sends data to the server and expects something back. Most often this is used for submitting forms to create data on the server or in the database.</dd>
 
 	<dt>PUT</dt>
-	<dd>If the URI refers to an already existing resource, it's modified with the data passed through the request. This is most often used for updating existing pieces of data.</dd>
+	<dd>If the URI refers to an already existing resource, a PUT request takes its data and tries to replace (or put) that data in for that specific resource. This is most often used for updating existing pieces of data on the server or in the database.</dd>
 
 	<dt>DELETE</dt>
-	<dd>Deletes the specified resource.</dd>
+	<dd>Pretty self-explanatory, but a DELETE request takes a given resource and tells the server it wants to remove it from the server or database.</dd>
 </dl>
