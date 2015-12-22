0ae1c8d18b3107f3a123a7c9a488661c2457b1f5
David VanScott <david.vanscott@gmail.com>
2015-12-22 00:06:31 -0500

    Fix removed route helpers

--- nova/src/Setup/Http/routes.php
+++ nova/src/Setup/Http/routes.php
@@ -7,10 +7,10 @@ $options = [
 
 Route::group($options, function ()
 {
-	get('env', 'SetupController@environment')->name('setup.env');
-	get('/', 'SetupController@index')->name('setup.home');
-	get('update', 'UpdateController@index')->name('setup.update');
-	post('uninstall', 'SetupController@uninstall')->name('setup.uninstall');
+	Route::get('env', 'SetupController@environment')->name('setup.env');
+	Route::get('/', 'SetupController@index')->name('setup.home');
+	Route::get('update', 'UpdateController@index')->name('setup.update');
+	Route::post('uninstall', 'SetupController@uninstall')->name('setup.uninstall');
 });
 
 $installOptions = array_merge($options, [
@@ -19,26 +19,26 @@ $installOptions = array_merge($options, [
 
 Route::group($installOptions, function ()
 {
-	get('/', 'InstallController@index')->name('setup.install');
+	Route::get('/', 'InstallController@index')->name('setup.install');
 
-	get('config-database', 'ConfigDbController@info')->name('setup.install.config.db');
-	get('config-database/success', 'ConfigDbController@success')->name('setup.install.config.db.success');
-	get('config-database/write', 'ConfigDbController@write')->name('setup.install.config.db.write');
-	post('config-database/check', 'ConfigDbController@check')->name('setup.install.config.db.check');
+	Route::get('config-database', 'ConfigDbController@info')->name('setup.install.config.db');
+	Route::get('config-database/success', 'ConfigDbController@success')->name('setup.install.config.db.success');
+	Route::get('config-database/write', 'ConfigDbController@write')->name('setup.install.config.db.write');
+	Route::post('config-database/check', 'ConfigDbController@check')->name('setup.install.config.db.check');
 
-	get('config-email', 'ConfigEmailController@info')->name('setup.install.config.email');
-	get('config-email/success', 'ConfigEmailController@success')->name('setup.install.config.email.success');
-	post('config-email/write', 'ConfigEmailController@write')->name('setup.install.config.email.write');
+	Route::get('config-email', 'ConfigEmailController@info')->name('setup.install.config.email');
+	Route::get('config-email/success', 'ConfigEmailController@success')->name('setup.install.config.email.success');
+	Route::post('config-email/write', 'ConfigEmailController@write')->name('setup.install.config.email.write');
 
-	get('nova', 'InstallController@installLanding')->name('setup.install.nova');
-	get('nova/success', 'InstallController@novaSuccess')->name('setup.install.nova.success');
-	post('nova', 'InstallController@install');
+	Route::get('nova', 'InstallController@installLanding')->name('setup.install.nova');
+	Route::get('nova/success', 'InstallController@novaSuccess')->name('setup.install.nova.success');
+	Route::post('nova', 'InstallController@install');
 
-	get('user', 'InstallController@user')->name('setup.install.user');
-	get('user/success', 'InstallController@userSuccess')->name('setup.install.user.success');
-	post('user', 'InstallController@createUser')->name('setup.install.user.store');
+	Route::get('user', 'InstallController@user')->name('setup.install.user');
+	Route::get('user/success', 'InstallController@userSuccess')->name('setup.install.user.success');
+	Route::post('user', 'InstallController@createUser')->name('setup.install.user.store');
 
-	get('settings', 'InstallController@settings')->name('setup.install.settings');
-	get('settings/success', 'InstallController@settingsSuccess')->name('setup.install.settings.success');
-	post('settings', 'InstallController@updateSettings')->name('setup.install.settings.store');
+	Route::get('settings', 'InstallController@settings')->name('setup.install.settings');
+	Route::get('settings/success', 'InstallController@settingsSuccess')->name('setup.install.settings.success');
+	Route::post('settings', 'InstallController@updateSettings')->name('setup.install.settings.store');
 });
