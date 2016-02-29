c533348c7ef2804a25b3662fbe9ce9ad434b6ac2
David VanScott <david.vanscott@gmail.com>
2016-02-29 13:18:13 -0500

    Update to the latest versions of Vue
    Vue 1.0.17 and Vue Resource 0.7

--- nova/resources/views/components/partials/include-vue.blade.php
+++ nova/resources/views/components/partials/include-vue.blade.php
@@ -1,5 +1,5 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.10/vue.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.1.17/vue-resource.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.17/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.7.0/vue-resource.js"></script>
 {!! HTML::script('nova/resources/js/vue-components.js') !!}
 {!! HTML::script('nova/resources/js/vue-filters.js') !!}
 <script>
