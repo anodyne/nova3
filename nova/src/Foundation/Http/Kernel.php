45e2c69ce39f80d0828c53795bde18eb7d02d66f
David VanScott <david.vanscott@gmail.com>
2016-03-15 22:33:24 -0400

    Middleware updates

--- nova/src/Foundation/Http/Kernel.php
+++ nova/src/Foundation/Http/Kernel.php
@@ -8,15 +8,30 @@ class Kernel extends HttpKernel {
 	/**
 	 * The application's global HTTP middleware stack.
 	 *
+	 * These middleware are run during every request to your application.
+	 *
 	 * @var array
 	 */
 	protected $middleware = [
 		\Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-		\Illuminate\Cookie\Middleware\EncryptCookies::class,
-		\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-		\Illuminate\Session\Middleware\StartSession::class,
-		\Illuminate\View\Middleware\ShareErrorsFromSession::class,
-		Middleware\VerifyCsrfToken::class,
+	];
+
+	/**
+	 * The application's route middleware groups.
+	 *
+	 * @var array
+	 */
+	protected $middlewareGroups = [
+		'web' => [
+			Middleware\EncryptCookies::class,
+			\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+			\Illuminate\Session\Middleware\StartSession::class,
+			\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+			Middleware\VerifyCsrfToken::class,
+		],
+		'api' => [
+			'throttle:60,1',
+		],
 	];
 
 	/**
@@ -25,11 +40,13 @@ class Kernel extends HttpKernel {
 	 * @var array
 	 */
 	protected $routeMiddleware = [
-		'auth'				=> Middleware\Authenticate::class,
-		'auth.basic'		=> \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-		'nova.installed'	=> Middleware\CheckInstallationStatus::class,
-		'nova.render'		=> Middleware\RenderController::class,
-		'guest'				=> Middleware\RedirectIfAuthenticated::class,
+		'auth' => Middleware\Authenticate::class,
+		'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+		'guest' => Middleware\RedirectIfAuthenticated::class,
+		'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+		
+		'nova.installed' => Middleware\CheckInstallationStatus::class,
+		'nova.render' => Middleware\RenderController::class,
 	];
 
 	public function handle($request)
--- nova/src/Foundation/Http/Middleware/RedirectIfAuthenticated.php
+++ nova/src/Foundation/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,41 +1,23 @@
 <?php namespace Nova\Foundation\Http\Middleware;
 
 use Closure;
-use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated {
-
-	/**
-	 * The Guard implementation.
-	 *
-	 * @var Guard
-	 */
-	protected $auth;
-
-	/**
-	 * Create a new filter instance.
-	 *
-	 * @param  Guard  $auth
-	 * @return void
-	 */
-	public function __construct(Guard $auth)
-	{
-		$this->auth = $auth;
-	}
-
+	
 	/**
 	 * Handle an incoming request.
 	 *
 	 * @param  \Illuminate\Http\Request  $request
 	 * @param  \Closure  $next
+	 * @param  string|null  $guard
 	 * @return mixed
 	 */
-	public function handle($request, Closure $next)
+	public function handle($request, Closure $next, $guard = null)
 	{
-		if ($this->auth->check())
+		if (Auth::guard($guard)->check())
 		{
-			return new RedirectResponse(url('/home'));
+			return redirect('/');
 		}
 
 		return $next($request);
