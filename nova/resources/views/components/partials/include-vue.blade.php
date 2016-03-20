123dfddf3a0ac79e088a4cb33a290fe516010be9
David VanScott <david.vanscott@gmail.com>
2016-03-20 11:21:10 -0400

    Update to Vue 1.0.18

--- nova/resources/views/components/partials/include-vue.blade.php
+++ nova/resources/views/components/partials/include-vue.blade.php
@@ -1,10 +1,9 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.17/vue.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.7.0/vue-resource.js"></script>
 {!! HTML::script('nova/resources/js/vue-components.js') !!}
 {!! HTML::script('nova/resources/js/vue-filters.js') !!}
 <script>
-	var vm = new Vue(
-	{
+	var vm = new Vue({
 		el: '#app',
 		mixins: [vue],
 		http: {
