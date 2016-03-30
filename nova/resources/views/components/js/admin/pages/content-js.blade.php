c25228f9cdc1989e88eec798d6586fe49bdc96c6
David VanScott <david.vanscott@gmail.com>
2016-03-29 20:11:33 -0400

    Fixes for Vue changes

--- nova/resources/views/components/js/admin/pages/content-js.blade.php
+++ nova/resources/views/components/js/admin/pages/content-js.blade.php
@@ -10,34 +10,28 @@
 		},
 
 		methods: {
-			removeContent: function(contentId)
-			{
+			removeContent: function (contentId) {
 				$('#removeContent').modal({
 					remote: "{{ url('admin/content') }}/" + contentId + "/remove"
 				}).modal('show')
 			},
 
-			resetFilters: function()
-			{
+			resetFilters: function () {
 				this.key = ""
 				this.value = ""
 			}
 		},
 
-		ready: function()
-		{
-			this.$http.get(this.baseUrl + '/api/page-contents').then(function (response)
-			{
-				this.contents = response.data
-			}, function (response)
-			{
+		ready: function () {
+			this.$http.get(this.baseUrl + '/api/page-contents').then(function (response) {
+				this.contents = response.data.data
+			}, function (response) {
 				this.loadingWithError = true
 			})
 		},
 
 		watch: {
-			"contents": function (value, oldValue)
-			{
+			"contents": function (value, oldValue) {
 				if (value.length > 0)
 					this.loading = false
 			}
