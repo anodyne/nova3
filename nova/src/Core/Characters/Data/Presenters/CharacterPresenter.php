d7a730c3d65f50b627b537279d63939e66edefa8
David VanScott <david.vanscott@gmail.com>
2016-04-03 13:58:06 -0400

    Placeholder for printing a character name

--- nova/src/Core/Characters/Data/Presenters/CharacterPresenter.php
+++ nova/src/Core/Characters/Data/Presenters/CharacterPresenter.php
@@ -4,6 +4,11 @@ use Laracasts\Presenter\Presenter;
 
 class CharacterPresenter extends Presenter {
 
-	//
+	public function name()
+	{
+		$arr = [$this->entity->first_name, $this->entity->last_name];
+
+		return implode(' ', $arr);
+	}
 
 }
