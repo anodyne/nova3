5150c11de8c8955c66669ce3c0b2c1b7c8f2f0aa
David VanScott <david.vanscott@gmail.com>
2016-04-07 10:53:56 -0400

    Move nested tabs to the bottom of the tab content
    For consistency, we want unbound fields, then sections, then tabs
    throughout

--- nova/resources/views/components/partials/form-tabs-content.blade.php
+++ nova/resources/views/components/partials/form-tabs-content.blade.php
@@ -8,13 +8,13 @@
 				{!! partial('form-fields', ['fields' => $tab->fieldsUnbound, 'editable' => $editable, 'form' => $form, 'action' => $action, 'data' => $data, 'id' => $id, 'fieldNameWrapper' => $fieldNameWrapper]) !!}
 			@endif
 
+			{!! partial('form-sections', ['sections' => $tab->sections, 'editable' => $editable, 'form' => $form, 'action' => $action, 'data' => $data, 'id' => $id, 'fieldNameWrapper' => $fieldNameWrapper]) !!}
+
 			@if ($tab->childrenTabs->count() > 0)
 				{!! partial('form-tabs-control', ['tabs' => $tab->childrenTabs, 'style' => 'pills', 'editable' => $editable, 'form' => $form, 'action' => $action, 'data' => $data, 'id' => $id]) !!}
 
 				{!! partial('form-tabs-content', ['tabs' => $tab->childrenTabs, 'editable' => $editable, 'form' => $form, 'action' => $action, 'data' => $data, 'id' => $id, 'fieldNameWrapper' => $fieldNameWrapper]) !!}
 			@endif
-
-			{!! partial('form-sections', ['sections' => $tab->sections, 'editable' => $editable, 'form' => $form, 'action' => $action, 'data' => $data, 'id' => $id, 'fieldNameWrapper' => $fieldNameWrapper]) !!}
 		</div>
 	@endforeach
 	</div>
