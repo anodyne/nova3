4975325e8f324ebd3f1371b3082be827f731c73a
David VanScott <david.vanscott@gmail.com>
2016-04-16 23:22:35 -0400

    Add the notifications table

---
+++ nova/src/Setup/database/migrations/2016_04_16_154539_create_notifications_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('notifications', function (Blueprint $table)
+		{
+			$table->string('id')->primary();
+			$table->integer('user_id');
+			$table->integer('created_by')->nullable(); // Null created_by means the system created it
+			$table->string('icon', 50)->nullable();
+			$table->text('body');
+			$table->string('action_text')->nullable();
+			$table->text('action_url')->nullable();
+			$table->tinyInteger('read')->default((int) false);
+			$table->timestamps();
+
+			$table->index(['user_id', 'created_at']);
+		});
+	}
+
+	public function down()
+	{
+		Schema::dropIfExists('notifications');
+	}
+}
