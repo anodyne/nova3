7f5155e0a3a30975806e16ceccda976065a9352a
David VanScott <david.vanscott@gmail.com>
2016-04-17 13:03:06 -0400

    Update the destroy resource

--- nova/resources/views/components/pages/admin/form-center/entry-remove.blade.php
+++ nova/resources/views/components/pages/admin/form-center/entry-remove.blade.php
@@ -1,2 +1,2 @@
-{!! Form::model($entry, ['route' => [$form->resource_delete, $form->key, $entry->id], 'method' => 'delete']) !!}
+{!! Form::model($entry, ['route' => [$form->resource_destroy, $form->key, $entry->id], 'method' => 'delete']) !!}
 {!! Form::close() !!}
