54b8caac7037f1a366677d60f550cf83c26117bb
David VanScott <david.vanscott@gmail.com>
2016-04-17 14:59:20 -0400

    Form center updates
    Set submission limits and dynamically pluralize “entry” based on how
    many entries there are

--- nova/resources/views/components/pages/admin/form-center/show.blade.php
+++ nova/resources/views/components/pages/admin/form-center/show.blade.php
@@ -15,29 +15,33 @@
 	{!! $form->present()->message !!}
 
 	<mobile>
-		<div class="row">
-			<div class="col-sm-6">
-				<p><a href="#" class="btn btn-success btn-lg btn-block" @click.prevent="switchToForm">Fill Out Form</a></p>
+		@if ( ! $form->allow_multiple_submissions and $entries->count() == 0)
+			<div class="row">
+				<div class="col-sm-6">
+					<p><a href="#" class="btn btn-success btn-lg btn-block" @click.prevent="switchToForm">Fill Out Form</a></p>
+				</div>
+				<div class="col-sm-6">
+					<p><a href="#" class="btn btn-default btn-lg btn-block" @click.prevent="switchToEntries">Show My {{ Str::plural('Entry', $entries->count()) }}</a></p>
+				</div>
 			</div>
-			<div class="col-sm-6">
-				<p><a href="#" class="btn btn-default btn-lg btn-block" @click.prevent="switchToEntries">Show My Entries</a></p>
-			</div>
-		</div>
+		@endif
 	</mobile>
 	<desktop>
-		<div class="btn-toolbar">
-			<div class="btn-group">
-				<a href="#" class="btn btn-success" @click.prevent="switchToForm">Fill Out Form</a>
+		@if ( ! $form->allow_multiple_submissions and $entries->count() == 0)
+			<div class="btn-toolbar">
+				<div class="btn-group">
+					<a href="#" class="btn btn-success" @click.prevent="switchToForm">Fill Out Form</a>
+				</div>
+				<div class="btn-group">
+					<a href="#" class="btn btn-default" @click.prevent="switchToEntries">Show My {{ Str::plural('Entry', $entries->count()) }}</a>
+				</div>
 			</div>
-			<div class="btn-group">
-				<a href="#" class="btn btn-default" @click.prevent="switchToEntries">Show My Entries</a>
-			</div>
-		</div>
+		@endif
 	</desktop>
 
 	<div v-if="showEntries">
 		@if ($entries->count() > 0)
-			<h3>My Entries</h3>
+			<h3>My {{ Str::plural('Entry', $entries->count()) }}</h3>
 
 			<div class="data-table data-table-striped data-table-bordered">
 			@foreach ($entries as $entry)
