8c0da46e699dd3565f572e04dd7ef80f700bf37b
David VanScott <david.vanscott@gmail.com>
2016-04-22 13:57:02 -0400

    Add a user seeder

--- nova/src/Setup/database/seeds/DatabaseSeeder.php
+++ nova/src/Setup/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder {
 
 		// $this->call('UserTableSeeder');
 		$this->call('FormSeeder');
+		$this->call('UserSeeder');
 	}
 
 }
---
+++ nova/src/Setup/database/seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder {
+
+	public $userCount = 10;
+	
+	public function run()
+	{
+		$faker = \Faker\Factory::create();
+
+		$userCreator = app('nova.user.creator');
+
+		for ($i = 1; $i < $this->userCount; $i++)
+		{
+			$userCreator->create([
+				'email' => $faker->safeEmail,
+				'password' => 'password',
+				'name' => $faker->name,
+				'status' => $faker->numberBetween(1, 3),
+			]);
+		}
+	}
+	
+}
