1687a4e649dbb99f0e1047306e4d773167c30896
David VanScott <david.vanscott@gmail.com>
2016-04-27 09:42:09 -0400

    Add a validate helper

--- nova/src/Foundation/helpers.php
+++ nova/src/Foundation/helpers.php
@@ -192,3 +192,21 @@ if ( ! function_exists('compile'))
 		return app('nova.page.compiler')->compile($text);
 	}
 }
+
+if ( ! function_exists('validate'))
+{
+	function validate($fields, $rules)
+	{
+		if ( ! is_array($fields))
+		{
+			$fields = ['default' => $fields];
+		}
+
+		if ( ! is_array($rules))
+		{
+			$rules = ['default' => $rules];
+		}
+
+		return Validator::make($fields, $rules)->passes();
+	}
+}
