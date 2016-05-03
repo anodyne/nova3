6e149e23d3c9e8d5765aa2a15d7c2da103de3116
David VanScott <david.vanscott@gmail.com>
2016-05-03 16:24:59 -0400

    Material theme updates (very VERY broken)

--- themes/material/Theme.php
+++ themes/material/Theme.php
@@ -1,35 +1,51 @@
 <?php
 
-use Nova\Foundation\Services\Themes\Theme as BaseTheme;
+use Nova\Foundation\Themes\Theme as BaseTheme;
 
 class Theme extends BaseTheme {
 
-	public function getIconMap()
+	public function getIconMap(): array
 	{
 		return [
 			'add'			=> 'add',
 			'announcement'	=> 'mic',
 			'archive'		=> 'archive',
+			'arrow-back'	=> 'arrow_back',
+			'arrow-down'	=> 'arrow_downward',
+			'arrow-forward'	=> 'arrow_forward',
+			'arrow-up'		=> 'arrow_upward',
 			'ban'			=> 'block',
 			'bookmark'		=> 'bookmark',
+			'brush'			=> 'brush',
 			'check'			=> 'check',
 			'clock'			=> 'schedule',
 			'close'			=> 'clear',
+			'code'			=> 'code',
 			'comment'		=> 'chat_bubble',
 			'comments'		=> 'forum',
+			'copy'			=> 'content_copy',
+			'cut'			=> 'content_cut',
 			'dashboard'		=> 'dashboard',
 			'delete'		=> 'delete',
 			'edit'			=> 'edit',
 			'file'			=> 'insert_drive_file',
+			'flag'			=> 'flag',
 			'forward'		=> 'forward',
 			'heart'			=> 'favorite',
+			'heart-empty'	=> 'favorite_border',
 			'home'			=> 'home',
 			'image'			=> 'image',
 			'inbox'			=> 'inbox',
 			'info'			=> 'info',
+			'leaf'			=> 'nature',
+			'light'			=> 'lightbulb_outline',
+			'link'			=> 'link',
+			'list'			=> 'list',
+			'location'		=> 'location_on',
 			'lock'			=> 'lock',
 			'more'			=> 'more_horiz',
 			'notifications'	=> 'notifications',
+			'paste'			=> 'content_paste',
 			'question'		=> 'help',
 			'refresh'		=> 'autorenew',
 			'reply'			=> 'reply',
@@ -41,8 +57,8 @@ class Theme extends BaseTheme {
 			'sign-in'		=> 'power_settings_new',
 			'sign-out'		=> 'power_settings_new',
 			'star'			=> 'star',
-			'tag'			=> 'tag',
-			'tags'			=> 'fa fa-tags',
+			'star-empty'	=> 'star_border',
+			'tag'			=> 'label',
 			'tasks'			=> 'playlist_add_check',
 			'thumbs-down'	=> 'thumb_down',
 			'thumbs-up'		=> 'thumb_up',
@@ -51,6 +67,11 @@ class Theme extends BaseTheme {
 			'users'			=> 'people',
 			'warning'		=> 'warning',
 		];
+	}
+
+	public function iconTemplate()
+	{
+		return '<i class="material-icons %classes%">%icon%</i>';
 	}
 
 }
--- themes/material/components/partials/icon.blade.php
+++
@@ -1,1 +0,0 @@
-<i class="material-icons {{ $additional }}">{{ $icon }}</i>
