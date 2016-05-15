d24489589afa808fc0260572ed26c074e3857ace
David VanScott <david.vanscott@gmail.com>
2016-05-15 18:27:46 -0400

    Simplify the Vue templates for picker components

--- nova/resources/views/components/partials/access-picker.blade.php
+++ nova/resources/views/components/partials/access-picker.blade.php
@@ -1,2 +1,29 @@
 <access-picker :roles="roles" :permissions="permissions"></access-picker>
-@include('nova/resources/js/vue/templates/access-picker')
+
+<template id="access-picker-template">
+	<div class="form-group">
+		<div class="col-xs-12">
+			<div class="radio-inline">
+				<label><input type="radio" name="access_type_selection" v-model="accessTypeSelection" value="roles"> Restrict by Role(s)</label>
+			</div>
+			<div class="radio-inline">
+				<label><input type="radio" name="access_type_selection" v-model="accessTypeSelection" value="permissions"> Restrict by Permission(s)</label>
+			</div>
+		</div>
+	</div>
+
+	<div class="nova-access-picker" v-show="accessTypeSelection != ''">
+		<input type="text" :placeholder="config.inputPlaceholder" autocomplete="off" v-model="query" class="typeaheadInput" :class="[ config.inputClasses ]">
+
+		<div class="selected-access-items">
+			<span v-for="item in items">
+				<span class="label label-default">
+					@{{ item.type | capitalize }}: @{{ item.name }}
+					<a class="remove" role="button" @click="removeSelectedItem(item)">&times;</a>
+				</span>
+			</span>
+		</div>
+
+		<input type="hidden" name="access" :value="accessItems">
+	</div>
+</template>
--- nova/resources/views/components/partials/icon-picker.blade.php
+++ nova/resources/views/components/partials/icon-picker.blade.php
@@ -1,2 +1,17 @@
 <icon-picker></icon-picker>
-@include('nova/resources/js/vue/templates/icon-picker')
+
+<template id="icon-picker-template">
+	<div class="nova-icon-picker">
+		<div class="row">
+			<div class="col-md-10">
+				<select :name="config.inputName" :class="[ config.inputClasses ]" v-model="selectedIcon">
+					<option value="">None</option>
+					<option v-for="icon in icons" :value="icon.value">@{{ icon.key }}</option>
+				</select>
+			</div>
+			<div class="col-md-2">
+				<p class="form-control-static">@{{{ iconPreview }}}</p>
+			</div>
+		</div>
+	</div>
+</template>
