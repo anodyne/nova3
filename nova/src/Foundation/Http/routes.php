e53a71df97c02c6501b57f902e1f601c713c7892
David VanScott <david.vanscott@gmail.com>
2016-07-15 09:07:32 -0400

    Some more tweaks to the route file

--- nova/src/Foundation/Http/routes.php
+++ nova/src/Foundation/Http/routes.php
@@ -5,41 +5,23 @@ $pages = app('nova.pages');
 
 if ($pages->count() > 0)
 {
-	// Allowed verbs
-	$allowedVerbs = ['get', 'post', 'put', 'delete'];
+	// Allowed HTTP verbs
+	$allowedVerbs = collect(['get', 'post', 'put', 'delete']);
 
+	// Go through the pages and build the routes
 	$pages->each(function ($page) use ($allowedVerbs, $router)
 	{
-		// Get the verb and format it
+		// Get the page's verb and format it
 		$verb = strtolower($page->verb);
 
 		// Make sure the page is using one of the allowed verbs
-		if (in_array($verb, $allowedVerbs))
+		if ($allowedVerbs->contains($verb))
 		{
-			// Build the options
-			$options = [
+			// Call the appropriate method on the $router
+			call_user_func_array([$router, $verb], [$page->uri, [
 				'as' => $page->key,
 				'uses' => ($page->resource) ?: $page->default_resource,
-			];
-
-			// Call the appropriate router method to build the route collection
-			call_user_func_array([$router, $verb], [$page->uri, $options]);
+			]]);
 		}
 	});
-
-	/*foreach ($pages as $page)
-	{
-		// Get the verb and format it
-		$verb = strtolower($page->verb);
-
-		// Make sure the page is using one of the allowed verbs
-		if (in_array($verb, $allowedVerbs))
-		{
-			// Build the options
-			$options['as'] = $page->key;
-			$options['uses'] = ($page->resource) ?: $page->default_resource;
-
-			call_user_func_array([$router, $verb], [$page->uri, $options]);
-		}
-	}*/
 }
