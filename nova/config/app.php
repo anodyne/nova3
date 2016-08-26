4f5ea8f63458fcbab913fbcd87e58acbeb1266af
David VanScott <david.vanscott@gmail.com>
2016-08-26 07:20:32 -0400

    We need a default key or things will fail

--- nova/config/app.php
+++ nova/config/app.php
@@ -90,7 +90,7 @@ return [
 	|
 	*/
 
-	'key' => env('APP_KEY', 'SomeRandomString'),
+	'key' => env('APP_KEY', 'base64:TMZNjNdADtYcKjuYMTL1LoFP15PTYCjxku8g8y+rUe4='),
 
 	'cipher' => 'AES-256-CBC',
 
