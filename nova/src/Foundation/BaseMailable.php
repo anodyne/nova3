12d075f18f0fc28366bb925e1ed41f63a608be64
David VanScott <david.vanscott@gmail.com>
2016-08-29 15:42:09 -0400

    Add the base Mailable class

---
+++ nova/src/Foundation/BaseMailable.php
@@ -0,0 +1,29 @@
+<?php namespace Nova\Foundation;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+abstract class BaseMailable extends Mailable {
+
+	use Queueable, SerializesModels;
+
+	public function __construct() {}
+
+	public function subject($subject)
+	{
+		$subjectPrefix = app('nova.settings')->get('mail_subject_prefix');
+
+		$this->subject = "{$subjectPrefix} {$subject}";
+
+		return $this;
+	}
+
+	public function view($view, array $data = [])
+	{
+		$this->view = locate()->email($view);
+		$this->viewData = $data;
+
+		return $this;
+	}
+}
