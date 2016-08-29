b8a119316f433dd4a0c203193ae06ede4b3872ef
David VanScott <david.vanscott@gmail.com>
2016-08-29 15:42:36 -0400

    Placeholder character events

---
+++ nova/src/Core/Characters/Notifications/CharacterCreated.php
@@ -0,0 +1,14 @@
+<?php namespace Nova\Core\Characters\Notifications;
+
+class CharacterCreated {
+
+	public function via($notifiable)
+	{
+		return ['database'];
+	}
+
+	public function toArray($notifiable)
+	{
+		return [];
+	}
+}
---
+++ nova/src/Core/Characters/Notifications/CharacterLinkedToUser.php
@@ -0,0 +1,14 @@
+<?php namespace Nova\Core\Characters\Notifications;
+
+class CharacterLinkedToUser {
+
+	public function via($notifiable)
+	{
+		return ['database'];
+	}
+
+	public function toArray($notifiable)
+	{
+		return [];
+	}
+}
