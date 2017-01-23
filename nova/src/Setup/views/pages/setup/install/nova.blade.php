840a248b67f8e16e3942d10d70607efb9ce18ae2
David VanScott <david.vanscott@gmail.com>
2017-01-23 16:17:03 -0500

    Add a new animated spinner

--- nova/src/Setup/views/pages/setup/install/nova.blade.php
+++ nova/src/Setup/views/pages/setup/install/nova.blade.php
@@ -12,7 +12,11 @@
 	<h1>Install {{ config('nova.app.name') }}</h1>
 
 	<div v-show="loading">
-		<p class="text-center"><img src="{{ asset('nova/src/Setup/views/design/images/ajax-loader.gif') }}"></p>
+		<div class="spinner">
+			<div class="bounce1"></div>
+			<div class="bounce2"></div>
+			<div class="bounce3"></div>
+		</div>
 	</div>
 	<div v-show="loadingWithError" v-cloak>
 		<div class="alert alert-danger">
