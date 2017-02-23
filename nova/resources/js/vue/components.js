99da9f8d63bb459569c4a9ce94a63e2322562c39
David VanScott <david.vanscott@gmail.com>
2017-02-22 22:22:38 -0500

    Update the Vue components for BS4

--- nova/resources/js/vue/components.js
+++ nova/resources/js/vue/components.js
@@ -1,21 +1,21 @@
 Vue.component('desktop-lg', {
-	template: '<div class="visible-lg-block" v-cloak><slot></slot></div>'
+	template: '<div class="hidden-lg-down" v-cloak><slot></slot></div>'
 })
 
 Vue.component('desktop-sm', {
-	template: '<div class="visible-md-block" v-cloak><slot></slot></div>'
+	template: '<div class="hidden-xl-up hidden-md-down" v-cloak><slot></slot></div>'
 })
 
 Vue.component('desktop', {
-	template: '<div class="visible-md-block visible-lg-block" v-cloak><slot></slot></div>'
+	template: '<div class="hidden-md-down" v-cloak><slot></slot></div>'
 })
 
 Vue.component('mobile', {
-	template: '<div class="visible-xs-block visible-sm-block" v-cloak><slot></slot></div>'
+	template: '<div class="hidden-md-up" v-cloak><slot></slot></div>'
 })
 
 Vue.component('phone', {
-	template: '<div class="visible-xs-block" v-cloak><slot></slot></div>'
+	template: '<div class="hidden-sm-up" v-cloak><slot></slot></div>'
 })
 
 Vue.component('tablet', {
