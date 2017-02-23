a101f9fe67cf002e0a2f20e08a16f27826246aa1
David VanScott <david.vanscott@gmail.com>
2017-02-22 22:23:20 -0500

    Making the nav match BS4

--- nova/resources/views/components/partials/menu/menu-admin-combined.blade.php
+++ nova/resources/views/components/partials/menu/menu-admin-combined.blade.php
@@ -1,17 +1,11 @@
-<nav class="navbar navbar-default">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a href="{{ route('home') }}" class="navbar-brand">{{ $_content->get('sim.name') }}</a>
-		</div>
-		<div id="navbar" class="navbar-collapse collapse">
-			{!! MenuBuilder::menuCombined() !!}
-			{!! MenuBuilder::menuUser() !!}
-		</div>
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded mb-3">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<a href="{{ route('home') }}" class="navbar-brand">{{ $_content->get('sim.name') }}</a>
+	
+	<div id="navbar" class="navbar-collapse collapse">
+		{!! MenuBuilder::menuCombined() !!}
+		{!! MenuBuilder::menuUser() !!}
 	</div>
 </nav>
