de1ee1cbbea7ebf49caa567e319129361677d03b
David VanScott <david.vanscott@gmail.com>
2017-02-24 22:47:28 -0500

    Small updates to the icon listing

--- nova/resources/views/components/pages/icons.blade.php
+++ nova/resources/views/components/pages/icons.blade.php
@@ -1,12 +1,12 @@
 <div class="page-header">
-	<h1>Icons</h1>
+	<h1>Icons ({{ count($icons) }} icons)</h1>
 </div>
 
 <div class="row">
 	@foreach ($icons as $key => $value)
 		<div class="col-md-2 text-center">
-			<div>{!! icon($key, 'md') !!}</div>
-			<p class="text-muted">{{ $key }}</p>
+			<div>{!! icon($key, 'x2') !!}</div>
+			<small class="text-muted">{{ $key }}</small>
 		</div>
 	@endforeach
 </div>
