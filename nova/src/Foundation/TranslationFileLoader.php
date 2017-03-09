4ffeb6800e0b02881d795c1d8ad63f6346ecf105
David VanScott <david.vanscott@gmail.com>
2017-03-09 11:22:24 -0500

    I18n file loading updates

--- nova/src/Foundation/TranslationFileLoader.php
+++ nova/src/Foundation/TranslationFileLoader.php
@@ -1,17 +1,17 @@
-<?php namespace Nova\Foundation\Translation;
+<?php namespace Nova\Foundation;
 
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Translation\FileLoader as BaseFileLoader;
 
-class FileLoader extends BaseFileLoader {
+class TranslationFileLoader extends BaseFileLoader {
 
-	protected $corePath;
+	protected $novaPath;
 
-	public function __construct(Filesystem $files, $path, $corePath)
+	public function __construct(Filesystem $files, $path, $novaPath)
 	{
 		parent::__construct($files, $path);
 
-		$this->corePath = $corePath;
+		$this->novaPath = $novaPath;
 	}
 
 	public function load($locale, $group, $namespace = null)
@@ -29,17 +29,17 @@ class FileLoader extends BaseFileLoader {
 
 	protected function loadJsonPath($path, $locale)
 	{
-		$coreLang = [];
+		$novaLang = [];
 		$appLang = [];
 
-		if ($this->files->exists($fullCore = "{$this->corePath}/{$locale}.json")) {
-			$coreLang = json_decode($this->files->get($fullCore), true);
+		if ($this->files->exists($fullCore = "{$this->novaPath}/{$locale}.json")) {
+			$novaLang = json_decode($this->files->get($fullCore), true);
 		}
 
 		if ($this->files->exists($fullApp = "{$this->path}/{$locale}.json")) {
 			$appLang = json_decode($this->files->get($fullApp), true);
 		}
 
-		return array_replace_recursive($coreLang, $appLang);
+		return array_replace_recursive($novaLang, $appLang);
 	}
 }
