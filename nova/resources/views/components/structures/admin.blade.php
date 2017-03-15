7b844dd2018db0536fa85d1ca29ad92ac99b8758
David VanScott <david.vanscott@gmail.com>
2017-03-15 09:36:15 -0400

    Structure file updates

--- nova/resources/views/components/structures/admin.blade.php
+++ nova/resources/views/components/structures/admin.blade.php
@@ -13,8 +13,7 @@
 		@else
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 		@endif
-		{!! HTML::style('nova/resources/css/summernote.css') !!}
-		{!! HTML::style('nova/resources/css/sweetalert.css') !!}
+		{!! HTML::style('nova/dist/css/all.css') !!}
 		{!! partial('fonts-include') !!}
 		{!! partial('icons-include') !!}
 
@@ -25,13 +24,13 @@
 				{!! HTML::style(theme_path('design/css/custom.icons.css')) !!}
 			@endif
 
-			{!! HTML::style('nova/resources/views/design/css/base.icons.css') !!}
+			{!! HTML::style('nova/dist/css/base.icons.css') !!}
 		@endif
 
 		@if (app('files')->exists(theme_path('design/css/style.css', false)))
 			{!! HTML::style(theme_path('design/css/style.css')) !!}
 		@else
-			{!! HTML::style('nova/resources/views/design/css/base.style.css') !!}
+			{!! HTML::style('nova/dist/css/base.style.css') !!}
 
 			@if (app('files')->exists(theme_path('design/css/custom.css', false)))
 				{!! HTML::style(theme_path('design/css/custom.css')) !!}
@@ -52,7 +51,7 @@
 				{!! HTML::style(theme_path('design/css/custom.responsive.css')) !!}
 			@endif
 
-			{!! HTML::style('nova/resources/views/design/css/base.responsive.css') !!}
+			{!! HTML::style('nova/dist/css/base.responsive.css') !!}
 		@endif
 
 		{!! $styles or false !!}
@@ -63,14 +62,9 @@
 		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.7.0/vue-resource.js"></script>
-		{!! HTML::script('nova/resources/js/underscore-min.js') !!}
-		{!! HTML::script('nova/resources/js/summernote.min.js') !!}
-		{!! HTML::script('nova/resources/js/functions.js') !!}
-		{!! HTML::script('nova/resources/js/sweetalert.min.js') !!}
-		{!! HTML::script('nova/resources/js/vue/components.js') !!}
-		{!! HTML::script('nova/resources/js/vue/filters.js') !!}
+		<script src="https://unpkg.com/vue@2.2.4"></script>
+		<script src="https://unpkg.com/axios@0.15.3/dist/axios.min.js"></script>
+		{!! HTML::script('nova/dist/js/all.js') !!}
 		<script>
 			// Destroy all modals when they're hidden
 			$('.modal').on('hidden.bs.modal', function() {
@@ -82,34 +76,6 @@
 				vm.$compile($(this).get(0))
 			})
 
-			/*// Setup the CSRF token on Ajax requests
-			$.ajaxPrefilter(function (options, originalOptions, xhr) {
-				var token = $('meta[name="csrf-token"]').attr('content')
-
-				if (token) {
-					return xhr.setRequestHeader('X-CSRF-TOKEN', token)
-				}
-			})
-
-			// Setup the CSRF token on Ajax requests
-			$.ajaxSetup({
-				headers: {
-					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-				}
-			})
-
-			$(document).ajaxError(function(event, xhr, settings, thrownError) {
-				if (xhr.status == 403) {
-					swal({
-						title: "Unauthorized!",
-						text: "You do not have permission to take this action!",
-						type: "error",
-						timer: null,
-						html: true
-					})
-				}
-			})*/
-
 			var vue = {}
 			window.Nova = <?php echo json_encode(Nova::scriptVariables());?>
 
@@ -119,9 +85,8 @@
 					text: "{{ session('flash_message.message') }}",
 					type: "{{ session('flash_message.level') }}",
 					timer: 2250,
-					showConfirmButton: false,
-					html: true
-				})
+					showConfirmButton: false
+				}).then(function () {}, function (dismiss) {})
 			@endif
 
 			@if (session()->has('flash_message_overlay'))
@@ -129,10 +94,9 @@
 					title: "{{ session('flash_message_overlay.title') }}",
 					text: "{{ session('flash_message_overlay.message') }}",
 					type: "{{ session('flash_message_overlay.level') }}",
-					timer: null,
-					confirmButtonText: "OK",
-					html: true,
-					allowOutsideClick: true
+					confirmButtonText: "{{ _m('ok') }}",
+					confirmButtonClass: 'btn btn-success',
+					buttonsStyling: false
 				})
 			@endif
 
@@ -141,30 +105,13 @@
 				$('.js-tooltip-bottom').tooltip({ placement: 'bottom' })
 				$('.js-tooltip-left').tooltip({ placement: 'left' })
 				$('.js-tooltip-right').tooltip({ placement: 'right' })
-
-				$('.editor').summernote({
-					height: 250,
-					toolbar: [
-						['style', ['bold', 'italic', 'underline', 'clear']],
-						['font', ['superscript', 'subscript']],
-						['fontsize', ['fontsize']],
-						['para', ['ul', 'ol', 'paragraph']],
-						['insert', ['picture', 'link', 'table', 'hr']],
-						['misc', ['codeview']]
-					]
-				})
 			})
 		</script>
 		{!! $scripts or false !!}
 		<script>
 			var vm = new Vue({
 				el: '#app',
-				mixins: [vue],
-				http: {
-					headers: {
-						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-					}
-				}
+				mixins: [vue]
 			})
 		</script>
 	</body>
--- nova/resources/views/components/structures/auth.blade.php
+++ nova/resources/views/components/structures/auth.blade.php
@@ -13,16 +13,15 @@
 		@else
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 		@endif
-		{!! HTML::style('nova/resources/css/summernote.css') !!}
-		{!! HTML::style('nova/resources/css/sweetalert.css') !!}
+		{!! HTML::style('nova/dist/css/all.css') !!}
 		{!! partial('fonts-include') !!}
 		{!! partial('icons-include') !!}
 		
 		@if (app('files')->exists(theme_path('design/css/auth.style.css', false)))
 			{!! HTML::style(theme_path('design/css/auth.style.css')) !!}
 		@else
-			{!! HTML::style('nova/resources/views/design/css/base.style.css') !!}
-			{!! HTML::style('nova/resources/views/design/css/auth.style.css') !!}
+			{!! HTML::style('nova/dist/css/base.style.css') !!}
+			{!! HTML::style('nova/dist/css/auth.style.css') !!}
 
 			@if (app('files')->exists(theme_path('design/css/auth.custom.css', false)))
 				{!! HTML::style(theme_path('design/css/auth.custom.css')) !!}
@@ -32,7 +31,7 @@
 		@if (app('files')->exists(theme_path('design/css/responsive.css', false)))
 			{!! HTML::style(theme_path('design/css/responsive.css')) !!}
 		@else
-			{!! HTML::style('nova/resources/views/design/css/base.responsive.css') !!}
+			{!! HTML::style('nova/dist/css/base.responsive.css') !!}
 
 			@if (app('files')->exists(theme_path('design/css/custom.responsive.css', false)))
 				{!! HTML::style(theme_path('design/css/custom.responsive.css')) !!}
@@ -54,14 +53,9 @@
 		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.7.0/vue-resource.js"></script>
-		{!! HTML::script('nova/resources/js/underscore-min.js') !!}
-		{!! HTML::script('nova/resources/js/summernote.min.js') !!}
-		{!! HTML::script('nova/resources/js/functions.js') !!}
-		{!! HTML::script('nova/resources/js/sweetalert.min.js') !!}
-		{!! HTML::script('nova/resources/js/vue/components.js') !!}
-		{!! HTML::script('nova/resources/js/vue/filters.js') !!}
+		<script src="https://unpkg.com/vue@2.2.4"></script>
+		<script src="https://unpkg.com/axios@0.15.3/dist/axios.min.js"></script>
+		{!! HTML::script('nova/dist/js/all.js') !!}
 		<script>
 			// Destroy all modals when they're hidden
 			$('.modal').on('hidden.bs.modal', function() {
@@ -73,34 +67,6 @@
 				vm.$compile($(this).get(0))
 			})
 
-			/*// Setup the CSRF token on Ajax requests
-			$.ajaxPrefilter(function (options, originalOptions, xhr) {
-				var token = $('meta[name="csrf-token"]').attr('content')
-
-				if (token) {
-					return xhr.setRequestHeader('X-CSRF-TOKEN', token)
-				}
-			})
-
-			// Setup the CSRF token on Ajax requests
-			$.ajaxSetup({
-				headers: {
-					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-				}
-			})
-
-			$(document).ajaxError(function(event, xhr, settings, thrownError) {
-				if (xhr.status == 403) {
-					swal({
-						title: "Unauthorized!",
-						text: "You do not have permission to take this action!",
-						type: "error",
-						timer: null,
-						html: true
-					})
-				}
-			})*/
-
 			var vue = {}
 			window.Nova = <?php echo json_encode(Nova::scriptVariables());?>
 
@@ -110,9 +76,8 @@
 					text: "{{ session('flash_message.message') }}",
 					type: "{{ session('flash_message.level') }}",
 					timer: 2250,
-					showConfirmButton: false,
-					html: true
-				})
+					showConfirmButton: false
+				}).then(function () {}, function (dismiss) {})
 			@endif
 
 			@if (session()->has('flash_message_overlay'))
@@ -120,10 +85,9 @@
 					title: "{{ session('flash_message_overlay.title') }}",
 					text: "{{ session('flash_message_overlay.message') }}",
 					type: "{{ session('flash_message_overlay.level') }}",
-					timer: null,
-					confirmButtonText: "OK",
-					html: true,
-					allowOutsideClick: true
+					confirmButtonText: "{{ _m('ok') }}",
+					confirmButtonClass: 'btn btn-success',
+					buttonsStyling: false
 				})
 			@endif
 
@@ -132,30 +96,13 @@
 				$('.js-tooltip-bottom').tooltip({ placement: 'bottom' })
 				$('.js-tooltip-left').tooltip({ placement: 'left' })
 				$('.js-tooltip-right').tooltip({ placement: 'right' })
-
-				$('.editor').summernote({
-					height: 250,
-					toolbar: [
-						['style', ['bold', 'italic', 'underline', 'clear']],
-						['font', ['superscript', 'subscript']],
-						['fontsize', ['fontsize']],
-						['para', ['ul', 'ol', 'paragraph']],
-						['insert', ['picture', 'link', 'table', 'hr']],
-						['misc', ['codeview']]
-					]
-				})
 			})
 		</script>
 		{!! $scripts or false !!}
 		<script>
 			var vm = new Vue({
 				el: '#app',
-				mixins: [vue],
-				http: {
-					headers: {
-						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-					}
-				}
+				mixins: [vue]
 			})
 		</script>
 	</body>
