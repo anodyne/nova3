f5bee77cffeb5cf3f32474b184d0e306f0170778
David VanScott <david.vanscott@gmail.com>
2017-03-16 07:09:10 -0400

    Temporary indicator for RWD

--- nova/resources/views/components/structures/public.blade.php
+++ nova/resources/views/components/structures/public.blade.php
@@ -59,6 +59,14 @@
 	<body>
 		{!! $template or false !!}
 
+		<div class="container">
+			<code class="hidden-sm-up">xs</code>
+			<code class="hidden-xs-down hidden-md-up">sm</code>
+			<code class="hidden-sm-down hidden-lg-up">md</code>
+			<code class="hidden-md-down hidden-xl-up">lg</code>
+			<code class="hidden-lg-down">xl</code>
+		</div>
+
 		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
