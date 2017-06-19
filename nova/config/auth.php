83b9ae3f31dfee508803de10cfb3fbbb57771a68
David VanScott <david.vanscott@gmail.com>
2017-06-19 16:17:50 -0400

    Config updates

--- nova/config/auth.php
+++ nova/config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => Nova\Foundation\User::class,
         ],
 
         // 'users' => [
