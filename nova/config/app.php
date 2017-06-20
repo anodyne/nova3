3bdf21ced239c7f007f817bab7ae8713146290d9
David VanScott <david.vanscott@gmail.com>
2017-06-20 12:50:18 -0400

    Add an alias for the Str class

--- nova/config/app.php
+++ nova/config/app.php
@@ -226,6 +226,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Str' => Illuminate\Support\Str::class,
+
     ],
 
 ];
