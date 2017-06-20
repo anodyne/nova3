9bb28c711966b6c512d6053499fd9c2ca318a2cc
David VanScott <david.vanscott@gmail.com>
2017-06-20 14:42:47 -0400

    Add some tests for the i18n loader

--- nova/src/Foundation/TranslationFileLoader.php
+++ nova/src/Foundation/TranslationFileLoader.php
@@ -7,6 +7,8 @@ use Illuminate\Translation\FileLoader as BaseFileLoader;
 class TranslationFileLoader extends BaseFileLoader
 {
 	protected $novaPath;
+	protected $novaLangItems = [];
+	protected $appLangItems = [];
 
 	public function __construct(Filesystem $files, $path, $novaPath)
 	{
@@ -28,19 +30,29 @@ class TranslationFileLoader extends BaseFileLoader
 		return $this->loadNamespaced($locale, $group, $namespace);
 	}
 
+	public function getAppLangItems(): array
+	{
+		return $this->appLangItems;
+	}
+
+	public function getNovaLangItems(): array
+	{
+		return $this->novaLangItems;
+	}
+
 	protected function loadJsonPath($path, $locale)
 	{
 		$novaLang = [];
 		$appLang = [];
 
 		if ($this->files->exists($fullCore = "{$this->novaPath}/{$locale}.json")) {
-			$novaLang = json_decode($this->files->get($fullCore), true);
+			$this->novaLangItems = json_decode($this->files->get($fullCore), true);
 		}
 
 		if ($this->files->exists($fullApp = "{$this->path}/{$locale}.json")) {
-			$appLang = json_decode($this->files->get($fullApp), true);
+			$this->appLangItems = json_decode($this->files->get($fullApp), true);
 		}
 
-		return array_replace_recursive($novaLang, $appLang);
+		return array_replace_recursive($this->novaLangItems, $this->appLangItems);
 	}
 }
