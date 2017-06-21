f4ae068cf7209c780af14cd917bd5f8793279e6c
David VanScott <david.vanscott@gmail.com>
2017-06-21 18:04:42 -0400

    Updates for creating a role

--- nova/resources/views/pages/authorize/create-role.blade.php
+++ nova/resources/views/pages/authorize/create-role.blade.php
@@ -15,9 +15,57 @@
 			</div>
 		</div>
 
+		<div class="row">
+			<div class="col-md-8">
+				<fieldset>
+					<legend>{{ _m('authorize-permissions') }}</legend>
+					
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<input type="text"
+									   class="form-control"
+									   placeholder="{{ _m('authorize-permission-find') }}"
+									   v-model="permissionSearch">
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-4 mb-3" v-for="permission in filteredPermissions">
+							<input type="checkbox" name="permissions[]" :value="permission.id"> @{{ permission.name }}
+						</div>
+					</div>
+				</fieldset>
+			</div>
+		</div>
+
 		<div class="form-group">
 			<button type="submit" class="btn btn-primary">{{ _m('authorize-role-add') }}</button>
 			<a href="{{ route('roles.index') }}" class="btn btn-link">{{ _m('cancel') }}</a>
 		</div>
 	{!! Form::close() !!}
+@endsection
+
+@section('js')
+	<script>
+		vue = {
+			data: {
+				permissions: {!! $permissions !!},
+				permissionSearch: ''
+			},
+
+			computed: {
+				filteredPermissions () {
+					let self = this
+
+					return self.permissions.filter(function (permission) {
+						let searchRegex = new RegExp(self.permissionSearch, 'i')
+
+						return searchRegex.test(permission.name) || searchRegex.test(permission.key)
+					})
+				}
+			}
+		}
+	</script>
 @endsection
