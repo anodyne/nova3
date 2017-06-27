c8e4eca9320cd6c94c3d75c2a9977f6ade2f8b90
David VanScott <david.vanscott@gmail.com>
2017-06-27 12:47:09 -0400

    Add some seed data for authorization

--- nova/database/migrations/2017_06_20_192633_create_authorization_tables.php
+++ nova/database/migrations/2017_06_20_192633_create_authorization_tables.php
@@ -1,5 +1,7 @@
 <?php
 
+use Nova\Authorize\Role;
+use Nova\Authorize\Permission;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -38,6 +40,8 @@ class CreateAuthorizationTables extends Migration
 			$table->foreign('permission_id')->references('id')->on('permissions');
 			$table->foreign('role_id')->references('id')->on('roles');
 		});
+
+		$this->seed();
 	}
 
 	public function down()
@@ -46,5 +50,36 @@ class CreateAuthorizationTables extends Migration
 		Schema::dropIfExists('users_roles');
 		Schema::dropIfExists('permissions');
 		Schema::dropIfExists('roles');
+	}
+
+	public function seed()
+	{
+		// Create permissions
+		$permissions = [
+			['name' => "Create roles", 'key' => "role.create"],
+			['name' => "Update roles", 'key' => "role.update"],
+			['name' => "Delete roles", 'key' => "role.delete"],
+
+			['name' => "Create permissions", 'key' => "permission.create"],
+			['name' => "Update permissions", 'key' => "permission.update"],
+			['name' => "Delete permissions", 'key' => "permission.delete"],
+
+			['name' => "Create users", 'key' => "user.create"],
+			['name' => "Update users", 'key' => "user.update"],
+			['name' => "Delete users", 'key' => "user.delete"],
+		];
+
+		foreach ($permissions as $permission) {
+			Permission::create($permission);
+		}
+
+		// Create roles
+		$roles = [
+			['name' => 'System Admin']
+		];
+
+		foreach ($roles as $role) {
+			Role::create($role);
+		}
 	}
 }
