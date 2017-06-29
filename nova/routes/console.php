67bdb88b24b8884934b7fb2f4147b26c5bcc60f5
David VanScott <david.vanscott@gmail.com>
2017-06-29 16:44:05 -0400

    Add a refresh demo command

--- nova/routes/console.php
+++ nova/routes/console.php
@@ -2,17 +2,38 @@
 
 use Illuminate\Foundation\Inspiring;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
-
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+	$this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('nova:refresh-demo', function () {
+	// Flush the caches
+	Cache::flush();
+	Session::flush();
+	Artisan::call('config:clear');
+	Artisan::call('route:clear');
+	$this->info('Cleared system caches.');
+
+	// Refresh the migrations
+	Artisan::call('migrate:refresh', ['--force' => true]);
+	$this->info('Refreshed database.');
+
+	// Create a user with the System Admin role
+	$admin = factory('Nova\Users\User')->create([
+		'name' => "John Admin",
+		'email' => "admin@example.com",
+		'remember_token' => null
+	]);
+	$admin->attachRole(Nova\Authorize\Role::name('System Admin')->first());
+	$admin->attachRole(Nova\Authorize\Role::name('Active User')->first());
+	$this->info('Created user with the System Admin and Active User roles.');
+
+	// Create a user with the Active User role
+	$active = factory('Nova\Users\User')->create([
+		'name' => "Susie User",
+		'email' => 'user@example.com',
+		'remember_token' => null
+	]);
+	$active->attachRole(Nova\Authorize\Role::name('Active User')->first());
+	$this->info('Created user with the Active User role.');
+});
--- nova/src/Foundation/Console/Kernel.php
+++ nova/src/Foundation/Console/Kernel.php
@@ -5,32 +5,16 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-	/**
-	 * The Artisan commands provided by your application.
-	 *
-	 * @var array
-	 */
 	protected $commands = [
 		//
 	];
 
-	/**
-	 * Define the application's command schedule.
-	 *
-	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-	 * @return void
-	 */
 	protected function schedule(Schedule $schedule)
 	{
-		// $schedule->command('inspire')
-		//          ->hourly();
+		$schedule->command('nova:refresh-demo')
+				 ->dailyAt('04:30');
 	}
 
-	/**
-	 * Register the Closure based commands for the application.
-	 *
-	 * @return void
-	 */
 	protected function commands()
 	{
 		require base_path('nova/routes/console.php');
