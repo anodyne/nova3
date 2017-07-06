426e3578203bd6550781309a008d7c5227093084
David VanScott <david.vanscott@gmail.com>
2017-07-06 08:16:15 -0400

    Add the status class

---
+++ nova/src/Foundation/Status.php
@@ -0,0 +1,14 @@
+<?php namespace Nova\Foundation;
+
+class Status
+{
+	const PENDING		= 1;
+	const ACTIVE		= 2;
+	const INACTIVE		= 3;
+	const REMOVED		= 4;
+	const IN_PROGRESS	= 5;
+	const APPROVED		= 6;
+	const REJECTED		= 7;
+	const ASSIGNED		= 8;
+	const UNASSIGNED	= 9;
+}
