675f0df03bb884d4bc239cb25d7c5ff4198423f4
David VanScott <david.vanscott@gmail.com>
2017-07-06 08:15:10 -0400

    Clean up the routes file

--- routes.php
+++ routes.php
@@ -1,11 +1,7 @@
 <?php
 
 Route::get('test', function () {
-	$theme = new Nova\Foundation\Theme\Theme;
-
-	session()->flash('flash', 'message');
-
-	//dd($theme->iconMap(), $theme->getIcon('delete'), $theme->renderIcon('delete', 'fa-2x'));
+	//
 });
 
 Route::get('status', function () {
