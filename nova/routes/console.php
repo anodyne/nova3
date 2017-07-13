ff92b01b11745ac75f10f478fbd244229be66f98
David VanScott <david.vanscott@gmail.com>
2017-07-12 21:56:27 -0400

    Add a command to quickly refresh everything

--- nova/routes/console.php
+++ nova/routes/console.php
@@ -6,6 +6,30 @@ Artisan::command('inspire', function () {
 	$this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
 
+Artisan::command('nova:refresh', function () {
+	// Flush the caches
+	Cache::flush();
+	Session::flush();
+	Artisan::call('config:clear');
+	Artisan::call('route:clear');
+	$this->info('Cleared system caches.');
+
+	// Refresh the migrations
+	Artisan::call('migrate:refresh', ['--force' => true]);
+	$this->info('Refreshed database.');
+
+	// Create a user with the System Admin role
+	$admin = factory('Nova\Users\User')->create([
+		'name' => "David",
+		'email' => "david.vanscott@gmail.com",
+		'remember_token' => null,
+		'status' => Status::ACTIVE,
+	]);
+	$admin->attachRole(Nova\Authorize\Role::name('System Admin')->first());
+	$admin->attachRole(Nova\Authorize\Role::name('Active User')->first());
+	$this->info('Created user with the System Admin and Active User roles.');
+});
+
 Artisan::command('nova:refresh-demo', function () {
 	// Flush the caches
 	Cache::flush();
@@ -22,7 +46,8 @@ Artisan::command('nova:refresh-demo', function () {
 	$admin = factory('Nova\Users\User')->create([
 		'name' => "John Admin",
 		'email' => "admin@example.com",
-		'remember_token' => null
+		'remember_token' => null,
+		'status' => Status::ACTIVE,
 	]);
 	$admin->attachRole(Nova\Authorize\Role::name('System Admin')->first());
 	$admin->attachRole(Nova\Authorize\Role::name('Active User')->first());
@@ -32,7 +57,8 @@ Artisan::command('nova:refresh-demo', function () {
 	$active = factory('Nova\Users\User')->create([
 		'name' => "Susie User",
 		'email' => 'user@example.com',
-		'remember_token' => null
+		'remember_token' => null,
+		'status' => Status::ACTIVE,
 	]);
 	$active->attachRole(Nova\Authorize\Role::name('Active User')->first());
 	$this->info('Created user with the Active User role.');
