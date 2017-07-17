59477172060f29eddef8f0f6c10f8337505e7407
David VanScott <david.vanscott@gmail.com>
2017-07-17 10:06:50 -0400

    Update the refresh command

--- nova/routes/console.php
+++ nova/routes/console.php
@@ -20,46 +20,22 @@ Artisan::command('nova:refresh', function () {
 
 	// Create a user with the System Admin role
 	$admin = factory('Nova\Users\User')->create([
-		'name' => "David",
-		'email' => "david.vanscott@gmail.com",
-		'remember_token' => null,
-		'status' => Status::ACTIVE,
-	]);
-	$admin->attachRole(Nova\Authorize\Role::name('System Admin')->first());
-	$admin->attachRole(Nova\Authorize\Role::name('Active User')->first());
-	$this->info('Created user with the System Admin and Active User roles.');
-});
-
-Artisan::command('nova:refresh-demo', function () {
-	// Flush the caches
-	Cache::flush();
-	Session::flush();
-	Artisan::call('config:clear');
-	Artisan::call('route:clear');
-	$this->info('Cleared system caches.');
-
-	// Refresh the migrations
-	Artisan::call('migrate:refresh', ['--force' => true]);
-	$this->info('Refreshed database.');
-
-	// Create a user with the System Admin role
-	$admin = factory('Nova\Users\User')->create([
-		'name' => "John Admin",
+		'name' => "Adam Doe",
 		'email' => "admin@example.com",
 		'remember_token' => null,
 		'status' => Status::ACTIVE,
 	]);
 	$admin->attachRole(Nova\Authorize\Role::name('System Admin')->first());
 	$admin->attachRole(Nova\Authorize\Role::name('Active User')->first());
-	$this->info('Created user with the System Admin and Active User roles.');
 
 	// Create a user with the Active User role
-	$active = factory('Nova\Users\User')->create([
-		'name' => "Susie User",
-		'email' => 'user@example.com',
+	$user = factory('Nova\Users\User')->create([
+		'name' => "Ben Doe",
+		'email' => "user@example.com",
 		'remember_token' => null,
 		'status' => Status::ACTIVE,
 	]);
-	$active->attachRole(Nova\Authorize\Role::name('Active User')->first());
-	$this->info('Created user with the Active User role.');
+	$user->attachRole(Nova\Authorize\Role::name('Active User')->first());
+	
+	$this->info('Created test users.');
 });
