dcb3e11c0191a9854c5badcd0b56e8ae57af386d
David VanScott <david.vanscott@gmail.com>
2017-07-18 00:57:57 -0400

    Add some rank data

--- nova/database/migrations/2017_07_10_161432_create_genre_tables.php
+++ nova/database/migrations/2017_07_10_161432_create_genre_tables.php
@@ -1,5 +1,8 @@
 <?php
 
+use Nova\Genres\Rank;
+use Nova\Genres\RankInfo;
+use Nova\Genres\RankGroup;
 use Nova\Genres\Department;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
@@ -56,6 +59,7 @@ class CreateGenreTables extends Migration
 		});
 
 		$this->departments();
+		$this->ranks();
 	}
 
 	public function down()
@@ -140,5 +144,66 @@ class CreateGenreTables extends Migration
 		foreach ($data as $department) {
 			Department::create($department);
 		}
+	}
+
+	protected function ranks()
+	{
+		$groups = [
+			['name' => 'Admiralty', 'order' => 0],
+			['name' => 'Command', 'order' => 1],
+			['name' => 'Operations', 'order' => 2],
+			['name' => 'Sciences', 'order' => 3],
+			['name' => 'Marines', 'order' => 4],
+		];
+
+		$info = [
+			['name' => 'Admiral', 'short_name' => 'ADM', 'order' => 0],
+			['name' => 'Vice Admiral', 'short_name' => 'VADM', 'order' => 1],
+			['name' => 'Rear Admiral', 'short_name' => 'RADM', 'order' => 2],
+			['name' => 'Commodore', 'short_name' => 'COMO', 'order' => 3],
+			['name' => 'Captain', 'short_name' => 'CAPT', 'order' => 4],
+			['name' => 'Commander', 'short_name' => 'CMDR', 'order' => 5],
+			['name' => 'Lieutenant Commander', 'short_name' => 'LT CMDR', 'order' => 6],
+			['name' => 'Lieutenant', 'short_name' => 'LT', 'order' => 7],
+			['name' => 'Lieutenant (JG)', 'short_name' => 'LT JG', 'order' => 8],
+			['name' => 'Ensign', 'short_name' => 'ENS', 'order' => 9],
+			['name' => 'Master Chief Petty Officer', 'short_name' => 'MCPO', 'order' => 10],
+			['name' => 'Senior Chief Petty Officer', 'short_name' => 'SCPO', 'order' => 11],
+			['name' => 'Chief Petty Officer', 'short_name' => 'CPO', 'order' => 12],
+			['name' => 'Petty Officer 1st Class', 'short_name' => 'PO1', 'order' => 13],
+			['name' => 'Petty Officer 2nd Class', 'short_name' => 'PO2', 'order' => 14],
+			['name' => 'Petty Officer 3rd Class', 'short_name' => 'PO3', 'order' => 15],
+			['name' => 'Crewman 1st Class', 'short_name' => 'CR1', 'order' => 16],
+			['name' => 'Crewman 2nd Class', 'short_name' => 'CR2', 'order' => 17],
+			['name' => 'Crewman 3rd Class', 'short_name' => 'CR3', 'order' => 18],
+
+			['name' => 'General', 'short_name' => 'GEN', 'order' => 20],
+			['name' => 'Lieutenant General', 'short_name' => 'LT GEN', 'order' => 21],
+			['name' => 'Major General', 'short_name' => 'MAJ GEN', 'order' => 22],
+			['name' => 'Brigadier General', 'short_name' => 'BRG GEN', 'order' => 23],
+			['name' => 'Colonel', 'short_name' => 'COL', 'order' => 24],
+			['name' => 'Lieutenant Colonel', 'short_name' => 'LT COL', 'order' => 25],
+			['name' => 'Major', 'short_name' => 'MAJ', 'order' => 26],
+			['name' => 'Captain', 'short_name' => 'MCAPT', 'order' => 27],
+			['name' => '1st Lieutenant', 'short_name' => '1LT', 'order' => 28],
+			['name' => '2nd Lieutenant', 'short_name' => '2LT', 'order' => 29],
+			['name' => 'Sergeant Major', 'short_name' => 'SGT MAJ', 'order' => 30],
+			['name' => 'Master Sergeant', 'short_name' => 'MSGT', 'order' => 31],
+			['name' => 'Gunnery Sergeant', 'short_name' => 'GSGT', 'order' => 32],
+			['name' => 'Staff Sergeant', 'short_name' => 'SSGT', 'order' => 33],
+			['name' => 'Sergeant', 'short_name' => 'SGT', 'order' => 34],
+			['name' => 'Corporal', 'short_name' => 'CPL', 'order' => 35],
+			['name' => 'Private 1st Class', 'short_name' => 'PVT1', 'order' => 36],
+			['name' => 'Private E-2', 'short_name' => 'PVT E2', 'order' => 37],
+			['name' => 'Private E-1', 'short_name' => 'PVT E1', 'order' => 38],
+		];
+
+		collect($groups)->each(function ($group) {
+			factory(RankGroup::class)->create($group);
+		});
+
+		collect($info)->each(function ($i) {
+			factory(RankInfo::class)->create($i);
+		});
 	}
 }
