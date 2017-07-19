e1e6424f0bf266ec77f2f1f92622fa4ea723088f
David VanScott <david.vanscott@gmail.com>
2017-07-19 16:10:08 -0400

    Delete positions when a dept is deleted

--- nova/src/Genres/Data/DepartmentDeletor.php
+++ nova/src/Genres/Data/DepartmentDeletor.php
@@ -1,5 +1,6 @@
 <?php namespace Nova\Genres\Data;
 
+use Nova\Genres\Position;
 use Nova\Foundation\Data\BindsData;
 use Nova\Foundation\Data\Deletable;
 
@@ -9,6 +10,11 @@ class DepartmentDeletor implements Deletable
 
 	public function delete($department)
 	{
+		// Delete all of the positions in the department
+		$department->positions->each(function ($position) {
+			deletor(Position::class)->delete($position);
+		});
+
 		// Delete the department
 		$department->delete();
 
