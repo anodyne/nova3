0f3d8171360c13a5a86b382984f16a77f29cccca
David VanScott <david.vanscott@gmail.com>
2017-07-24 21:46:32 -0400

    Minor updates to the menus

--- nova/resources/views/layouts/app.blade.php
+++ nova/resources/views/layouts/app.blade.php
@@ -43,8 +43,8 @@
 								<a class="dropdown-item" href="{{ route('positions.index') }}">Positions</a>
 								<a class="dropdown-item" href="{{ route('ranks.index') }}">Ranks</a>
 								<div class="dropdown-divider"></div>
+								{{-- <a class="dropdown-item" href="{{ route('users.index') }}">Characters</a> --}}
 								<a class="dropdown-item" href="{{ route('roles.index') }}">Roles</a>
-								<div class="dropdown-divider"></div>
 								<a class="dropdown-item" href="{{ route('users.index') }}">Users</a>
 							</div>
 						</div>
--- nova/resources/views/layouts/welcome.blade.php
+++ nova/resources/views/layouts/welcome.blade.php
@@ -152,7 +152,6 @@
 							<a class="dropdown-item" href="{{ route('ranks.index') }}">Ranks</a>
 							<div class="dropdown-divider"></div>
 							<a class="dropdown-item" href="{{ route('roles.index') }}">Roles</a>
-							<div class="dropdown-divider"></div>
 							<a class="dropdown-item" href="{{ route('users.index') }}">Users</a>
 						</div>
 					</span>
