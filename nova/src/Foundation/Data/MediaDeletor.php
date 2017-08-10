17342c1470a474b946f48b632d1d15481aea5a7a
David VanScott <david.vanscott@gmail.com>
2017-08-10 06:35:21 -0400

    Update media delete process
    If the media item being deleted is a primary item, we need to make sure that another item is marked as the primary

--- nova/src/Foundation/Data/MediaDeletor.php
+++ nova/src/Foundation/Data/MediaDeletor.php
@@ -14,6 +14,19 @@ class MediaDeletor implements Deletable
 			$media->filename
 		]);
 
+		// If the image we're deleting is the primary, we've got some work to do...
+		if ($media->primary === (int) true) {
+			// Make sure we have all the media, but exclude the image we're deleting
+			$allMedia = $media->mediable->media->filter(function ($m) use ($media) {
+				return $m->id != $media->id;
+			});
+
+			// As long as there's something left, set the first one as the primary
+			if ($allMedia->count() > 0) {
+				$allMedia->first()->update(['primary' => (int) true]);
+			}
+		}
+
 		// Delete the file
 		Storage::disk('public')->delete($path);
 
