eb1e7950d9b846ed7c03f66e35e66f177e694f29
David VanScott <david.vanscott@gmail.com>
2017-08-14 14:29:12 -0400

    Yield the title (finally)

--- nova/resources/views/layouts/app.blade.php
+++ nova/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<title>{{ config('app.name', 'Laravel') }}</title>
+	<title>@yield('title') &bull; {{ config('app.name', 'Laravel') }}</title>
 
 	<script defer src="{{ asset('assets/js/packs/regular.min.js') }}"></script>
 	<script defer src="{{ asset('assets/js/fontawesome.min.js') }}"></script>
