2f6a75081f3a7f1e1bee83e33d9f1b3a841ccc41
David VanScott <david.vanscott@gmail.com>
2017-08-15 16:52:05 -0400

    Working on the manifest

--- nova/resources/views/pages/characters/manifest.blade.php
+++ nova/resources/views/pages/characters/manifest.blade.php
@@ -6,16 +6,32 @@
 	<h1>Manifest</h1>
 
 	<div class="row">
-		<div class="col-md-5">
-			<div class="form-group input-group">
-				<input type="text"
-					   class="form-control"
-					   placeholder="Find characters, departments, or positions"
-					   v-model="search">
-				<span class="input-group-btn">
-					<a href="#" class="btn btn-secondary" @click.prevent="search = ''">{!! icon('close') !!}</a>
-				</span>
+		<div class="col">
+			<div class="row">
+				<div class="col-md-5">
+					<div class="form-group input-group">
+						<input type="text"
+							   class="form-control"
+							   placeholder="Find characters, departments, or positions"
+							   v-model="search">
+						<span class="input-group-btn">
+							<a href="#" class="btn btn-secondary" @click.prevent="search = ''">{!! icon('close') !!}</a>
+						</span>
+					</div>
+				</div>
 			</div>
+		</div>
+		<div class="col col-auto">
+			<a class="btn btn-secondary btn-action js-popover"
+			   id="manifest-filters-trigger"
+			   tabindex="0"
+			   data-placement="bottom"
+			   title="Filter Manifest">{!! icon('filter') !!}</a>
+			<a class="btn btn-secondary btn-action js-popover ml-1"
+			   id="manifest-options-trigger"
+			   tabindex="0"
+			   data-placement="bottom"
+			   title="Manifest Options">{!! icon('settings-alt') !!}</a>
 		</div>
 	</div>
 
@@ -35,7 +51,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="col" v-show="position.characters.length == 0">
+				<div class="col" v-show="position.characters.length == 0 && showAvailable">
 					<div class="row align-items-center">
 						<div class="col col-auto">
 							<rank></rank>
@@ -66,7 +82,7 @@
 								</div>
 							</div>
 						</div>
-						<div class="col" v-show="subPosition.characters.length == 0">
+						<div class="col" v-show="subPosition.characters.length == 0 && showAvailable">
 							<div class="row align-items-center">
 								<div class="col col-auto">
 									<rank></rank>
@@ -82,6 +98,59 @@
 			</fieldset>
 		</div>
 	</fieldset>
+
+	<div class="d-none">
+		<div id="manifest-filters-content">
+			<div class="form-group">
+				<label class="custom-control custom-checkbox">
+					<input type="checkbox" class="custom-control-input">
+					<span class="custom-control-indicator"></span>
+					<span class="custom-control-description">Primary characters</span>
+				</label>
+			</div>
+
+			<div class="form-group">
+				<label class="custom-control custom-checkbox">
+					<input type="checkbox" class="custom-control-input">
+					<span class="custom-control-indicator"></span>
+					<span class="custom-control-description">NPCs</span>
+				</label>
+			</div>
+
+			<label class="custom-control custom-checkbox">
+				<input type="checkbox" class="custom-control-input" v-model="showAvailable">
+				<span class="custom-control-indicator"></span>
+				<span class="custom-control-description">Available positions</span>
+			</label>
+
+		</div>
+
+		<div id="manifest-options-content">
+			<div class="form-group">
+				<label class="custom-control custom-radio d-flex align-items-center">
+					<input name="layout" type="radio" value="list" class="custom-control-input" v-model="layout">
+					<span class="custom-control-indicator"></span>
+					<span class="custom-control-description d-flex align-items-center">
+						<i class="far fa-list fa-lg mr-2 text-muted"></i>
+						<span>List</span>
+					</span>
+				</label>
+			</div>
+
+			<div class="form-group">
+				<label class="custom-control custom-radio d-flex align-items-center">
+					<input name="layout" type="radio" value="card" class="custom-control-input" v-model="layout">
+					<span class="custom-control-indicator"></span>
+					<span class="custom-control-description d-flex align-items-center">
+						<i class="far fa-address-card fa-lg mr-2 text-muted"></i>
+						<span>Cards</span>
+					</span>
+				</label>
+			</div>
+
+			<p class="text-info mb-0"><strong>Note:</strong> Manifest layout changes will apply to all visitors.</p>
+		</div>
+	</div>
 @endsection
 
 @section('js')
@@ -89,13 +158,41 @@
 		vue = {
 			data: {
 				departments: {!! $departments !!},
-				search: ''
+				layout: 'list',
+				search: '',
+				showAvailable: true
 			},
 
 			computed: {
 				filteredDepartments () {
 					return filter(this.departments, this.search)
 				}
+			},
+
+			mounted () {
+				$('#manifest-filters-trigger').on('click', function (e) {
+					e.preventDefault()
+					$('#manifest-options-trigger').popover('hide')
+				}).popover({
+					container: 'body',
+					content: $('#manifest-filters-content'),
+					html: true,
+					trigger: 'focus'
+				})
+
+				$('#manifest-options-trigger').on('click', function (e) {
+					e.preventDefault()
+					$('#manifest-filters-trigger').popover('hide')
+				}).popover({
+					container: 'body',
+					content: $('#manifest-options-content'),
+					html: true,
+					trigger: 'focus'
+				})
+
+				$('body').on('click', function (e) {
+					console.log(e.target.parentElement)
+				})
 			}
 		}
 
