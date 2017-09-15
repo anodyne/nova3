738ffcd2486fb4f45440ce902ed02ef351b46de0
David VanScott <david.vanscott@gmail.com>
2017-09-15 09:12:40 -0400

    Better statuses for avatars

--- nova/resources/assets/js/components/Avatar.vue
+++ nova/resources/assets/js/components/Avatar.vue
@@ -156,15 +156,27 @@
 				}
 
 				if (this.isCharacter) {
-					if (this.item.user) {
+					if (this.item.user && this.item.status == 2) {
 						if (this.item.isPrimaryCharacter) {
-							return this._m('characters-primary-of', {2:this.item.user.displayName});
+							return this.lang('characters-primary-of', {2:this.item.user.displayName});
 						} else {
-							return this._m('characters-pnpc-of', {2:this.item.user.displayName});
+							return this.lang('characters-character-of', {2:this.item.user.displayName});
 						}
 					}
 
-					return this._m('characters-npc');
+					if (this.item.status == 1) {
+						return this.lang('characters-pending');
+					}
+
+					if (this.item.status == 3) {
+						return this.lang('characters-inactive');
+					}
+
+					if (this.item.status == 4) {
+						return this.lang('characters-removed');
+					}
+
+					return this.lang('characters-npc');
 				}
 
 				return null;
@@ -172,8 +184,8 @@
 		},
 
 		methods: {
-			_m (key, variables = '') {
-				return window._m(key, variables);
+			lang (key, variables = '') {
+				return window.lang(key, variables);
 			}
 		}
 	};
