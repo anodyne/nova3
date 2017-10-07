91d27dc4b2f87a62385cea5f340cd0862835bc43
David VanScott <david.vanscott@gmail.com>
2017-10-07 11:44:50 -0400

    Update the status doc

--- nova/resources/views/pages/status.blade.php
+++ nova/resources/views/pages/status.blade.php
@@ -25,74 +25,14 @@
 			<div class="card">
 				<div class="card-body">
 					<h3 class="d-flex align-items-center justify-content-start mb-0">
-						<span class="mr-auto">Characters</span>
+						<span class="mr-auto">Setup</span>
 						<i class="fa fa-code fa-fw"></i>
 					</h3>
-				</div>
-			</div>
-
-			<div class="card text-white bg-success">
-				<div class="card-body">
-					<h3 class="d-flex align-items-center justify-content-start">
-						<span class="mr-auto">Genre Assets</span>
-						<i class="fa fa-check-circle fa-fw"></i>
-					</h3>
 
 					<ul>
-						<li>Authorized users can create, delete, reorder, and update departments</li>
-						<li>Authorized users can create, delete, reorder, and update positions</li>
-						<li>Authorized users can create, delete, reorder, duplicate, and update rank groups</li>
-						<li>Authorized users can create, delete, reorder, and update rank info</li>
-						<li>Authorized users can create, delete, reorder, dupliate, and update ranks</li>
-						<li>Genre data for departments, positions, rank groups, rank info, and ranks is inserted during install</li>
-					</ul>
-				</div>
-			</div>
-
-			<div class="card text-white bg-success">
-				<div class="card-body">
-					<h3 class="d-flex align-items-center justify-content-start">
-						<span class="mr-auto">Users</span>
-						<i class="fa fa-check-circle fa-fw"></i>
-					</h3>
-
-					<ul>
-						<li>Authorized users can create, update, delete, and restore users</li>
-						<li>Authorized users can force a password reset</li>
-						<li>Authenticated users can view the profile of any user</li>
-						<li>Users can update their user profile</li>
-						<li>Users can update their password</li>
-					</ul>
-				</div>
-			</div>
-
-			<div class="card text-white bg-success">
-				<div class="card-body">
-					<h3 class="d-flex align-items-center justify-content-start">
-						<span class="mr-auto">Authorization</span>
-						<i class="fa fa-check-circle fa-fw"></i>
-					</h3>
-
-					<ul>
-						<li>Authorized users can create, update, and delete roles</li>
-						<li>Authorized users can create, update, and delete permissions</li>
-					</ul>
-				</div>
-			</div>
-
-			<div class="card text-white bg-success">
-				<div class="card-body">
-					<h3 class="d-flex align-items-center justify-content-start">
-						<span class="mr-auto">Authentication</span>
-						<i class="fa fa-check-circle fa-fw"></i>
-					</h3>
-
-					<ul>
-						<li>Users can sign in</li>
-						<li>Users can sign out</li>
-						<li>Users can reset their password</li>
-						<li>Users are locked out for 30 minutes after 5 unsuccessful sign in attempts</li>
-						<li>If an admin forces a reset, the user is redirected to the password reset screen on their next sign in attempt</li>
+						<li>Update Nova</li>
+						<li>Migrate from Nova 2</li>
+						<li>Uninstall Nova</li>
 					</ul>
 				</div>
 			</div>
@@ -100,8 +40,49 @@
 			<div class="card">
 				<div class="card-body">
 					<h3 class="d-flex align-items-center justify-content-start mb-0">
-						<span class="mr-auto">Setup</span>
-						<i class="fa fa-road fa-fw"></i>
+						<span class="mr-auto">Characters</span>
+						<i class="fa fa-code fa-fw"></i>
+					</h3>
+
+					<ul>
+						<li>Card-style manifest</li>
+						<li>Character bio</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="card text-white bg-success">
+				<div class="card-body">
+					<h3 class="d-flex align-items-center justify-content-start mb-0">
+						<span class="mr-auto">Genre Assets</span>
+						<i class="fa fa-check-circle fa-fw"></i>
+					</h3>
+				</div>
+			</div>
+
+			<div class="card text-white bg-success">
+				<div class="card-body">
+					<h3 class="d-flex align-items-center justify-content-start mb-0">
+						<span class="mr-auto">Users</span>
+						<i class="fa fa-check-circle fa-fw"></i>
+					</h3>
+				</div>
+			</div>
+
+			<div class="card text-white bg-success">
+				<div class="card-body">
+					<h3 class="d-flex align-items-center justify-content-start mb-0">
+						<span class="mr-auto">Authorization</span>
+						<i class="fa fa-check-circle fa-fw"></i>
+					</h3>
+				</div>
+			</div>
+
+			<div class="card text-white bg-success">
+				<div class="card-body">
+					<h3 class="d-flex align-items-center justify-content-start mb-0">
+						<span class="mr-auto">Authentication</span>
+						<i class="fa fa-check-circle fa-fw"></i>
 					</h3>
 				</div>
 			</div>
