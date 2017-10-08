2dc1211e824873ec948fce12b26bae0b47c73384
David VanScott <david.vanscott@gmail.com>
2017-10-08 10:43:00 -0400

    Add manifest layout to seed data

--- nova/database/migrations/2017_07_20_130448_create_settings_table.php
+++ nova/database/migrations/2017_07_20_130448_create_settings_table.php
@@ -31,6 +31,7 @@ class CreateSettingsTable extends Migration
 			['key' => 'mail_default_address', 'value' => '', 'protected' => (int)true],
 			['key' => 'mail_default_name', 'value' => '', 'protected' => (int)true],
 			['key' => 'mail_subject_prefix', 'value' => '', 'protected' => (int)true],
+			['key' => 'manifest_layout', 'value' => 'cards', 'protected' => (int)true],
 			['key' => 'manifest_show_assigned', 'value' => 'true', 'protected' => (int)true],
 			['key' => 'manifest_show_available', 'value' => 'true', 'protected' => (int)true],
 			['key' => 'manifest_show_inactive', 'value' => 'false', 'protected' => (int)true],
