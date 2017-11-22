38385c3fcbaec5a2b1f1d006002db70f61625178
David VanScott <david.vanscott@gmail.com>
2017-11-22 15:43:21 -0500

    Clean up some migrations

--- nova/database/migrations/2017_10_25_195237_create_system_info_table.php
+++ nova/database/migrations/2017_10_25_195237_create_system_info_table.php
@@ -13,12 +13,25 @@ class CreateSystemInfoTable extends Migration
 			$table->increments('id');
 			$table->string('version')->default(config('nova.version'));
 			$table->tinyInteger('install_phase')->default(0);
+			// $table->text('install_checklist')->nullable();
 			$table->tinyInteger('migration_phase')->default(0);
+			// $table->text('migration_checklist')->nullable();
 			$table->tinyInteger('update_phase')->default(0);
 			$table->timestamps();
 		});
 
-		SystemInfo::create([]);
+		// $installChecklist = [
+		// 	['key' => 'settings', 'complete' => false],
+		// 	['key' => 'genre-data', 'complete' => false],
+		// 	['key' => 'ranks', 'complete' => false],
+		// 	['key' => 'email-test', 'complete' => false],
+		// ];
+		$migrationChecklist = [];
+
+		SystemInfo::create([
+			// 'install_checklist' => json_encode($installChecklist),
+			// 'migration_checklist' => json_encode($migrationChecklist),
+		]);
 	}
 
 	public function down()
