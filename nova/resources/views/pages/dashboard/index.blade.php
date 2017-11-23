4647e352a4f0034fb9a550c8702b29b59f04916a
David VanScott <david.vanscott@gmail.com>
2017-11-22 23:45:02 -0500

    Include the checklists
    They don't work, but at least people will see the general idea

--- nova/resources/views/pages/dashboard/index.blade.php
+++ nova/resources/views/pages/dashboard/index.blade.php
@@ -10,8 +10,8 @@
 		<span>Welcome to Nova NextGen's dashboard! The dashboard is currently under construction and will be built out through the development process. If you have suggestions about things that should be on the dashboard, let us know.</span>
 	</div>
 
-	{{-- @include('pages.dashboard._checklist-install')
-	@include('pages.dashboard._checklist-migrate') --}}
+	@include('pages.dashboard._checklist-install')
+	@include('pages.dashboard._checklist-migrate')
 @endsection
 
 @section('js')
