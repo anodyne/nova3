aa7a2a80df7f3d198de08d0ee775c38c0d4499e4
David VanScott <david.vanscott@gmail.com>
2018-06-12 10:28:01 -0400

    More work on pickers

--- nova/resources/assets/js/components.js
+++ nova/resources/assets/js/components.js
@@ -4,10 +4,10 @@ import Rank from './components/Rank.vue'
 import Flash from './components/Flash.vue'
 import Avatar from './components/Avatar.vue'
 // import RankPicker from './components/RankPicker.vue'
-import UserPicker from './components/UserPicker.vue'
+// import UserPicker from './components/UserPicker.vue'
 import MediaManager from './components/MediaManager.vue'
-import PositionPicker from './components/PositionPicker.vue'
-import CharacterPicker from './components/CharacterPicker.vue'
+// import PositionPicker from './components/PositionPicker.vue'
+// import CharacterPicker from './components/CharacterPicker.vue'
 import PositionAvailable from './components/PositionAvailable.vue'
 
 import TextInput from './components/forms/TextInput.vue'
@@ -30,18 +30,18 @@ Vue.component('media-manager', Vue.extend(MediaManager))
 Vue.component('position-available', Vue.extend(PositionAvailable))
 Vue.component('icon', Vue.extend(Icon))
 // Vue.component('rank-picker', Vue.extend(RankPicker))
-Vue.component('user-picker', Vue.extend(UserPicker))
-Vue.component('position-picker', Vue.extend(PositionPicker))
-Vue.component('character-picker', Vue.extend(CharacterPicker))
+// Vue.component('user-picker', Vue.extend(UserPicker))
+// Vue.component('position-picker', Vue.extend(PositionPicker))
+// Vue.component('character-picker', Vue.extend(CharacterPicker))
 
-import NovaPicker from './components/forms/Picker.vue'
-Vue.component('nova-picker', Vue.extend(NovaPicker))
+// import NovaPicker from './components/forms/Picker.vue'
+// Vue.component('nova-picker', Vue.extend(NovaPicker))
 
-import RankPicker from './components/forms/RankPicker.vue'
-Vue.component('rank-picker', Vue.extend(RankPicker))
+// import RankPicker from './components/forms/RankPicker.vue'
+// Vue.component('rank-picker', Vue.extend(RankPicker))
 
-import ResponsiveCodeSample from './components/ResponsiveCodeSample.vue'
-Vue.component('responsive-code-sample', Vue.extend(ResponsiveCodeSample))
+import PickList from './components/forms/Picker.vue'
+Vue.component('pick-list', Vue.extend(PickList))
 
 Vue.component('mobile-view', require('./components/responsive-mobile').default)
 Vue.component('tablet-view', require('./components/responsive-tablet').default)
