8d60dfdd351ed99424d7350adbe7ad2951803e9b
David VanScott <david.vanscott@gmail.com>
2018-07-10 08:24:04 -0400

    Update the sign-in view to match the new theme structure

--- nova/resources/views/components/pages/auth/sign-in.blade.php
+++ nova/resources/views/components/pages/auth/sign-in.blade.php
@@ -1,4 +1,32 @@
-<h1>{{ _m('sign-in') }}</h1>
+<div class="text-center text-4xl font-semibold mb-1 text-black">{{ _m('sign-in') }}</div>
+<div class="text-center text-grey-dark text-sm mb-12">Free access to our dashboard</div>
+
+<form role="form" method="POST" action="{{ route('sign-in') }}">
+	@csrf
+	<input type="hidden" name="remember" value="true">
+
+	<div>
+		<div>
+			<label class="block mb-3 text-black text-sm font-medium">Email Address</label>
+			<input type="email" id="email" name="email" class="block w-full rounded border p-3 bg-transparent focus:outline-none focus:border-blue" placeholder="name@example.com">
+		</div>
+
+		<div class="my-6">
+			<div class="flex items-center justify-between mb-3">
+				<label class="text-black text-sm font-medium">Password</label>
+				<a href="{{ route('password.request') }}" class="no-underline text-grey text-sm hover:underline hover:text-grey-dark" tabindex="-1">{{ _m('auth-forgot-password') }}</a>
+			</div>
+
+			<input type="password" name="password" id="password" class="block w-full rounded border p-3 bg-transparent focus:outline-none focus:border-blue" placeholder="Enter your password">
+		</div>
+
+		<button type="submit" class="w-full button is-primary is-large">Sign In</button>
+
+		<div class="mt-6 text-grey-dark text-xs text-center">Don't have an account yet? <a href="#" class="no-underline text-blue">Sign up.</a></div>
+	</div>
+</form>
+
+{{-- <h1>{{ _m('sign-in') }}</h1>
 
 <form role="form" method="POST" action="{{ route('sign-in') }}">
 	{{ csrf_field() }}
@@ -24,4 +52,4 @@
 	<div class="form-group">
 		<button type="submit" class="button is-primary is-block">{{ _m('sign-in') }}</button>
 	</div>
-</form>
+</form> --}}
