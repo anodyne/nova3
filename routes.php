ee665e06b55d530ac3bb80f2b4cbc0676fd7ed79
David VanScott <david.vanscott@gmail.com>
2018-07-10 08:21:47 -0400

    Clean up the routes file

--- routes.php
+++ routes.php
@@ -1,10 +1,5 @@
 <?php
 
 Route::get('test', function () {
-	view()->share('_user', auth()->user());
-	return view('pages.test');
+	//
 });
-
-Route::get('status', function () {
-	return view('pages.status');
-})->name('status');
