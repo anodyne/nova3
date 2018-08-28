93efb05b9d658878c64255e23e369c9b33a53f41
David VanScott <david.vanscott@gmail.com>
2018-08-28 08:41:53 -0400

    Theme work

--- nova/resources/views/components/pages/themes/all-themes.blade.php
+++ nova/resources/views/components/pages/themes/all-themes.blade.php
@@ -1,6 +1,36 @@
 <page-header>
 	<template slot="pretitle">Presentation</template>
 	<template slot="title">Themes</template>
+	<template slot="controls">
+		<div class="flex items-center">
+				<a href="{{ route('permissions.create') }}" class="button is-primary">
+					<icon name="add" class="btn-icon"></icon>
+					<span>Add a Permission</span>
+				</a>
+
+				<div class="dropdown is-right ml-3">
+					<div class="dropdown-trigger">
+						<button type="button"
+								class="button is-flush"
+								data-toggle="dropdown"
+								aria-haspopup="true"
+								aria-expanded="false"
+						>
+							<icon name="more"></icon>
+						</button>
+					</div>
+
+					<div class="dropdown-menu">
+						<div class="dropdown-content">
+								<a href="{{ route('roles.index') }}" class="dropdown-item">
+									<icon name="lock" :wrapper="{ class:'dropdown-icon' }"></icon>
+									{{ _m('authorize-roles') }}
+								</a>
+						</div>
+					</div>
+				</div>
+		</div>
+	</template>
 </page-header>
 
 @if ($themes->count() > 0)
@@ -13,18 +43,18 @@
 					</div>
 				</div>
 
-				<div class="row" v-for="role in roles">
+				<div class="row" v-for="theme in themes">
 					<div class="col">
-						@{{ role.name }}
+						@{{ theme.name }}
 					</div>
 					<div class="col-auto row-controls">
 						<nova-dropdown direction="right">
 							<icon name="more" class="btn-icon" slot="trigger-simple"></icon>
 
-							<a class="dropdown-item" :href="editLink(role.id)">
+							<a class="dropdown-item" :href="editLink(theme.id)">
 								{{ _m('edit') }}
 							</a>
-							<a class="dropdown-item is-danger" href="#" @click.prevent="deleteRole(role.id)">
+							<a class="dropdown-item is-danger" href="#" @click.prevent="deleteTheme(theme.id)">
 								{{ _m('delete') }}
 							</a>
 						</nova-dropdown>
