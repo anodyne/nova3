49cc7475cf59d55e0d20c25bf176bb1494f1c55f
David VanScott <david.vanscott@gmail.com>
2018-08-28 08:42:05 -0400

    Fix broken icons in Pulsar

--- themes/pulsar/Theme.php
+++ themes/pulsar/Theme.php
@@ -7,18 +7,4 @@ use Nova\Themes\BaseTheme;
 class Theme extends BaseTheme
 {
 	public $path = 'pulsar';
-	public $spriteMap = false;
-
-	public function getIcon($iconKey, $additionalClasses = null)
-	{
-		$classes = implode(' ', array_merge([], [$additionalClasses]));
-
-		return view("icons.{$iconKey}", compact('classes'))->render();
-
-		try {
-
-		} catch (\Exception $ex) {
-			// return parent::getIcon($iconKey, $additionalClasses);
-		}
-	}
 }
