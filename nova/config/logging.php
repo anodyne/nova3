fc49bf113d8ee2224048ade14a20f28a48b0f4c8
David VanScott <david.vanscott@gmail.com>
2018-11-01 08:15:09 -0400

    Update logging config

--- nova/config/logging.php
+++ nova/config/logging.php
@@ -16,7 +16,7 @@ return [
 	|
 	*/
 
-	'default' => env('LOG_CHANNEL', 'daily'),
+	'default' => env('LOG_CHANNEL', 'stack'),
 
 	/*
 	|--------------------------------------------------------------------------
