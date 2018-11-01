edf538c97c7cdd365f25f0c4490f9818d9d8b1ba
David VanScott <david.vanscott@gmail.com>
2018-11-01 08:08:02 -0400

    Update the users migration

--- nova/database/migrations/2014_10_12_000000_create_users_table.php
+++ nova/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ class CreateUsersTable extends Migration
 			$table->unsignedInteger('primary_character')->nullable();
 			$table->string('name');
 			$table->string('email')->unique();
+			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password')->nullable();
 			$table->string('gender')->default('neutral')->nullable();
 			$table->rememberToken();
