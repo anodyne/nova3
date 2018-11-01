141c170ff186f90822f365184a70220d8805ca37
David VanScott <david.vanscott@gmail.com>
2018-11-01 08:07:41 -0400

    Update the config files

--- nova/config/app.php
+++ nova/config/app.php
@@ -108,6 +108,19 @@ return [
 	'fallback_locale' => 'en',
 
 	/*
+    |--------------------------------------------------------------------------
+    | Faker Locale
+    |--------------------------------------------------------------------------
+    |
+    | This locale will be used by the Faker PHP library when generating fake
+    | data for your database seeds. For example, this will be used to get
+    | localized telephone numbers, street address information and more.
+    |
+	*/
+
+	'faker_locale' => 'en_US',
+
+	/*
 	|--------------------------------------------------------------------------
 	| Encryption Key
 	|--------------------------------------------------------------------------
--- nova/config/services.php
+++ nova/config/services.php
@@ -16,7 +16,8 @@ return [
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+		'secret' => env('MAILGUN_SECRET'),
+		'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
     'ses' => [
@@ -32,7 +33,11 @@ return [
     'stripe' => [
         'model' => App\User::class,
         'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
+		'secret' => env('STRIPE_SECRET'),
+		'webhook' => [
+			'secret' => env('STRIPE_WEBHOOK_SECRET'),
+			'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
+		],
     ],
 
 ];
