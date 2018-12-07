77f422a0d8185202592a1487319723bdf387b6c1
David VanScott <david.vanscott@gmail.com>
2018-12-07 11:47:16 -0500

    Webpack updates

---
+++ webpack-custom-config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+	resolve: {
+		symlinks: false,
+		alias: {
+			'@': path.resolve(__dirname, './nova/resources/js/'),
+			'@node_modules': path.resolve(__dirname, './node_modules/')
+		}
+	}
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,37 +1,16 @@
-let mix = require('laravel-mix')
-let tailwindcss = require('tailwindcss')
+const mix = require('laravel-mix');
+require('laravel-mix-tailwind');
 
-mix.setPublicPath('assets')
+mix.setPublicPath('assets');
 
-mix.autoload({
-		jquery: ['$', 'jquery', 'jQuery', 'window.jquery'],
-		'moment': ['moment', 'window.moment'],
-		'webui-popover': ['webuiPopover', 'window.webuiPopover']
-	})
-	.extract([
-		'jquery', 'axios', 'croppie', 'humanize-number',
-		'jquery-confirm', 'lodash', 'md5', 'moment', 'pluralize',
-		'sortablejs', 'vue', 'vue-clickaway', 'vue-js-toggle-button',
-		'webui-popover', 'sweetalert2', 'lightbox2'
-	])
-   .js('nova/resources/js/app.js', 'assets/js')
-   .less('nova/resources/less/app.less', 'assets/css')
-   .less('nova/resources/less/vendor.less', 'assets/css')
-   .options({
-   		postCss: [
-			tailwindcss('./nova/resources/js/tailwind.js'),
-		],
-		processCssUrls: false
-	})
-	.webpackConfig({
-		resolve: {
-			symlinks: false,
-			alias: {
-				'@': path.resolve(__dirname, 'nova/resources/js/'),
-			}
-		}
-	})
-	.sourceMaps(false)
+mix.js('nova/resources/js/app.js', 'assets/js')
+    .less('nova/resources/less/app.less', 'assets/css')
+    .less('nova/resources/less/vendor.less', 'assets/css')
+    .tailwind()
+    .options({ processCssUrls: false })
+    .webpackConfig(require('./webpack-custom-config'))
+    .sourceMaps(false);
 
-// mix.less('themes/pulsar/design/custom.less', 'themes/pulsar/design')
-// mix.less('themes/titan/design/custom.less', 'themes/titan/design')
+if (process.env.npm_lifecycle_event !== 'hot') {
+    mix.version();
+}
