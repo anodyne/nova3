e818e6b525db0e9a4d1e247e0d41c7a6b3010c29
David VanScott <david.vanscott@gmail.com>
2018-12-16 14:16:02 -0500

    Webpack updates

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,7 @@ mix.setPublicPath('assets');
 mix.js('nova/resources/js/app.js', 'assets/js')
     .less('nova/resources/less/app.less', 'assets/css')
     .less('nova/resources/less/vendor.less', 'assets/css')
-    .tailwind()
+    .tailwind('nova/resources/js/tailwind.js')
     .options({ processCssUrls: false })
     .webpackConfig(require('./webpack-custom-config'))
     .sourceMaps(false);
