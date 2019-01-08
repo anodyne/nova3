ce4234a68d2f3ec1a5132321121e01f00553c1b1
David VanScott <david.vanscott@gmail.com>
2019-01-07 22:08:06 -0500

    Add field wrapper component

---
+++ nova/resources/js/components/forms/FieldWrapper.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="field-wrapper" :class="errorStyles">
+        <div class="field-label" v-if="hasLabel">
+            <label :for="fieldId">{{ label }}</label>
+        </div>
+
+        <slot></slot>
+
+        <div class="field-error" v-if="hasError">
+            {{ error }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FieldWrapper',
+
+    props: {
+        error: {
+            type: String,
+            default: ''
+        },
+        fieldId: {
+            type: String,
+            default: '',
+        },
+        label: {
+            type: String,
+            default: ''
+        }
+    },
+
+    computed: {
+        errorStyles () {
+            return {
+                'has-error': this.hasError
+            };
+        },
+
+        hasError () {
+            return this.error !== '';
+        },
+
+        hasLabel () {
+            return this.label !== '';
+        }
+    }
+};
+</script>
