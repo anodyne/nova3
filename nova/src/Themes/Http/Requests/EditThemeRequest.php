638f0cb70a446dc61cd0e7c01f860ba10d80095a
David VanScott <david.vanscott@gmail.com>
2019-01-12 14:32:35 -0500

    More traditional form request classes

--- nova/src/Themes/Http/Requests/EditThemeRequest.php
+++ nova/src/Themes/Http/Requests/EditThemeRequest.php
@@ -2,7 +2,39 @@
 
 namespace Nova\Themes\Http\Requests;
 
-class EditThemeRequest extends BaseThemeRequest
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditThemeRequest extends FormRequest
 {
-    //
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required'],
+            'location' => ['required'],
+            'credits' => ['nullable'],
+            'layout_auth' => ['required'],
+            'layout_auth_settings' => ['nullable'],
+            'layout_admin' => ['required'],
+            'layout_admin_settings' => ['nullable'],
+            'layout_public' => ['required'],
+            'layout_public_settings' => ['nullable'],
+            'variants' => ['nullable'],
+            'icon_set' => ['nullable'],
+        ];
+    }
 }
