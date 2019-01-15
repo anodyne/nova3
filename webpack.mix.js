b2f67d6cd9633cb3cf696de7efe9a77e236e4ad9
David VanScott <david.vanscott@gmail.com>
2019-01-15 10:01:50 -0500

    Add vendor stylesheet

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,7 @@ mix.setPublicPath('dist');
 
 mix.js('nova/resources/js/app.js', 'dist/js')
    .less('nova/resources/less/app.less', 'dist/css')
+   .less('nova/resources/less/vendor.less', 'dist/css')
    .tailwind()
    .webpackConfig(require('./webpack-custom-config'))
    .extract();
