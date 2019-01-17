7744e7e7910f684ca1d7770ed8689b5e78cb5a09
David VanScott <david.vanscott@gmail.com>
2019-01-17 18:22:46 -0500

    Tab components

---
+++ nova/resources/js/components/TabListItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div
+        v-show="isActive"
+        v-cloak
+        class="tab-item"
+    >
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TabListItem',
+
+    inject: ['tabListState'],
+
+    props: {
+        active: {
+            type: Boolean,
+            default: false
+        },
+        id: {
+            type: String,
+            required: true
+        },
+        label: {
+            type: String,
+            required: true
+        }
+    },
+
+    computed: {
+        isActive () {
+            return this.id === this.tabListState.activeId;
+        }
+    }
+};
+</script>
