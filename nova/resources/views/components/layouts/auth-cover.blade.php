f2820a881543d8e2b01d87c515539e3cb987368b
David VanScott <david.vanscott@gmail.com>
2019-01-24 15:09:29 -0500

    Auth cover updates

---
+++ nova/resources/views/components/layouts/auth-cover.blade.php
@@ -0,0 +1,9 @@
+<div class="layout-auth-cover">
+    <aside style="background-image:url('/themes/pulsar/design/images/space-wallpaper-5.jpg')"></aside>
+
+    <section>
+        <main>
+            {!! $template ?? false !!}
+        </main>
+    </section>
+</div>
