6348fca0e8c319168e4d3199962b002eebfb3777
David VanScott <david.vanscott@gmail.com>
2019-02-03 16:31:29 -0500

    Adding the snackbar component

---
+++ nova/resources/js/components/notices/NovaSnackbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <transition
+        :enter-active-class="transition.enter"
+        :leave-active-class="transition.leave"
+    >
+        <div
+            v-show="isActive"
+            class="snackbar"
+            :class="[type, position]"
+        >
+            <p class="message">{{ message }}</p>
+
+            <div
+                v-if="actionText"
+                class="action"
+                :class="type"
+                @click="action"
+            >
+                <button class="button button-dark">{{ actionText }}</button>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+import config from '@/mixins/config';
+import NoticesMixin from '@/mixins/NoticesMixin';
+
+export default {
+    name: 'NovaSnackbar',
+
+    mixins: [NoticesMixin],
+
+    props: {
+        actionText: {
+            type: String,
+            default: 'OK'
+        },
+        onAction: {
+            type: Function,
+            default: () => {}
+        },
+        indefinite: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    data () {
+        return {
+            newDuration: this.duration || config.defaultSnackbarDuration
+        };
+    },
+
+    methods: {
+        action () {
+            this.onAction();
+            this.close();
+        }
+    }
+};
+</script>
