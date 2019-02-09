8b3c775b7e80c4b8bd97f3da22f061088a223bca
David VanScott <david.vanscott@gmail.com>
2019-02-09 16:18:09 -0500

    Work on alerts

--- nova/resources/js/components/notices/NovaNotices.vue
+++ nova/resources/js/components/notices/NovaNotices.vue
@@ -1,7 +1,14 @@
 <template>
-    <div class="notices" :class="parentStyles">
-        <nova-toast :session="toastData"></nova-toast>
-        <nova-snackbar :session="snackbarData"></nova-snackbar>
+    <div
+        class="notices"
+        :class="parentStyles"
+        aria-atomic="true"
+    >
+        <nova-alert
+            v-for="(alert, index) in alerts"
+            :key="index"
+            :data="alert"
+        ></nova-alert>
     </div>
 </template>
 
@@ -21,8 +28,9 @@ export default {
 
     data () {
         return {
-            position: '',
-            type: ''
+            alerts: [],
+            position: 'is-bottom',
+            type: 'is-dark'
         };
     },
 
@@ -32,41 +40,15 @@ export default {
                 'is-bottom': this.position.includes('bottom'),
                 'is-top': this.position.includes('top')
             };
-        },
-
-        snackbarData () {
-            if (has(this.session, 'snackbar')) {
-                return this.session.snackbar;
-            }
-
-            return null;
-        },
-
-        toastData () {
-            if (has(this.session, 'toast')) {
-                return this.session.toast;
-            }
-
-            return null;
         }
     },
 
     mounted () {
         if (this.session !== null && !isArray(this.session)) {
-            if (has(this.session, 'toast')) {
-                this.setData(this.session.toast);
-            }
-
-            if (has(this.session, 'snackbar')) {
-                this.setData(this.session.snackbar);
-            }
+            this.setDadta(this.session);
         }
 
-        Nova.$on('nova.notices.snackbar', (params) => {
-            this.setData(params);
-        });
-
-        Nova.$on('nova.notices.toast', (params) => {
+        Nova.$on('nova.alert', (params) => {
             this.setData(params);
         });
     },
@@ -75,6 +57,14 @@ export default {
         setData (data) {
             this.type = has(data, 'type') ? data.type : 'is-dark';
             this.position = has(data, 'position') ? data.position : 'is-bottom';
+
+            this.alerts.push({
+                message: has(data, 'message') ? data.message : '',
+                type: this.type,
+                position: this.position,
+                actionFunction: has(data, 'actionFunction') ? data.actionFunction : null,
+                actionText: has(data, 'actionText') ? data.actionText : ''
+            });
         }
     }
 };
--- nova/resources/js/util/notices/toast.js
+++ nova/resources/js/util/notices/toast.js
@@ -1,5 +1,7 @@
 export default class Toast {
     constructor () {
+        this.actionText = '';
+        this.actionFunction = null;
         this.message = '';
         this.type = 'is-dark';
         this.position = 'is-bottom';
@@ -41,22 +43,28 @@ export default class Toast {
         return this;
     }
 
-    toastDark () {
-        this.type = 'is-dark';
-
-        this.createToast();
-    }
-
-    toastError () {
+    error () {
         this.type = 'is-danger';
 
         this.createToast();
     }
 
-    toastSuccess () {
+    success () {
         this.type = 'is-success';
 
         this.createToast();
+    }
+
+    withAction (callback) {
+        this.actionFunction = callback;
+
+        return this;
+    }
+
+    withActionText (value) {
+        this.actionText = value;
+
+        return this;
     }
 
     withMessage (value) {
@@ -65,8 +73,10 @@ export default class Toast {
         return this;
     }
 
-    createToast () {
-        Nova.$emit('nova.notices.toast', {
+    make () {
+        Nova.$emit('nova.alert', {
+            actionText: this.actionText,
+            actionFunction: this.actionFunction,
             type: this.type,
             message: this.message,
             position: this.position
