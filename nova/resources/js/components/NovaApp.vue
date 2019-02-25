444b90144e80154e05cd0c7f7d17ac8410943c85
David VanScott <david.vanscott@gmail.com>
2019-02-25 08:26:38 -0500

    Add the NovaApp component

---
+++ nova/resources/js/components/NovaApp.vue
@@ -0,0 +1,30 @@
+<template>
+    <component :is="layout">
+        <component :is="component" v-bind="props"></component>
+    </component>
+</template>
+
+<script>
+import axios from '@/util/axios';
+
+export default {
+    name: 'NovaApp',
+
+    props: {
+        component: {
+            type: String,
+            required: true
+        },
+        props: {
+            type: Object,
+            required: true
+        }
+    },
+
+    computed: {
+        layout () {
+            return 'SidebarLayout';
+        }
+    }
+};
+</script>
