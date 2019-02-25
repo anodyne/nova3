8f488c570566c447917183ef3ea0df24943fd9da
David VanScott <david.vanscott@gmail.com>
2019-02-25 08:26:23 -0500

    Add some layout components (WIP)

---
+++ nova/resources/js/layouts/HeroLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <header class="flex h-64 bg-blue-500 mb-12"></header>
+
+        <main>
+            <div class="container">
+                <slot></slot>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeroLayout',
+
+    props: {
+        page: {
+            type: Object,
+            default: () => {
+                return {};
+            }
+        },
+        theme: {
+            type: Object,
+            default: () => {
+                return {};
+            }
+        }
+    }
+};
+</script>
---
+++ nova/resources/js/layouts/SidebarLayout.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="layout-app-sidebar">
+        <nav class="sidebar">
+            <div>
+                <a href="#" class="flex justify-center my-6 leading-0">
+                </a>
+
+                <div class="flex flex-col -mx-6">
+                    <a href="#" class="sidebar-link">
+                        <div class="flex items-center">
+                            <nova-icon name="home" class="mr-3"></nova-icon>
+                            Dashboard
+                        </div>
+                    </a>
+                    <a href="#" class="sidebar-link">
+                        <div class="flex items-center">
+                            <nova-icon name="file" class="mr-3"></nova-icon>
+                            Pages
+                        </div>
+                        <nova-icon name="chevron-down" class="h-4 w-4"></nova-icon>
+                    </a>
+                    <a href="#" class="sidebar-link">
+                        <div class="flex items-center">
+                            <nova-icon name="user" class="mr-3"></nova-icon>
+                            Authentication
+                        </div>
+                        <nova-icon name="chevron-down" class="h-4 w-4"></nova-icon>
+                    </a>
+                    <a href="#" class="sidebar-link">
+                        <div class="flex items-center">
+                            <nova-icon name="layout" class="mr-3"></nova-icon>
+                            Layouts
+                        </div>
+                        <nova-icon name="chevron-down" class="h-4 w-4"></nova-icon>
+                    </a>
+                </div>
+
+                <div class="sidebar-divider"></div>
+
+                <div class="sidebar-header">Documentation</div>
+
+                <div class="flex flex-col -mx-6">
+                    <a href="#" class="sidebar-link">
+                        <div class="flex items-center">
+                            <nova-icon name="clipboard" class="mr-3"></nova-icon>
+                            Getting started
+                        </div>
+                    </a>
+                    <a href="#" class="sidebar-link">
+                        <div class="flex items-center">
+                            <nova-icon name="book-open" class="mr-3"></nova-icon>
+                            Components
+                        </div>
+                        <nova-icon name="chevron-down" class="h-4 w-4"></nova-icon>
+                    </a>
+                    <a href="#" class="sidebar-link">
+                        <div class="flex items-center">
+                            <nova-icon name="git-branch" class="mr-3"></nova-icon>
+                            Changelog
+                        </div>
+                        <div class="rounded bg-blue text-white text-2xs py-1 px-2">v1.0</div>
+                    </a>
+                    <a
+                        :href="route('logout')"
+                        class="sidebar-link"
+                        onclick="event.preventDefault();document.getElementById('logout-form').submit();"
+                    >
+                        <div class="flex items-center">
+                            <nova-icon name="sign-out" class="mr-3"></nova-icon>
+                            Log out
+                        </div>
+                    </a>
+                    <form
+                        id="logout-form"
+                        :action="route('logout')"
+                        method="POST"
+                        style="display: none;"
+                    >
+                        @csrf
+                    </form>
+                </div>
+            </div>
+
+            <div class="sidebar-footer">
+                <a href="#" class="sidebar-footer-link">
+                    <nova-icon name="notification"></nova-icon>
+                </a>
+
+                <div class="flex items-center">
+                    <!-- <user-avatar :user="{{ Nova\Users\User::first() }}" :show-meta="false"></user-avatar>
+                    {{-- <avatar :item="{{ $_user->toJson() }}"
+                            :show-content="false"
+                            :show-status="false"
+                            type="image"></avatar>
+                    <icon name="chevron-down" size="small" classes="ml-1"></icon> --}}
+                    <nova-icon name="chevron-down" class="h-4 w-4 ml-1 text-grey"></nova-icon> -->
+                </div>
+
+                <a href="#" class="sidebar-footer-link">
+                    <nova-icon name="search"></nova-icon>
+                </a>
+            </div>
+        </nav>
+
+        <main>
+            <slot></slot>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SidebarLayout',
+
+    props: {
+        page: {
+            type: Object,
+            default: () => {
+                return {};
+            }
+        },
+        theme: {
+            type: Object,
+            default: () => {
+                return {};
+            }
+        }
+    }
+};
+</script>
