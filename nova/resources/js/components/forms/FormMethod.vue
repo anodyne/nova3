e38c359975f4f77a86bcfe62b3356a84638908e4
David VanScott <david.vanscott@gmail.com>
2019-02-25 08:25:52 -0500

    Add a form method component

---
+++ nova/resources/js/components/forms/FormMethod.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+    name: 'FormMethod',
+
+    props: {
+        delete: {
+            type: Boolean,
+            default: false
+        },
+        get: {
+            type: Boolean,
+            default: false
+        },
+        patch: {
+            type: Boolean,
+            default: false
+        },
+        post: {
+            type: Boolean,
+            default: false
+        },
+        put: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    computed: {
+        method () {
+            if (this.delete) {
+                return 'DELETE';
+            }
+
+            if (this.get) {
+                return 'GET';
+            }
+
+            if (this.patch) {
+                return 'PATCH';
+            }
+
+            if (this.post) {
+                return 'POST';
+            }
+
+            if (this.put) {
+                return 'PUT';
+            }
+        }
+    },
+
+    render (h) {
+        return h('input', {
+            attrs: {
+                type: 'hidden',
+                name: '_method'
+            },
+            domProps: {
+                value: this.method
+            }
+        });
+    }
+};
+</script>
