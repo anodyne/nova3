144361b76765169f394add60004399a96cd93cc5
David VanScott <david.vanscott@gmail.com>
2019-02-25 08:26:45 -0500

    Add a page header component

---
+++ nova/resources/js/components/PageHeader.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="page-header">
+        <div class="page-header-content">
+            <div v-if="hasPreTitle" class="page-header-pretitle">
+                {{ pretitle }}
+            </div>
+
+            <div class="page-header-title">
+                <slot>{{ title }}</slot>
+            </div>
+        </div>
+
+        <div v-if="hasSlot('controls')" class="page-header-controls">
+            <slot name="controls"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import SlotHelpers from '@/mixins/SlotHelpers';
+
+export default {
+    name: 'PageHeader',
+
+    mixins: [SlotHelpers],
+
+    props: {
+        pretitle: {
+            type: String,
+            default: ''
+        },
+        title: {
+            type: String,
+            default: ''
+        }
+    },
+
+    computed: {
+        hasPreTitle () {
+            return this.pretitle !== '';
+        }
+    }
+};
+</script>
