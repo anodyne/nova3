6caa403c84793206f19e3da83683056ecbb77f59
David VanScott <david.vanscott@gmail.com>
2019-03-01 10:29:14 -0500

    Create a toggle switch

---
+++ nova/resources/js/components/forms/ToggleSwitch.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+        class="switch"
+        role="checkbox"
+        tabindex="0"
+        :aria-checked="value.toString()"
+        @click="toggle"
+        @keydown.space.prevent="toggle"
+    ></div>
+</template>
+
+<script>
+export default {
+    name: 'ToggleSwitch',
+
+    props: {
+        value: {
+            type: Boolean,
+            required: true
+        }
+    },
+
+    methods: {
+        toggle () {
+            this.$emit('input', !this.value);
+        }
+    }
+};
+</script>
