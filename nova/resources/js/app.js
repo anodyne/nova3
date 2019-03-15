7c5ffae898964ae0257b9530d22c07bda3d22c22
David VanScott <david.vanscott@gmail.com>
2019-03-15 11:56:10 -0400

    Fix some issues

--- nova/resources/js/app.js
+++ nova/resources/js/app.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import store from './store';
 import Nova from './nova';
-import axios from '@/util/axios';
 
 import './components';
 import './directives';
@@ -19,13 +18,9 @@ if (isCSR) {
         store,
 
         mounted () {
-            axios.post(window.route('bootstrap-nova'))
-                .then(({ data }) => {
-                    this.$store.set('Icons/initialIcons', data.icons);
-                    this.$store.set('Theme/initialTheme', data.theme);
-                    this.$store.set('Settings/initialSettings', data.settings);
-                    this.$store.set('User/initialUser', data.user);
-                });
+            this.$store.set('Icons/initialIcons', window.novaSettings.icons);
+            this.$store.set('Theme/initialTheme', window.novaSettings.theme);
+            this.$store.set('User/initialUser', window.novaSettings.user);
         },
 
         render (h) {
@@ -46,13 +41,9 @@ if (isCSR) {
         store,
 
         mounted () {
-            axios.post(window.route('bootstrap-nova'))
-                .then(({ data }) => {
-                    this.$store.set('Icons/initialIcons', data.icons);
-                    this.$store.set('Theme/initialTheme', data.theme);
-                    this.$store.set('Settings/initialSettings', data.settings);
-                    this.$store.set('User/initialUser', data.user);
-                });
+            this.$store.set('Icons/initialIcons', window.novaSettings.icons);
+            this.$store.set('Theme/initialTheme', window.novaSettings.theme);
+            this.$store.set('User/initialUser', window.novaSettings.user);
         }
     }).$mount(root);
 }
