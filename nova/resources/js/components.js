b39dbdf3b63fd429f00bc4354a1cde132db4ffed
David VanScott <david.vanscott@gmail.com>
2019-04-05 09:50:26 -0400

    Clean up component imports

--- nova/resources/js/components.js
+++ nova/resources/js/components.js
@@ -1,14 +1,18 @@
 import Vue from 'vue';
 import NovaIcon from '@/Shared/Icons/NovaIcon';
+import PageHeader from '@/Shared/PageHeader';
+
 import CsrfToken from '@/Shared/Forms/CsrfToken';
 import FormMethod from '@/Shared/Forms/FormMethod';
 import FormField from '@/Shared/Forms/FormField';
+import PasswordField from '@/Shared/Forms/PasswordField';
 import ToggleSwitch from '@/Shared/Forms/ToggleSwitch';
-import PageHeader from '@/Shared/PageHeader';
 
 Vue.component('nova-icon', NovaIcon);
+Vue.component('page-header', PageHeader);
+
 Vue.component('csrf-token', CsrfToken);
 Vue.component('form-method', FormMethod);
 Vue.component('form-field', FormField);
-Vue.component('page-header', PageHeader);
+Vue.component('password-field', PasswordField);
 Vue.component('toggle-switch', ToggleSwitch);
