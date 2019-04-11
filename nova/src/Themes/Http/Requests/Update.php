16a74a5fc7a94a4b5d65c24cbf02fa25ef4d520c
David VanScott <david.vanscott@gmail.com>
2019-04-11 08:34:32 -0400

    Remove some unnecessary code

--- nova/src/Themes/Http/Requests/Update.php
+++ nova/src/Themes/Http/Requests/Update.php
@@ -6,21 +6,6 @@ use Nova\Foundation\Http\Requests\BaseFormRequest;
 
 class Update extends BaseFormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
