cd91a1e544322ef03e07ce3bf66ec35d27faf8f7
David VanScott <david.vanscott@gmail.com>
2019-04-11 17:05:45 -0400

    Update installing themes

--- nova/src/Themes/Http/Controllers/InstallThemeController.php
+++ nova/src/Themes/Http/Controllers/InstallThemeController.php
@@ -4,29 +4,39 @@ namespace Nova\Themes\Http\Controllers;
 
 use Nova\Themes\Jobs;
 use Nova\Themes\Events;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Nova\Foundation\Http\Controllers\Controller;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Nova\Themes\Exceptions\MissingQuickInstallFileException;
+use Nova\Themes\Http\Requests\Install as ValidateInstallingTheme;
+use Nova\Themes\Http\Authorizers\Install as AuthorizeInstallingTheme;
 
 class InstallThemeController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __construct()
     {
-        try {
-            $themeProperties = json_decode(
-                Storage::disk('themes')->get("{$request->theme}/theme.json"),
-                true
-            );
-        } catch (FileNotFoundException $ex) {
-            throw new MissingQuickInstallFileException;
-        }
+        parent::__construct();
+
+        $this->middleware('auth');
+    }
+
+    public function __invoke(AuthorizeInstallingTheme $gate, ValidateInstallingTheme $request)
+    {
+        $themeProperties = $this->getThemePropertiesFromQuickInstallFile($request->theme);
 
         $theme = dispatch_now(new Jobs\InstallTheme($themeProperties));
 
         event(new Events\ThemeInstalled($theme));
 
         return $theme->fresh();
+    }
+
+    protected function getThemePropertiesFromQuickInstallFile($location)
+    {
+        try {
+            return json_decode(Storage::disk('themes')->get("{$location}/theme.json"), true);
+        } catch (FileNotFoundException $ex) {
+            throw new MissingQuickInstallFileException;
+        }
     }
 }
