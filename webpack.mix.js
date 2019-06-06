a1c8b5f9c3458c99c5dc17e1fcec2f5efaa55a64
David VanScott <david.vanscott@gmail.com>
2019-06-06 07:40:34 -0400

    Move custom configs into Mix file

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const path = require('path');
 require('laravel-mix-tailwind');
 
 mix.setPublicPath('dist');
@@ -8,4 +9,19 @@ mix.js('nova/resources/js/app.js', 'dist/js')
     .less('nova/resources/less/app.less', 'dist/css')
     .less('nova/resources/less/vendor.less', 'dist/css')
     .tailwind('./tailwind.config.js')
-    .webpackConfig(require('./webpack-custom-config'));
+    .babelConfig({
+        plugins: ['@babel/plugin-syntax-dynamic-import']
+    })
+    .webpackConfig({
+        output: {
+            chunkFilename: 'js/[name].[contenthash].js',
+            publicPath: '/dist/'
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './nova/resources/js/'),
+                '@node_modules': path.resolve(__dirname, './node_modules/')
+            },
+            symlinks: false
+        }
+    });
