377f69d7ec4579602e96682bfc2715b7a09946de
David VanScott <david.vanscott@gmail.com>
2019-08-21 07:36:29 -0400

    Add search to themes

--- nova/resources/js/Pages/Themes/Index.vue
+++ nova/resources/js/Pages/Themes/Index.vue
@@ -13,49 +13,70 @@
             @theme-installed="installedThemes.push($event)"
         ></install-themes>
 
-        <transition-group
-            tag="div"
-            leave-active-class="animated fadeOut"
-        >
-            <panel
-                v-for="theme in installedThemes"
-                :key="theme.id"
-                class="flex items-center justify-between mt-8 first:mt-0"
-            >
-                <div class="flex items-center">
-                    <div class="flex flex-col">
-                        <div class="font-semibold">{{ theme.name }}</div>
-                        <div class="text-gray-600">themes/{{ theme.location }}</div>
+        <section>
+            <div class="mb-6 w-1/3">
+                <div class="flex items-center py-1 px-2 rounded-full bg-white border-2 border-transparent text-gray-500 focus-within:bg-white focus-within:border-gray-400 focus-within:text-gray-600">
+                    <icon name="search" class="mr-2"></icon>
+
+                    <input
+                        v-model="search"
+                        type="text"
+                        placeholder="Find a theme..."
+                        class="w-full appearance-none bg-transparent text-gray-800 focus:outline-none"
+                    >
+
+                    <a
+                        v-show="search != ''"
+                        role="button"
+                        class="ml-2 text-gray-500 hover:text-danger-500"
+                        @click="search = ''"
+                    >
+                        <icon name="close"></icon>
+                    </a>
+                </div>
+            </div>
+
+            <transition-group leave-active-class="animated fadeOut">
+                <div
+                    v-for="theme in filteredThemes"
+                    :key="theme.id"
+                    class="panel flex items-center justify-between"
+                >
+                    <div class="flex items-center">
+                        <div class="flex flex-col">
+                            <div class="font-semibold">{{ theme.name }}</div>
+                            <div class="text-gray-600">themes/{{ theme.location }}</div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <dropdown placement="bottom-end">
+                            <icon name="more-horizontal" class="h-6 w-6"></icon>
+
+                            <template #dropdown="{ dropdownProps }">
+                                <inertia-link
+                                    v-if="themes.can.update"
+                                    :href="route('themes.edit', { theme })"
+                                    class="dropdown-link"
+                                >
+                                    <icon name="edit" class="dropdown-item-icon"></icon>
+                                    Edit
+                                </inertia-link>
+                                <a
+                                    v-if="themes.can.delete"
+                                    role="button"
+                                    class="dropdown-link-danger"
+                                    @click="confirmRemove(theme)"
+                                >
+                                    <icon name="delete" class="dropdown-item-icon"></icon>
+                                    Delete
+                                </a>
+                            </template>
+                        </dropdown>
                     </div>
                 </div>
-
-                <div>
-                    <dropdown placement="bottom-end">
-                        <icon name="more-horizontal" class="h-6 w-6"></icon>
-
-                        <template #dropdown="{ dropdownProps }">
-                            <inertia-link
-                                v-if="themes.can.update"
-                                :href="route('themes.edit', { theme })"
-                                class="dropdown-link"
-                            >
-                                <icon name="edit" class="dropdown-item-icon"></icon>
-                                Edit
-                            </inertia-link>
-                            <a
-                                v-if="themes.can.delete"
-                                role="button"
-                                class="dropdown-link-danger"
-                                @click="confirmRemove(theme)"
-                            >
-                                <icon name="delete" class="dropdown-item-icon"></icon>
-                                Delete
-                            </a>
-                        </template>
-                    </dropdown>
-                </div>
-            </panel>
-        </transition-group>
+            </transition-group>
+        </section>
 
         <modal
             :open="modalIsShown"
@@ -107,8 +128,19 @@ export default {
     data () {
         return {
             form: new Form(),
-            installedThemes: this.themes.data
+            installedThemes: this.themes.data,
+            search: ''
         };
+    },
+
+    computed: {
+        filteredThemes () {
+            return this.installedThemes.filter((theme) => {
+                const searchRegex = new RegExp(this.search, 'i');
+
+                return searchRegex.test(theme.name) || searchRegex.test(theme.location);
+            });
+        }
     },
 
     methods: {
