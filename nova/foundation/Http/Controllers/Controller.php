13ac0bfc8ece0d8cdb7c264f8df8ec2fa6fc5313
David VanScott <david.vanscott@gmail.com>
2019-09-04 11:44:23 -0400

    Thank you Laravel 6!

--- nova/foundation/Http/Controllers/Controller.php
+++ nova/foundation/Http/Controllers/Controller.php
@@ -14,22 +14,4 @@ class Controller extends BaseController
     public function __construct()
     {
     }
-
-    /**
-     * Get the map of resource methods to ability names.
-     *
-     * @return array
-     */
-    protected function resourceAbilityMap()
-    {
-        return [
-            'index' => 'index',
-            'show' => 'view',
-            'create' => 'create',
-            'store' => 'create',
-            'edit' => 'update',
-            'update' => 'update',
-            'destroy' => 'delete',
-        ];
-    }
 }
