8b0f737ac5078bac796b30ee54f6bb1c1f73f0c9
David VanScott <david.vanscott@gmail.com>
2020-01-23 10:46:09 -0500

    Add new avatar and avatar group components

---
+++ nova/resources/js/Shared/Avatars/Avatar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="avatar" :class="styles.size">
+        <img
+            v-if="url !== ''"
+            :src="url"
+            alt="avatar image"
+            class="avatar-image"
+        >
+        <div
+            v-if="url === ''"
+            class="avatar-title"
+            :class="styles.title"
+            v-text="title"
+        ></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Avatar',
+
+    props: {
+        size: {
+            type: String,
+            default: ''
+        },
+        title: {
+            type: String,
+            default: ''
+        },
+        url: {
+            type: String,
+            default: ''
+        }
+    },
+
+    computed: {
+        styles () {
+            return {
+                size: {
+                    [`avatar-${this.size}`]: this.size !== ''
+                },
+                title: {
+                    'avatar-title-soft': this.title.charAt(0) === '+'
+                }
+            };
+        }
+    }
+};
+</script>
---
+++ nova/resources/js/Shared/Avatars/AvatarGroup.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="avatar-group">
+        <avatar
+            v-for="item in finalItems"
+            :key="item.title"
+            :size="size"
+            v-bind="item"
+        ></avatar>
+    </div>
+</template>
+
+<script>
+import Avatar from '@/Shared/Avatars/Avatar';
+
+export default {
+    name: 'AvatarGroup',
+
+    components: { Avatar },
+
+    props: {
+        items: {
+            type: Array,
+            required: true
+        },
+        size: {
+            type: String,
+            default: ''
+        }
+    },
+
+    computed: {
+        finalItems () {
+            const items = this.items.slice(0, 4);
+
+            if (this.items.length > 4) {
+                items.push({ url: '', title: `+${this.items.length - 4}` });
+            }
+
+            return items;
+        }
+    }
+};
+</script>
