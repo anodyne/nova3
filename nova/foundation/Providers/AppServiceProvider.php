df3096efe3970d9a1d0e6e1ab0d5e0fdf00d8b7f
David VanScott <david.vanscott@gmail.com>
2020-01-27 14:49:27 -0500

    Add the meta info for paginated collections

--- nova/foundation/Providers/AppServiceProvider.php
+++ nova/foundation/Providers/AppServiceProvider.php
@@ -107,6 +107,23 @@ class AppServiceProvider extends ServiceProvider
                     ];
                 }
 
+                public function meta()
+                {
+                    return [
+                        'current_page' => $this->currentPage(),
+                        'first_page_url' => $this->url(1),
+                        'from' => $this->firstItem(),
+                        'last_page' => $this->lastPage(),
+                        'last_page_url' => $this->url($this->lastPage()),
+                        'next_page_url' => $this->nextPageUrl(),
+                        'path' => $this->path(),
+                        'per_page' => $this->perPage(),
+                        'prev_page_url' => $this->previousPageUrl(),
+                        'to' => $this->lastItem(),
+                        'total' => $this->total(),
+                    ];
+                }
+
                 public function links($view = null, $data = [])
                 {
                     $this->appends(Request::all());
