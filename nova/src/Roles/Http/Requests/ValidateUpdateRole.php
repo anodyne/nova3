0e306bf932e7cc3577c374cd74c02cbd7a965d32
David VanScott <david.vanscott@gmail.com>
2020-01-28 08:48:56 -0500

    Fix validation errors

--- nova/src/Roles/Http/Requests/ValidateUpdateRole.php
+++ nova/src/Roles/Http/Requests/ValidateUpdateRole.php
@@ -9,19 +9,18 @@ class ValidateUpdateRole extends ValidatesRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'unique:roles'],
+            'name' => ['required'],
             'display_name' => ['required'],
             'permissions' => ['nullable'],
             'users' => ['nullable'],
         ];
     }
 
-    public function users()
+    public function messages()
     {
         return [
             'display_name.required' => 'The name field is required.',
             'name.required' => 'The key field is required.',
-            'name.unique' => 'That value already exists. Please choose a unique key.',
         ];
     }
 }
