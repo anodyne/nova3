ec6085eab58f3797d5fe7b9e5f46115fce4509cc
David VanScott <david.vanscott@gmail.com>
2020-02-03 10:32:41 -0500

    Add a checkbox component

---
+++ nova/resources/js/Shared/Forms/FormCheckbox.vue
@@ -0,0 +1,31 @@
+<template>
+    <label class="flex items-center">
+        <input
+            type="checkbox"
+            class="form-checkbox"
+            :class="checkboxClass"
+            :value="value"
+            @change="$emit('input', !value)"
+        >
+        <span class="ml-2">
+            <slot></slot>
+        </span>
+    </label>
+</template>
+
+<script>
+export default {
+    name: 'FormCheckbox',
+
+    props: {
+        checkboxClass: {
+            type: String,
+            default: 'text-primary-500'
+        },
+        value: {
+            type: Boolean,
+            required: true
+        }
+    }
+};
+</script>
