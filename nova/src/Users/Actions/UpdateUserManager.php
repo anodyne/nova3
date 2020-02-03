fa9c947f2d075830048d5c4f49b4f8f12e8be160
David VanScott <david.vanscott@gmail.com>
2020-02-03 10:32:21 -0500

    Remove an avatar

--- nova/src/Users/Actions/UpdateUserManager.php
+++ nova/src/Users/Actions/UpdateUserManager.php
@@ -8,23 +8,38 @@ use Nova\Users\DataTransferObjects\UserData;
 
 class UpdateUserManager
 {
+    /**
+     * @var  UpdateUser
+     */
     protected $updateUser;
 
+    /**
+     * @var  UploadUserAvatar
+     */
     protected $uploadAvatar;
+
+    /**
+     * @var  RemoveUserAvatar
+     */
+    protected $removeAvatar;
 
     public function __construct(
         UpdateUser $updateUser,
-        UploadUserAvatar $uploadAvatar
+        UploadUserAvatar $uploadAvatar,
+        RemoveUserAvatar $removeAvatar
     ) {
         $this->updateUser = $updateUser;
         $this->uploadAvatar = $uploadAvatar;
+        $this->removeAvatar = $removeAvatar;
     }
 
     public function execute(User $user, Request $request): User
     {
         $this->updateUser->execute($user, UserData::fromRequest($request));
 
-        $this->uploadAvatar->execute($user, $request->file('image'));
+        $this->uploadAvatar->execute($user, $request->file('avatar'));
+
+        $this->removeAvatar->execute($user, $request->input('remove_avatar', false));
 
         return $user->refresh();
     }
