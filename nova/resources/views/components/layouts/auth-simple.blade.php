ca894c38ca7ec18322ab5b8d2d4ee044fe04dfc2
David VanScott <david.vanscott@gmail.com>
2020-02-05 16:49:08 -0500

    Layout updates

--- nova/resources/views/components/layouts/auth-simple.blade.php
+++ nova/resources/views/components/layouts/auth-simple.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col justify-center items-center min-h-screen">
-    <main class="flex flex-col w-96 mt-6 p-6 bg-white rounded-lg shadow-md overflow-hidden">
+    <main class="flex flex-col w-96 mt-6 p-6 bg-white rounded shadow-lg overflow-hidden">
         {!! $template ?? false !!}
     </main>
 </div>
