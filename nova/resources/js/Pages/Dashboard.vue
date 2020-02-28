38bbd9b39af3689a8f588800b7244f855bae1618
David VanScott <david.vanscott@gmail.com>
2020-02-28 00:10:41 -0500

    Tests

--- nova/resources/js/Pages/Dashboard.vue
+++ nova/resources/js/Pages/Dashboard.vue
@@ -1,13 +1,52 @@
 <template>
-    <sidebar-layout>
+    <admin-layout>
         <page-header title="Dashboard"></page-header>
 
         <div class="panel">
-            Welcome to Nova 3.
+            <p>Welcome to Nova 3.</p>
+
+            <avatar-group :items="avatars"></avatar-group>
+
+            <div class="flex justify-end mt-8">
+                <dropdown placement="bottom-end" class="button button-primary">
+                    Dropdown
+
+                    <template #dropdown>
+                        <a href="#" class="dropdown-link">
+                            <icon name="eye" class="dropdown-icon"></icon>
+                            View
+                        </a>
+                        <a href="#" class="dropdown-link">
+                            <icon name="eye" class="dropdown-icon"></icon>
+                            View
+                        </a>
+                        <a href="#" class="dropdown-link">
+                            <icon name="eye" class="dropdown-icon"></icon>
+                            View
+                        </a>
+                    </template>
+                </dropdown>
+            </div>
         </div>
-    </sidebar-layout>
+    </admin-layout>
 </template>
 
 <script>
-export default {};
+import AvatarGroup from '@/Shared/Avatars/AvatarGroup';
+
+export default {
+    components: { AvatarGroup },
+
+    data () {
+        return {
+            avatars: [
+                { id: 1, 'image-url': 'https://api.adorable.io/avatars/285/abc.png', tooltip: '' },
+                { id: 2, 'image-url': 'https://api.adorable.io/avatars/285/def.png', tooltip: '' },
+                { id: 3, 'image-url': 'https://api.adorable.io/avatars/285/ghi.png', tooltip: '' },
+                { id: 4, 'image-url': 'https://api.adorable.io/avatars/285/jkl.png', tooltip: '' },
+                { id: 5, 'image-url': 'https://api.adorable.io/avatars/285/xyz.png', tooltip: '' }
+            ]
+        };
+    }
+};
 </script>
