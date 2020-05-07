32b15560d999612dd151c6055cc2e204f6bc247c
David VanScott <david.vanscott@gmail.com>
2020-05-07 08:00:20 -0400

    Add Nova logo to login pages

--- nova/resources/views/components/layouts/auth-simple.blade.php
+++ nova/resources/views/components/layouts/auth-simple.blade.php
@@ -1,6 +1,7 @@
 <div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 | sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        {{-- <img class="mx-auto h-12 w-auto" src="/img/logos/workflow-mark-on-white.svg" alt="Workflow" /> --}}
+        <x-nova-logo class="mx-auto h-12 w-auto text-blue-500" />
+
         <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
             Sign in to your account
         </h2>
