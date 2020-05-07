af3f4d903f9512cdb215f4582abf96c9cda1206f
David VanScott <david.vanscott@gmail.com>
2020-05-06 23:13:39 -0400

    Move Blade directives

--- nova/foundation/NovaBladeDirectives.php
+++ nova/foundation/NovaBladeDirectives.php
@@ -4,6 +4,11 @@ namespace Nova\Foundation;
 
 class NovaBladeDirectives
 {
+    public static function icon($expression)
+    {
+        return "<?php echo e(icon(${expression})); ?>";
+    }
+
     public static function novaScripts($expression)
     {
         return '{!! \Nova\Foundation\Nova::scripts(' . $expression . ') !!}';
