3200d9ddff15ae118f3ddf44de759d80230c0573
David VanScott <david.vanscott@gmail.com>
2020-05-08 16:23:27 -0400

    JS updates

--- nova/resources/js/plugins.js
+++ nova/resources/js/plugins.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
-import { InertiaApp } from '@inertiajs/inertia-vue';
 import VueTippy from 'vue-tippy';
 import VuePortal from '@linusborg/vue-simple-portal';
 import Toast from '@/Shared/Toasts';
 
 Vue.use(Toast);
-Vue.use(InertiaApp);
 Vue.use(VuePortal, {
     selector: '#portal'
 });
