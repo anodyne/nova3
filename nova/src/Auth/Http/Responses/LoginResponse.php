fb1ae2554a2ff3d45910e52c7aa8f1b12637798c
David VanScott <david.vanscott@gmail.com>
2020-05-13 10:42:29 -0400

    Response updates

--- nova/src/Auth/Http/Responses/LoginResponse.php
+++ nova/src/Auth/Http/Responses/LoginResponse.php
@@ -7,11 +7,4 @@ use Nova\Foundation\Http\Responses\ServerResponse;
 class LoginResponse extends ServerResponse
 {
     public $view = 'auth.login';
-
-    public function views(): array
-    {
-        return [
-            'page' => 'auth.login',
-        ];
-    }
 }
