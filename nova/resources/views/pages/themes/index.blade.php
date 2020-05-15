4dc46cc2f5efb465e2161caa40d5350243073b20
David VanScott <david.vanscott@gmail.com>
2020-05-14 20:46:05 -0400

    Dark mode

--- nova/resources/views/pages/themes/index.blade.php
+++ nova/resources/views/pages/themes/index.blade.php
@@ -12,7 +12,7 @@
     </x-page-header>
 
     @if ($pendingThemes->count() > 0)
-        <div class="bg-gray-200 overflow-hidden rounded-lg">
+        <div class="bg-gray-200 dark:bg-gray-900 overflow-hidden rounded-lg">
             <div class="px-4 py-5 | sm:p-6">
                 <h3 class="text-lg leading-6 font-medium text-gray-900">
                     Themes to be installed
@@ -57,11 +57,11 @@
             </x-slot>
 
             <div class="flex items-center justify-between">
-                <h3 class="text-xl leading-7 font-semibold text-gray-900">
+                <h3 class="text-xl leading-7 font-semibold text-gray-900 dark:text-gray-100">
                     {{ $theme->name }}
                 </h3>
 
-                <dropdown class="flex items-center text-gray-400 hover:text-gray-600 focus:outline-none focus:text-gray-600 transition ease-in-out duration-150" placement="bottom-end">
+                <dropdown class="flex items-center text-gray-400 dark:text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600 transition ease-in-out duration-150" placement="bottom-end">
                     @icon('more', 'h-6 w-6')
 
                     <template #dropdown="{ toggle }">
@@ -88,8 +88,8 @@
                     </template>
                 </dropdown>
             </div>
-            <p class="mt-1 flex items-center text-base leading-6 text-gray-500">
-                @icon('folder', 'flex-shrink-0 mr-2 h-5 w-5 text-gray-400')
+            <p class="mt-1 flex items-center text-base leading-6 text-gray-500 dark:text-gray-400">
+                @icon('folder', 'flex-shrink-0 mr-2 h-5 w-5 text-gray-400 dark:text-gray-500')
                 themes/{{ $theme->location }}
             </p>
         </x-card>
