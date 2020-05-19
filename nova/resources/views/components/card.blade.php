cdca1745d725d5ddd24eebada9f28dc06971bd59
David VanScott <david.vanscott@gmail.com>
2020-05-19 11:28:17 -0400

    Card updates

--- nova/resources/views/components/card.blade.php
+++ nova/resources/views/components/card.blade.php
@@ -1,9 +1,9 @@
 @props(['header', 'footer'])
 
-<div {{ $attributes->merge(['class' => 'flex flex-col rounded-lg shadow-lg overflow-hidden']) }}>
+<div {{ $attributes->merge(['class' => 'flex flex-col rounded-lg shadow-lg overflow-hidden relative bg-white']) }}>
     {{ $header ?? false }}
 
-    <div class="flex-1 bg-white dark:bg-gray-800 flex flex-col justify-between">
+    <div class="flex-1 flex flex-col justify-between">
         <div class="flex-1 p-6">
             <div class="block">
                 {{ $slot }}
