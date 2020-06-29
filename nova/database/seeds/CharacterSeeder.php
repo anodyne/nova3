57ba9f2039cbf91e6babc9d24300163f7c545347
David VanScott <david.vanscott@gmail.com>
2020-06-29 11:59:27 -0400

    Seeder updates

--- nova/database/seeds/CharacterSeeder.php
+++ nova/database/seeds/CharacterSeeder.php
@@ -5,12 +5,66 @@ use Illuminate\Database\Seeder;
 use Nova\Characters\Models\Character;
 use Nova\Characters\Models\States\Active;
 use Nova\Characters\Models\States\Inactive;
+use Nova\Departments\Models\Position;
 
 class CharacterSeeder extends Seeder
 {
     public function run()
     {
         activity()->disableLogging();
+
+        $picard = factory(Character::class)->create([
+            'name' => 'Jean-Luc Picard',
+            'rank_id' => 1,
+        ]);
+        $picard->users()->save(User::find(1));
+        $picard->positions()->save(Position::find(1));
+        $picard->status->transitionTo(Active::class);
+
+        $riker = factory(Character::class)->create([
+            'name' => 'William Riker',
+            'rank_id' => 2,
+        ]);
+        $riker->users()->save(User::find(2));
+        $riker->positions()->save(Position::find(2));
+        $riker->status->transitionTo(Active::class);
+
+        $data = factory(Character::class)->create([
+            'name' => 'Data',
+            'rank_id' => 17,
+        ]);
+        $data->positions()->save(Position::find(5));
+        $data->status->transitionTo(Active::class);
+
+        $laforge = factory(Character::class)->create([
+            'name' => 'Geordi LaForge',
+            'rank_id' => 17,
+        ]);
+        $laforge->positions()->save(Position::find(10));
+        $laforge->status->transitionTo(Active::class);
+
+        $worf = factory(Character::class)->create([
+            'name' => 'Worf',
+            'rank_id' => 18,
+        ]);
+        $worf->positions()->save(Position::find(7));
+        $worf->status->transitionTo(Active::class);
+
+        $crusher = factory(Character::class)->create([
+            'name' => 'Beverly Crusher',
+            'rank_id' => 30,
+        ]);
+        $crusher->positions()->save(Position::find(13));
+        $crusher->status->transitionTo(Active::class);
+
+        $guinan = factory(Character::class)->create([
+            'name' => 'Guinan',
+        ]);
+        $guinan->users()->saveMany([
+            User::find(1),
+            User::find(2),
+        ]);
+        $guinan->status->transitionTo(Active::class);
 
         $users = User::get();
 
@@ -21,9 +75,9 @@ class CharacterSeeder extends Seeder
 
             $decision = mt_rand(1, 3);
 
-            if ($decision === 2) {
-                $character->status->transitionTo(Active::class);
-            }
+            // if ($decision === 2) {
+            //     $character->status->transitionTo(Active::class);
+            // }
 
             if ($decision === 3) {
                 $character->status->transitionTo(Active::class);
@@ -34,9 +88,9 @@ class CharacterSeeder extends Seeder
         factory(Character::class)->times(25)->create()->each(function ($character) {
             $decision = mt_rand(1, 3);
 
-            if ($decision === 2) {
-                $character->status->transitionTo(Active::class);
-            }
+            // if ($decision === 2) {
+            //     $character->status->transitionTo(Active::class);
+            // }
 
             if ($decision === 3) {
                 $character->status->transitionTo(Active::class);
