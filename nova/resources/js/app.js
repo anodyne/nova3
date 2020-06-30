f229149029ab015d73570908e47c628180d29467
David VanScott <david.vanscott@gmail.com>
2020-06-30 17:50:13 -0400

    Add word counting

--- nova/resources/js/app.js
+++ nova/resources/js/app.js
@@ -1,8 +1,12 @@
 import 'alpinejs';
 import axios from 'axios';
+import Numeral from 'numeral';
+import Countable from 'countable';
 import Sortable from 'sortablejs';
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.axios = axios;
+window.Numeral = Numeral;
 window.Sortable = Sortable;
+window.Countable = Countable;
--- nova/resources/views/components/input/rich-text.blade.php
+++ nova/resources/views/components/input/rich-text.blade.php
@@ -1,13 +1,51 @@
 @props([
+    'countWords' => true,
     'initialValue' => '',
     'name',
 ])
 
-<input id="content" name="{{ $name }}" value="{{ $initialValue }}" type="hidden">
-<trix-editor input="content" class="trix-content relative w-full min-h-56 bg-transparent py-2 px-3 rounded-md border border-gray-200 bg-gray-50 shadow-sm transition ease-in-out duration-200 focus:border-primary-300 focus:bg-white focus:shadow-outline-primary"></trix-editor>
+<div x-data="wordCount()" x-init="init()">
+    <input id="content" name="{{ $name }}" value="{{ $initialValue }}" type="hidden">
+
+    <trix-editor
+        x-ref="editor"
+        @if ($countWords) x-on:trix-change="refreshCount($event)" @endif
+        input="content"
+        class="trix-content relative w-full min-h-56 bg-transparent py-2 px-3 rounded-md border border-gray-200 bg-gray-50 shadow-sm transition ease-in-out duration-200 focus:border-primary-300 focus:bg-white focus:shadow-outline-primary">
+    </trix-editor>
+
+    @if ($countWords)
+        <div class="mt-2 ml-2px text-sm text-gray-700">
+            Word count: <span x-text="count"></span>
+        </div>
+    @endif
+</div>
 
 @push('scripts')
     <script src="https://unpkg.com/trix@1.2.3/dist/trix.js"></script>
+    <script>
+        function wordCount()
+        {
+            return {
+                count: 0,
+
+                init () {
+                    this.refreshCount();
+                },
+
+                refreshCount (event) {
+                    if (event) {
+                        window.Countable.count(event.target.innerText, counter => {
+                            this.count = window.Numeral(counter.words).format('0,0');
+                        }, {
+                            stripTags: true
+                        });
+                    }
+
+                }
+            };
+        }
+    </script>
 @endpush
 
 @push('styles')
