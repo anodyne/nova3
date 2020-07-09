791b01932bcc3ecde1e61c06cf9ddb5c198c4f31
David VanScott <david.vanscott@gmail.com>
2020-07-08 22:57:45 -0400

    Updates to handle checked items

--- nova/resources/views/components/input/radio.blade.php
+++ nova/resources/views/components/input/radio.blade.php
@@ -1,6 +1,10 @@
-@props(['label', 'for'])
+@props([
+    'label',
+    'for' => false,
+    'checked' => false,
+])
 
 <label for="{{ $for }}" class="inline-flex items-center">
-    <input type="radio" {{ $attributes->merge(['class' => 'form-radio']) }}>
+    <input type="radio" {{ $attributes->merge(['class' => 'form-radio']) }} @if ($checked) checked @endif>
     <span class="ml-2">{{ $label }}</span>
 </label>
