aa4ae4060e0f3e4153bb1a7cab92ce92d6625ab1
David VanScott <david.vanscott@gmail.com>
2020-09-01 09:55:28 -0400

    Add some tests

---
+++ nova/tests/Feature/Stories/ShowStoryTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature\Stories;
+
+use Tests\TestCase;
+use Nova\Stories\Models\Story;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+/**
+ * @group stories
+ */
+class ShowStoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $story;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->story = create(Story::class);
+    }
+
+    /** @test **/
+    public function authorizedUserCanViewAStory()
+    {
+        $this->signInWithPermission('story.view');
+
+        $response = $this->get(route('stories.show', $this->story));
+        $response->assertSuccessful();
+        $response->assertViewHas('story', $this->story);
+    }
+
+    /** @test **/
+    public function unauthorizedUserCannotViewAStory()
+    {
+        $this->signIn();
+
+        $response = $this->get(route('stories.show', $this->story));
+        $response->assertForbidden();
+    }
+
+    /** @test **/
+    public function unauthenticatedUserCannotViewAStory()
+    {
+        $response = $this->getJson(route('stories.show', $this->story));
+        $response->assertUnauthorized();
+    }
+}
