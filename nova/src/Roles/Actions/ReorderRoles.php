2b77b1feb61c4f4c876f5633b52b20a513e20c5a
David VanScott <david.vanscott@gmail.com>
2020-12-05 16:53:30 -0500

    Fix issue with reordering roles

--- nova/src/Roles/Actions/ReorderRoles.php
+++ nova/src/Roles/Actions/ReorderRoles.php
@@ -6,7 +6,7 @@ use Nova\Roles\Models\Role;
 
 class ReorderRoles
 {
-    public function execute(string $sort): void
+    public function execute($sort): void
     {
         collect(explode(',', $sort))->each(function ($roleId, $index) {
             Role::where('id', $roleId)->update(['sort' => $index]);
