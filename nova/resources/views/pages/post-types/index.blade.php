43021c39c8d8e887d8f25d3920a79d73520969a6
David VanScott <david.vanscott@gmail.com>
2020-12-05 17:47:16 -0500

    Font size updates

--- nova/resources/views/pages/post-types/index.blade.php
+++ nova/resources/views/pages/post-types/index.blade.php
@@ -25,7 +25,7 @@
                         @icon('arrow-sort', 'h-6 w-6 text-purple-600')
                     </div>
                     <div class="ml-3">
-                        <h3 class="text-sm font-medium text-purple-900">
+                        <h3 class="font-medium text-purple-900">
                             Change Sorting Order
                         </h3>
                         <div class="mt-2 text-sm text-purple-800">
