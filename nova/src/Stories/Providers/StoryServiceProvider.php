0efd2cdc201aa2629b725ad7072807255f88b3c4
David VanScott <david.vanscott@gmail.com>
2021-04-28 00:26:43 -0400

    Spotlight commands for stories

--- nova/src/Stories/Providers/StoryServiceProvider.php
+++ nova/src/Stories/Providers/StoryServiceProvider.php
@@ -2,18 +2,21 @@
 
 namespace Nova\Stories\Providers;
 
-use Nova\Stories\Models\Story;
 use Nova\DomainServiceProvider;
-use Nova\Stories\Livewire\StoryStatus;
-use Nova\Stories\Policies\StoryPolicy;
 use Nova\Stories\Livewire\DeleteStories;
 use Nova\Stories\Livewire\StoryHierarchy;
-use Nova\Stories\Responses\ShowStoryResponse;
+use Nova\Stories\Livewire\StoryStatus;
+use Nova\Stories\Models\Story;
+use Nova\Stories\Policies\StoryPolicy;
 use Nova\Stories\Responses\CreateStoryResponse;
 use Nova\Stories\Responses\DeleteStoryResponse;
-use Nova\Stories\Responses\UpdateStoryResponse;
 use Nova\Stories\Responses\ReorderStoriesResponse;
 use Nova\Stories\Responses\ShowAllStoriesResponse;
+use Nova\Stories\Responses\ShowStoryResponse;
+use Nova\Stories\Responses\UpdateStoryResponse;
+use Nova\Stories\Spotlight\CreateStory;
+use Nova\Stories\Spotlight\EditStory;
+use Nova\Stories\Spotlight\ViewStory;
 
 class StoryServiceProvider extends DomainServiceProvider
 {
@@ -35,4 +38,13 @@ class StoryServiceProvider extends DomainServiceProvider
         ShowStoryResponse::class,
         UpdateStoryResponse::class,
     ];
+
+    public function spotlightCommands(): array
+    {
+        return [
+            CreateStory::class,
+            EditStory::class,
+            ViewStory::class,
+        ];
+    }
 }
