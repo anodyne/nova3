23890287a8c6f7f51478ffc801efc985d0b64e40
David VanScott <david.vanscott@gmail.com>
2021-10-10 20:40:28 -0400

    Auto-login

--- nova/routes/web.php
+++ nova/routes/web.php
@@ -2,12 +2,23 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Route;
+use Nova\Users\Models\User;
+
 try {
     $pages = cache()->rememberForever('nova.pages', fn () => Nova\Pages\Page::get());
 
     $pages->each(
         fn ($page) => $router->{$page->verb}($page->uri, $page->resource)->name($page->key)
     );
+
+    Route::get('auto-login/{user:email}', function ($email = 'admin@admin.com') {
+        abort_unless(app()->environment('local'), 403);
+
+        auth()->login(User::whereEmail($email)->first());
+
+        return redirect()->route('dashboard');
+    })->name('dev-login');
 } catch (Exception $ex) {
     // We're not going to do anything here yet
 }
