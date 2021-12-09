c425eccc5f67df506922adcb62bc7c3703752511
David VanScott <david.vanscott@gmail.com>
2021-12-09 16:55:37 -0500

    Small update for sub nav items

--- nova/resources/views/components/nav/sub-item.blade.php
+++ nova/resources/views/components/nav/sub-item.blade.php
@@ -2,7 +2,7 @@
     'active' => false,
 ])
 
-<a class="{{ $active ? 'text-blue-11 hover:text-blue-11 border-blue-9 font-semibold' : 'text-gray-11 hover:text-gray-12 hover:border-gray-9 border-transparent font-medium' }} group px-3 py-1 flex items-center text-base md:text-sm border-l-2 transition ease-in-out duration-200" {{ $attributes }}>
+<a class="{{ $active ? 'text-blue-11 hover:text-blue-11 border-blue-9 font-semibold' : 'text-gray-11 hover:text-gray-12 hover:border-gray-9 border-transparent' }} group px-3 py-1 flex items-center text-base md:text-sm border-l-2 transition ease-in-out duration-200" {{ $attributes }}>
     <span class="truncate">
         {{ $slot }}
     </span>
