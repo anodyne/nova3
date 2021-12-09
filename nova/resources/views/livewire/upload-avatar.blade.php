d8a3036039deafe6fbbe64b2b319ea1c84fa58dd
David VanScott <david.vanscott@gmail.com>
2021-12-09 16:55:25 -0500

    Using new file utilities

--- nova/resources/views/livewire/upload-avatar.blade.php
+++ nova/resources/views/livewire/upload-avatar.blade.php
@@ -1,29 +1,25 @@
 <div class="flex flex-col">
-    <div class="flex items-center space-x-5">
-        <span class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+    <div class="flex items-center space-x-6">
+        <div class="shrink-0">
             @if ($avatar)
-                <img src="{{ $avatar->temporaryUrl() }}" alt="Avatar">
+                <img src="{{ $avatar->temporaryUrl() }}" alt="Avatar" class="h-16 w-16 object-cover rounded-full">
             @else
                 @if ($existingAvatar)
-                    <img src="{{ $existingAvatar }}" alt="Avatar">
+                    <img src="{{ $existingAvatar }}" alt="Avatar" class="h-16 w-16 object-cover rounded-full">
                 @else
-                    <svg class="h-full w-full text-gray-9" fill="currentColor" viewBox="0 0 24 24">
+                    <svg class="h-16 w-16 rounded-full text-gray-9 bg-gray-3 border border-gray-6" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg>
                 @endif
             @endif
-        </span>
-        <span x-data="{ focused: false }">
-            <input @focus="focused = true" @blur="focused = false" type="file" id="avatar" wire:model="avatar" class="sr-only">
-            <label
-                for="avatar"
-                class="cursor-pointer inline-flex items-center text-center justify-center border rounded-md transition ease-in-out duration-200 focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 bg-gray-1 border-gray-7 text-gray-11 hover:bg-gray-2 hover:border-gray-8 focus:ring-2 focus:ring-offset-2 focus:ring-gray-7 px-2.5 py-1.5 text-xs uppercase tracking-wide font-semibold shadow-sm"
-                :class="{ 'border-blue-7 ring': focused }"
-            >
-                Change
-            </label>
-            <input type="hidden" name="avatar_path" wire:model="path">
-        </span>
+        </div>
+
+        <label for="avatar" class="block">
+            <span class="sr-only">Choose profile photo</span>
+            <input type="file" id="avatar" wire:model="avatar" class="block w-full text-sm file:cursor-pointer file:uppercase file:tracking-wide file:shadow-sm text-gray-11 file:mr-4 file:py-1.5 file:px-2.5 file:rounded-md file:text-xs file:font-semibold file:bg-gray-1 file:text-gray-11 file:border file:border-solid file:border-gray-7 hover:file:bg-gray-2 hover:file:border-gray-8 file:transition file:duration-200 focus:outline-none">
+        </label>
+
+        <input type="hidden" name="avatar_path" wire:model="path">
     </div>
 
     @error('avatar')
