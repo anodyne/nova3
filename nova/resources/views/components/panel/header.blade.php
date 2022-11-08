83727e69054b41bbe3ebae4e07752a9501eaf05b
David VanScott <david.vanscott@gmail.com>
2022-11-08 17:22:06 -0500

    Updated panel header

--- nova/resources/views/components/panel/header.blade.php
+++ nova/resources/views/components/panel/header.blade.php
@@ -4,20 +4,24 @@
     'controls' => false,
 ])
 
-<x-content-box height="sm" class="border-b border-gray-200 dark:border-gray-200/10">
-    <div class="flex flex-col md:flex-row md:items-center md:justify-between space-y-4 md:space-y-0 md:space-x-6">
-        <div>
-            <x-h2>{{ $title }}</x-h2>
+<div class="border-b border-gray-200 dark:border-gray-200/10">
+    <x-content-box height="sm">
+        <div class="flex flex-col md:flex-row md:items-center md:justify-between space-y-4 md:space-y-0 md:space-x-6">
+            <div>
+                <x-h2>{{ $title }}</x-h2>
 
-            @if ($message)
-                <p class="mt-0.5 text-gray-500 text-sm leading-5">{{ $message }}</p>
+                @if ($message)
+                    <p class="mt-0.5 text-gray-500 md:text-sm">{{ $message }}</p>
+                @endif
+            </div>
+
+            @if ($controls)
+                <div class="shrink-0 flex items-center space-x-reverse space-x-6 md:space-x-6">
+                    {{ $controls }}
+                </div>
             @endif
         </div>
+    </x-content-box>
 
-        @if ($controls)
-            <div class="shrink-0 flex items-center space-x-reverse space-x-6 md:space-x-6">
-                {{ $controls }}
-            </div>
-        @endif
-    </div>
-</x-content-box>
+    {{ $slot }}
+</div>
