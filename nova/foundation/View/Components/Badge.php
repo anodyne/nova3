10e480738911c33628e1c1ee67cd85ed386aa843
David VanScott <david.vanscott@gmail.com>
2023-05-29 16:59:46 -0400

    Color updates

--- nova/foundation/View/Components/Badge.php
+++ nova/foundation/View/Components/Badge.php
@@ -37,7 +37,7 @@ class Badge extends Component
             default => 'bg-gray-50 dark:bg-gray-400/10 text-gray-600 dark:text-gray-400 ring-gray-500/10 dark:ring-gray-400/20',
             'primary' => 'bg-primary-50 dark:bg-primary-400/10 text-primary-600 dark:text-primary-400 ring-primary-500/10 dark:ring-primary-400/20',
             'success' => 'bg-success-50 dark:bg-success-400/10 text-success-600 dark:text-success-400 ring-success-500/10 dark:ring-success-400/20',
-            'info' => 'bg-info-50 dark:bg-info-400/10 text-info-600 dark:text-info-400 ring-info-500/10 dark:ring-info-400/20',
+            'secondary' => 'bg-secondary-50 dark:bg-secondary-400/10 text-secondary-600 dark:text-secondary-400 ring-secondary-500/10 dark:ring-secondary-400/20',
             'danger' => 'bg-danger-50 dark:bg-danger-400/10 text-danger-600 dark:text-danger-400 ring-danger-500/10 dark:ring-danger-400/20',
             'warning' => 'bg-warning-50 dark:bg-warning-400/10 text-warning-600 dark:text-warning-400 ring-warning-500/10 dark:ring-warning-400/20',
         };
@@ -49,7 +49,7 @@ class Badge extends Component
             default => 'ring-gray-50 dark:ring-gray-900/50',
             'primary' => 'ring-primary-50 dark:ring-primary-900/50',
             'success' => 'ring-success-50 dark:ring-success-900/50',
-            'info' => 'ring-info-50 dark:ring-info-900/50',
+            'secondary' => 'ring-secondary-50 dark:ring-secondary-900/50',
             'danger' => 'ring-danger-50 dark:ring-danger-900/50',
             'warning' => 'ring-warning-50 dark:ring-warning-900/50',
         };
@@ -78,7 +78,7 @@ class Badge extends Component
     //         default => 'text-gray-500',
     //         'primary' => 'text-primary-500',
     //         'success' => 'text-success-500',
-    //         'info' => 'text-info-500',
+    //         'secondary' => 'text-secondary-500',
     //         'danger' => 'text-danger-500',
     //         'warning' => 'text-warning-500',
     //     };
