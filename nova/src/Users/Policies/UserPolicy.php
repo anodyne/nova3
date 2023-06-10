f62eea70d9fc7f8be5b93f532929dc30f8feef5a
David VanScott <david.vanscott@gmail.com>
2023-06-10 09:51:52 -0400

    User policy updates

--- nova/src/Users/Policies/UserPolicy.php
+++ nova/src/Users/Policies/UserPolicy.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Nova\Users\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 use Nova\Users\Models\States\Active;
 use Nova\Users\Models\States\Inactive;
 use Nova\Users\Models\States\Pending;
@@ -14,98 +15,75 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any user.
-     *
-     *
-     * @return mixed
-     */
-    public function viewAny(User $user)
+    public function viewAny(User $user): Response
     {
         return $user->isAbleTo('user.*')
             ? $this->allow()
             : $this->denyAsNotFound();
     }
 
-    /**
-     * Determine whether the user can view the theme.
-     *
-     *
-     * @return mixed
-     */
-    public function view(User $user, User $actionableUser)
+    public function view(User $user, User $actionableUser): Response
     {
         return $user->isAbleTo('user.view')
             ? $this->allow()
             : $this->denyAsNotFound();
     }
 
-    /**
-     * Determine whether the user can create users.
-     *
-     *
-     * @return mixed
-     */
-    public function create(User $user)
+    public function create(User $user): Response
     {
         return $user->isAbleTo('user.create')
             ? $this->allow()
             : $this->denyAsNotFound();
     }
 
-    /**
-     * Determine whether the user can update the theme.
-     *
-     *
-     * @return mixed
-     */
-    public function update(User $user, User $actionableUser)
+    public function updateAny(User $user): Response
     {
         return $user->isAbleTo('user.update')
             ? $this->allow()
             : $this->denyAsNotFound();
     }
 
-    /**
-     * Determine whether the user can delete the theme.
-     *
-     *
-     * @return mixed
-     */
-    public function delete(User $user, User $actionableUser)
+    public function update(User $user, User $actionableUser): Response
+    {
+        return $user->isAbleTo('user.update')
+            ? $this->allow()
+            : $this->denyAsNotFound();
+    }
+
+    public function delete(User $user, User $actionableUser): Response
     {
         return $user->isAbleTo('user.delete') && $user->isNot($actionableUser)
             ? $this->allow()
             : $this->denyAsNotFound();
     }
 
-    public function restore(User $user, User $actionableUser)
+    public function restore(User $user, User $actionableUser): Response
     {
         return $user->isAbleTo('user.create') || $user->isAbleTo('user.delete')
             ? $this->allow()
             : $this->denyAsNotFound();
     }
 
-    public function forceDelete(User $user, User $actionableUser)
+    public function forceDelete(User $user, User $actionableUser): Response
     {
         return $this->delete($user, $actionableUser);
     }
 
-    public function activate(User $user, User $actionableUser)
+    public function activate(User $user, User $actionableUser): Response
     {
         return $this->update($user, $actionableUser) && $actionableUser->status->equals(Inactive::class)
             ? $this->allow()
             : $this->denyAsNotFound();
     }
 
-    public function deactivate(User $user, User $actionableUser)
+    public function deactivate(User $user, User $actionableUser): Response
     {
         return $this->update($user, $actionableUser) && $actionableUser->status->equals(Active::class) && $actionableUser->isNot($user)
             ? $this->allow()
             : $this->denyAsNotFound();
     }
 
-    public function forcePasswordReset(User $user, User $actionableUser)
+    public function forcePasswordReset(User $user, User $actionableUser): Response
     {
         return $this->update($user, $actionableUser) && ! $actionableUser->status->equals(Pending::class)
             ? $this->allow()
