fe219f745cfd2eb383ff64750a049850a3400151
David VanScott <david.vanscott@gmail.com>
2023-06-13 12:43:20 -0400

    Updates for Inter

--- nova/resources/views/app.blade.php
+++ nova/resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="bg-white dark:bg-gray-950 xl:bg-gray-100">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
@@ -7,9 +7,6 @@
         <meta name="theme-color" content="#0091ff" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#0091ff" media="(prefers-color-scheme: dark)" />
         <title>{{ config('app.name', 'Nova NextGen') }}</title>
-
-        <link rel="stylesheet preload prefetch" href="/dist/fonts/Inter-roman.var.woff2" as="style" crossorigin />
-        <link rel="stylesheet preload prefetch" href="/dist/fonts/Inter-italic.var.woff2" as="style" crossorigin />
 
         <x-admin-theme :settings="settings()" />
 
@@ -20,7 +17,7 @@
 
         @stack('headScripts')
     </head>
-    <body class="bg-gray-100 font-sans text-gray-500 antialiased dark:bg-gray-950 dark:text-gray-400">
+    <body class="font-sans text-gray-500 antialiased dark:text-gray-400">
         <div id="nova">
             @yield('layout')
         </div>
