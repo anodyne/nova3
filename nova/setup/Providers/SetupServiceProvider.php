cb9ee693d07080fb50fb7cd757676678eccf0138
David VanScott <david.vanscott@gmail.com>
2023-08-23 13:59:21 -0400

    Set the primary color if Nova isn't installed

--- nova/setup/Providers/SetupServiceProvider.php
+++ nova/setup/Providers/SetupServiceProvider.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 namespace Nova\Setup\Providers;
 
+use Filament\Support\Colors\Color;
+use Filament\Support\Colors\ColorManager;
 use Nova\DomainServiceProvider;
+use Nova\Foundation\Nova;
 use Nova\Setup\Commands\RefreshNovaCommand;
 
 class SetupServiceProvider extends DomainServiceProvider
 {
+    public function domainBooted(): void
+    {
+        if (! Nova::isInstalled()) {
+            app(ColorManager::class)->register(['primary' => Color::Sky]);
+        }
+    }
+
     public function consoleCommands(): array
     {
         return [
