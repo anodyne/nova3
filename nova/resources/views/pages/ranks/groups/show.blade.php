dadc8f48a2f2a7a6f6ea86f76688b5d581b55419
David VanScott <david.vanscott@gmail.com>
2023-10-09 19:59:46 -0400

    Some better empty states

--- nova/resources/views/pages/ranks/groups/show.blade.php
+++ nova/resources/views/pages/ranks/groups/show.blade.php
@@ -31,7 +31,7 @@
             <x-h3>Assigned ranks</x-h3>
 
             <div class="grid grid-cols-1 gap-4 lg:grid-cols-2">
-                @foreach ($group->ranks as $rank)
+                @forelse ($group->ranks as $rank)
                     <x-panel as="light-well" class="group flex w-full items-center justify-between">
                         <x-content-box height="sm" width="sm" class="w-full">
                             <div class="group flex items-center justify-between">
@@ -57,7 +57,18 @@
                             </div>
                         </x-content-box>
                     </x-panel>
-                @endforeach
+                @empty
+                    <div class="col-span-2">
+                        <x-empty-state.small
+                            icon="rank"
+                            title="No ranks assigned"
+                            message="There aren't any ranks assigned to this rank group. Assign some ranks to this rank group to populate this list."
+                            :link-access="gate()->allows('viewAny', Nova\Ranks\Models\RankItem::class)"
+                            :link="route('ranks.items.index')"
+                            label="Assign ranks"
+                        ></x-empty-state.small>
+                    </div>
+                @endforelse
             </div>
         </x-content-box>
     </x-panel>
