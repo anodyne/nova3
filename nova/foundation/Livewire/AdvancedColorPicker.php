3d4eeb14818a4336b92bfcbd87f7d0b5c1fe9ed7
David VanScott <david.vanscott@gmail.com>
2024-04-19 09:58:53 -0400

    Code style updates

--- nova/foundation/Livewire/AdvancedColorPicker.php
+++ nova/foundation/Livewire/AdvancedColorPicker.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Nova\Foundation\Livewire;
 
 use Livewire\Attributes\Computed;
@@ -41,7 +43,7 @@ class AdvancedColorPicker extends Component
             700,
             800,
             900,
-            950
+            950,
         ];
     }
 
