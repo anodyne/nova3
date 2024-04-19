add5da39fcaaf6e87906cdb0369fd5f57779a792
David VanScott <david.vanscott@gmail.com>
2024-04-19 09:33:59 -0400

    Add info message

--- nova/resources/views/components/fieldset/info-message.blade.php
+++ nova/resources/views/components/fieldset/info-message.blade.php
@@ -1,14 +1,14 @@
-<div
-    data-slot="description"
+<p
+    data-slot="info"
     @class([
-        'text-base/6 text-gray-500 data-[disabled]:opacity-50 dark:text-gray-400 sm:text-sm/6',
+        'text-base/6 text-info-600 data-[disabled]:opacity-50 dark:text-info-500 sm:text-sm/6',
         '[&+[data-slot=description]]:mt-4',
         '[&+[data-slot=error]]:mt-4',
         '[&+[data-slot=warning]]:mt-4',
         '[&+[data-slot=info]]:mt-4',
-        $attributes->get('class'),
+        $attributes->get('class') => $attributes->has('class'),
     ])
     {{ $attributes }}
 >
     {{ $slot }}
-</div>
+</p>
