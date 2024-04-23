b38347c23cb54ac43d28516c9d427fbbd540d7d8
David VanScott <david.vanscott@gmail.com>
2024-04-22 23:22:41 -0400

    Support text after a fieldset label

--- nova/resources/views/components/fieldset/field.blade.php
+++ nova/resources/views/components/fieldset/field.blade.php
@@ -11,6 +11,7 @@
         '[&>[data-slot=label]+[data-slot=control]]:mt-3',
         '[&>[data-slot=label]+[data-slot=description]]:mt-1',
         '[&>[data-slot=label]+[data-slot=warning]]:mt-1',
+        '[&>[data-slot=label]+[data-slot=text]]:mt-1',
         '[&>[data-slot=description]+[data-slot=control]]:mt-3',
         '[&>[data-slot=warning]+[data-slot=control]]:mt-3',
         '[&>[data-slot=info]+[data-slot=control]]:mt-3',
