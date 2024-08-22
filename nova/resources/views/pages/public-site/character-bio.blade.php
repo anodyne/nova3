867eba6f4295c6f669161a91016e9d9573a1eacc
David VanScott <david.vanscott@gmail.com>
2024-08-22 13:33:51 -0400

    CSS class updates

--- nova/resources/views/pages/public-site/character-bio.blade.php
+++ nova/resources/views/pages/public-site/character-bio.blade.php
@@ -2,36 +2,37 @@
 
 @section('content')
     <div class="@container nova-advanced-page-content">
-        <div class="flex gap-16">
-            <div class="shrink-0">
+        <div class="nv-character-bio">
+            <div class="avatar-container">
                 <x-avatar :src="$character->avatar_url" size="3xl"></x-avatar>
             </div>
 
-            <div class="flex-1 space-y-8">
-                <div class="flex flex-col gap-1">
-                    <div class="flex items-baseline gap-x-8">
+            <div class="bio-content">
+                <div class="character-details">
+                    <div class="character-name">
                         <x-public::h1>{{ $character->name }}</x-public::h1>
-                        <div>
+
+                        <div class="rank">
                             <x-rank :rank="$character->rank"></x-rank>
                         </div>
                     </div>
 
-                    <div class="flex items-center gap-x-4">
-                        <div class="text-lg/7 font-semibold text-gray-500">
+                    <div class="character-metadata">
+                        <div class="metadata-item">
                             {{ $character->rank->name->name }}
                         </div>
 
-                        <div class="text-lg/7 text-gray-300">/</div>
+                        <div class="divider">/</div>
 
-                        <div class="text-lg/7 font-semibold text-gray-500">
+                        <div class="metadata-item">
                             {{ $character->positions->implode('name', ' & ') }}
                         </div>
                     </div>
                 </div>
 
-                <div class="h-px max-w-lg bg-gradient-to-r from-gray-200 to-transparent"></div>
+                <div class="horizontal-divider"></div>
 
-                <div>
+                <div class="form">
                     <livewire:dynamic-form
                         :form="$form"
                         :submission="$character->characterFormSubmission"
