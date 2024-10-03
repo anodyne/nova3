bab9f828a701657fbd1df9ca1d8e52b56165cfb1
David VanScott <david.vanscott@gmail.com>
2024-10-03 10:54:29 -0400

    Radio and checkbox updates

--- nova/resources/views/components/checkbox/group.blade.php
+++ nova/resources/views/components/checkbox/group.blade.php
@@ -1,15 +1,14 @@
 <div
     data-slot="control"
-    @class([
-        // Basic groups
-        'space-y-3',
+    {{
+        $attributes->class([
+            // Basic groups
+            'space-y-3',
 
-        // With descriptions
-        'has-[[data-slot=description]]:space-y-6 [&_[data-slot=label]]:has-[[data-slot=description]]:font-medium',
-
-        $attributes->get('class'),
-    ])
-    {{ $attributes }}
+            // With descriptions
+            'has-[[data-slot=description]]:space-y-6 [&_[data-slot=label]]:has-[[data-slot=description]]:font-medium',
+        ])
+    }}
 >
     {{ $slot }}
 </div>
--- nova/resources/views/components/radio/index.blade.php
+++ nova/resources/views/components/radio/index.blade.php
@@ -10,6 +10,7 @@
     <input type="radio" class="peer sr-only" {{ $attributes }} />
 
     <label
+        for="{{ $attributes->get('id') }}"
         @class([
             // Basic layout
             'relative isolate flex size-[1.1875rem] shrink-0 items-center justify-center rounded-full sm:size-[1.0625rem]',
@@ -57,7 +58,6 @@
                 default => '[--radio-checked-bg:theme(colors.primary.500)] [--radio-checked-border:theme(colors.primary.600/80%)] [--radio-checked-indicator:theme(colors.white)]',
             },
         ])
-        for="{{ $attributes->get('id') }}"
     >
         <span class="size-[0.375rem] rounded-full bg-[--radio-indicator]"></span>
     </label>
