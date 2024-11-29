49af5359676e9e7d63a0386b9bb74977767c6173
David VanScott <david.vanscott@gmail.com>
2024-11-29 12:38:43 -0500

    Add some helper classes

---
+++ nova/foundation/Helpers/FileHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nova\Foundation\Helpers;
+
+class FileHelper
+{
+    /**
+     * Formats the file size to a human readable size.
+     */
+    public static function formatFileSize(int $bytes): ?string
+    {
+        $units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+        $step = 1024;
+
+        $i = 0;
+
+        while (($bytes / $step) > 0.9) {
+            $bytes = $bytes / $step;
+            $i++;
+        }
+
+        return round($bytes, 2).$units[$i];
+    }
+}
