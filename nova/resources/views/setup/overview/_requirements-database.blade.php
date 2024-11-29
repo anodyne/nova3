07b50de3b9f8c40ae5f108565001afe24b111e85
David VanScott <david.vanscott@gmail.com>
2024-11-29 12:41:39 -0500

    Database requirement updates

--- nova/resources/views/setup/overview/_requirements-database.blade.php
+++ nova/resources/views/setup/overview/_requirements-database.blade.php
@@ -2,15 +2,15 @@
     <div class="flex justify-between gap-6">
         <div class="flex items-center gap-4">
             <div class="shrink-0">
-                <x-icon name="tabler-brand-mysql" class="text-gray-500" size="xl"></x-icon>
+                <x-icon name="tabler-database" class="text-gray-500" size="xl"></x-icon>
             </div>
-            <x-h3 class="flex-1">MySQL 8.0+</x-h3>
+            <x-h3 class="flex-1">MySQL 8.0+ or MariaDB 10.0+</x-h3>
         </div>
     </div>
     <div class="ml-12 mt-2 max-w-lg space-y-4 text-sm/6 font-normal text-gray-500">
         <p>
-            Nova requires a MySQL database to store and retrieve your game’s data. Your server has the ability to
-            connect to a MySQL database.
+            Nova requires a database to store and retrieve your game’s data. Your server has the ability to connect to a
+            MySQL-compatible database.
         </p>
 
         <p>
@@ -24,13 +24,12 @@
             <x-h3 class="text-warning-700">A note about MariaDB</x-h3>
 
             <p class="mt-2 text-sm/6 text-warning-600">
-                At this time, Nova does not support MariaDB. While it’s often considered a drop-in replacement for
-                MySQL, a
+                Nova does support using MariaDB instead of MySQL, however, a
                 {{-- format-ignore-start --}}
                 <a href="https://jira.mariadb.org/browse/MDEV-19077" target="_blank" class="font-medium text-warning-800 underline hover:text-warning-900">known bug</a>
                 {{-- format-ignore-end --}}
-                in MariaDB would prevent some of Nova’s core features from working correctly. When this bug has been
-                adequately addressed, we will update Nova’s database requirements.
+                in MariaDB prevents calculating values from nested resources. Due to this issue, Nova hides certain user
+                interface elements with these problematic calculations if you are using MariaDB.
             </p>
         </div>
     </div>
