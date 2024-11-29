ebabe5f8700fc3e2bc1937f05063a190c4dd9c5f
David VanScott <david.vanscott@gmail.com>
2024-11-29 13:14:10 -0500

    Removing locked properties where not needed

--- nova/src/Characters/Livewire/ActivateCharacterButton.php
+++ nova/src/Characters/Livewire/ActivateCharacterButton.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Nova\Characters\Livewire;
 
-use Livewire\Attributes\Locked;
 use Livewire\Component;
 use Nova\Characters\Actions\ActivateCharacter;
 use Nova\Characters\Events\CharacterActivated;
@@ -12,7 +11,6 @@ use Nova\Characters\Models\Character;
 
 class ActivateCharacterButton extends Component
 {
-    #[Locked]
     public Character $character;
 
     public function activate(): void
--- nova/src/Users/Livewire/DeactivateUserButton.php
+++ nova/src/Users/Livewire/DeactivateUserButton.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Nova\Users\Livewire;
 
-use Livewire\Attributes\Locked;
 use Livewire\Component;
 use Nova\Users\Actions\DeactivateUser;
 use Nova\Users\Events\UserDeactivated;
@@ -12,7 +11,6 @@ use Nova\Users\Models\User;
 
 class DeactivateUserButton extends Component
 {
-    #[Locked]
     public User $user;
 
     public function deactivate(): void
