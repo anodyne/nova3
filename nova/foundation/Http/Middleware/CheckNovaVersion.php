058d4f60a48a1408764407e3dde08000bec7b607
David VanScott <david.vanscott@gmail.com>
2024-11-29 12:38:07 -0500

    Updates for version checking

---
+++ nova/foundation/Http/Middleware/CheckNovaVersion.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nova\Foundation\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Nova\Foundation\Nova;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckNovaVersion
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Nova::isInstalled()) {
+            Cache::flexible('nova-latest-version', [86_400, 129_600], function () {
+                return Http::get(url('api/version'))->json();
+            });
+
+            Cache::flexible('nova-update-available', [86_400, 129_600], function () {
+                $latestVersion = Cache::get('nova-latest-version');
+
+                if (version_compare(nova()->version, $latestVersion['version'], '<')) {
+                    return $latestVersion['severity'];
+                }
+
+                return null;
+            });
+        }
+
+        return $next($request);
+    }
+}
