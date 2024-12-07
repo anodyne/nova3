33c63acd4cb9c547dab4f1e1bda08001e2a4389b
David VanScott <david.vanscott@gmail.com>
2024-12-07 17:10:05 -0500

    Add the system info table and model

---
+++ nova/database/migrations/2024_12_05_185830_create_system_info_table.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('system_info', function (Blueprint $table) {
+            $table->id();
+            $table->string('version');
+            $table->string('anodyne_game_id')->nullable();
+            $table->dateTime('install_date')->nullable();
+            $table->dateTime('last_update')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('system_info');
+    }
+};
