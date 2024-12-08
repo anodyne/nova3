930efaa49eccfb97fc3434cf84e9e53dfa2c6f6e
David VanScott <david.vanscott@gmail.com>
2024-12-07 22:10:02 -0500

    Fixed user notification audit modal

--- nova/resources/views/pages/settings/notification-user-audit.blade.php
+++ nova/resources/views/pages/settings/notification-user-audit.blade.php
@@ -1,13 +1,13 @@
 <x-filament.modal-content icon="preferences" title="User preference audit">
     <div class="flex flex-col">
-        <div class="flex items-center py-1.5 font-medium text-gray-600 dark:text-gray-400">
+        <div class="flex items-center px-3 py-1.5 font-medium text-gray-600 dark:text-gray-400">
             <div class="flex-1">User</div>
             <div class="flex w-20 items-center justify-center">In-app</div>
             <div class="flex w-20 items-center justify-center">Email</div>
         </div>
 
         @foreach ($record->userNotificationPreferences->loadMissing('user') as $preference)
-            <div class="flex items-center py-1.5 odd:bg-gray-950/[.04] dark:odd:bg-white/[.07]">
+            <div class="flex items-center rounded-md px-3 py-1.5 odd:bg-gray-950/[.04] dark:odd:bg-white/[.07]">
                 <div class="flex-1 truncate">{{ $preference->user->name }}</div>
                 <div
                     @class([
@@ -16,9 +16,7 @@
                         'text-danger-500' => ! $preference->database,
                     ])
                 >
-                    <div>
-                        <x-icon :name="$preference->database ? 'check' : 'dismiss'"></x-icon>
-                    </div>
+                    <x-icon :name="$preference->database ? 'check' : 'dismiss'" size="md"></x-icon>
                 </div>
                 <div
                     @class([
@@ -27,9 +25,7 @@
                         'text-danger-500' => ! $preference->mail,
                     ])
                 >
-                    <div>
-                        <x-icon :name="$preference->mail ? 'check' : 'dismiss'"></x-icon>
-                    </div>
+                    <x-icon :name="$preference->mail ? 'check' : 'dismiss'" size="md"></x-icon>
                 </div>
             </div>
         @endforeach
