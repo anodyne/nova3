1930ff01c3ea4314a0a0364f285114c1188dbbfb
David VanScott <david.vanscott@gmail.com>
2025-01-13 23:42:04 -0500

    Fix broken Livewire component

--- nova/resources/views/pages/public-site/livewire/stories-timeline.blade.php
+++ nova/resources/views/pages/public-site/livewire/stories-timeline.blade.php
@@ -1,7 +1,7 @@
-@if ($stories->count() > 0)
-    <div>
+<div>
+    @if ($stories->count() > 0)
         <x-public::stories.timeline :$stories :expanded="true"></x-public::stories.timeline>
-    </div>
-@else
-    <div>No stories</div>
-@endif
+    @else
+        No stories
+    @endif
+</div>
