f7fcf2da06301df50c2c7bc81493bb6a145e3989
David VanScott <david.vanscott@gmail.com>
2025-01-16 13:31:12 -0500

    Cleanup

--- nova/src/Dashboards/Controllers/SystemOverviewController.php
+++ nova/src/Dashboards/Controllers/SystemOverviewController.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Nova\Dashboards\Controllers;
 
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
 use Nova\Dashboards\Responses\SystemOverviewResponse;
 use Nova\Foundation\Controllers\Controller;
 
@@ -21,16 +19,5 @@ class SystemOverviewController extends Controller
     public function __invoke()
     {
         return SystemOverviewResponse::send();
-    }
-
-    protected function checkForLatestVersion()
-    {
-        if (Cache::missing('nova-latest-version')) {
-            $upstream = Http::get('https://anodyne-productions.com/api/nova/latest-version');
-
-            Cache::put('nova-latest-version', $upstream->json(), now()->addDay());
-        }
-
-        ray(Cache::get('nova-latest-version'));
     }
 }
