ce96242939dad515530d33d1bc1a237e2bb1deef
David VanScott <david.vanscott@gmail.com>
2025-01-16 14:39:36 -0500

    Fix some themeing

--- nova/resources/views/layouts/admin.blade.php
+++ nova/resources/views/layouts/admin.blade.php
@@ -18,6 +18,19 @@
         @filamentStyles
         @fluxStyles
         @novaAdminStyles
+        <style>
+            :root {
+                --color-accent: rgb(var(--primary-500));
+                --color-accent-content: rgb(var(--primary-600));
+                --color-accent-foreground: var(--color-white);
+            }
+
+            .dark {
+                --color-accent: rgb(var(--primary-500));
+                --color-accent-content: rgb(var(--primary-400));
+                --color-accent-foreground: var(--color-white);
+            }
+        </style>
         @stack('styles')
 
         {{ NovaView::renderHook('admin::styles.after') }}
