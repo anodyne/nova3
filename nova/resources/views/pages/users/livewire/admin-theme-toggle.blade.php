7b668f8a7d22a11b9c004f31b0d72893cd8473a0
David VanScott <david.vanscott@gmail.com>
2025-01-20 12:15:58 -0500

    No need for the admin theme toggle anymore

--- nova/resources/views/pages/users/livewire/admin-theme-toggle.blade.php
+++
@@ -1,3 +0,0 @@
-<div data-slot="control" x-data="adminThemeToggle($wire.entangle('appearance').live)">
-    <x-switch x-model="appearance" on-value="dark" off-value="light" id="appearance"></x-switch>
-</div>
