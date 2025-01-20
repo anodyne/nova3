f2980b8f29aeefa5de12905f7b0eefba1f2bec8c
David VanScott <david.vanscott@gmail.com>
2025-01-20 12:30:01 -0500

    Activity logging i18n

--- nova/lang/en/activity.php
+++ nova/lang/en/activity.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 return [
 
-    'ran-addon-script' => '**:name** ran the :script script for the add-on.',
+    'addons' => [
+        'ran-script' => '**:name** ran the :script script for the add-on.',
+    ],
+
+    'ranks' => [
+        'group-duplicated' => '**:name** duplicated the rank group to the new group **:rankGroup**.',
+        'name-duplicated' => '**:name** duplicated the rank name to the new name **:rankName**.',
+    ],
 
 ];
