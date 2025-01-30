cc5130d5fb34e6b825fb50a5945693a244d8fca3
David VanScott <david.vanscott@gmail.com>
2025-01-30 15:08:53 -0500

    Move the active users scope

--- nova/src/Users/Models/Scopes/ActiveUsers.php
+++ nova/src/Users/Models/Scopes/ActiveUsers.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Nova\Applications\Models\Scopes;
+namespace Nova\Users\Models\Scopes;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
