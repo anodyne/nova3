ad66b0dab0f5eb83bb3fc515e0a375ee4e15a280
David VanScott <david.vanscott@gmail.com>
2025-02-11 11:57:09 -0500

    Fix seeder logging activity

--- nova/database/seeders/DiscussionSeeder.php
+++ nova/database/seeders/DiscussionSeeder.php
@@ -15,6 +15,8 @@ class DiscussionSeeder extends Seeder
      */
     public function run(): void
     {
+        activity()->disableLogging();
+
         $groupChat = Discussion::factory()->create(['subject' => 'Group message']);
 
         $groupChat->allParticipants()->sync([1, 2, 3]);
@@ -45,5 +47,6 @@ class DiscussionSeeder extends Seeder
                 ]);
         }
 
+        activity()->enableLogging();
     }
 }
