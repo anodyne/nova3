cc1b29ceb9ebc2653ef90b1f0f38f17f490be146
David VanScott <david.vanscott@gmail.com>
2025-02-11 11:13:51 -0500

    Updates for table helpers

--- nova/foundation/Models/Concerns/HasTableHelpers.php
+++ nova/foundation/Models/Concerns/HasTableHelpers.php
@@ -8,30 +8,42 @@ use Illuminate\Support\Facades\DB;
 
 trait HasTableHelpers
 {
-    public static function table(): string
+    public static function table(bool $prefix = false): string
     {
-        return with(
-            DB::getTablePrefix(),
-            fn ($prefix) => $prefix.with(new static)->getTable()
-        );
+        $tableName = with(new static)->getTable();
+
+        if (! $prefix) {
+            return $tableName;
+        }
+
+        return with(DB::getTablePrefix(), fn ($prefix) => $prefix.$tableName);
     }
 
-    public static function column(string $columnName, ?string $tableAlias = null): string
+    public static function column(string $columnName, ?string $tableAlias = null, bool $prefixTable = false): string
     {
-        $tableAlias = $tableAlias ?? static::table();
+        $tableAlias = $tableAlias ?? static::table($prefixTable);
 
         return "{$tableAlias}.{$columnName}";
     }
 
-    public static function columnAs(string $columnName, string $as, ?string $tableAlias = null): string
+    public static function prefixedColumn(string $columnName, ?string $tableAlias = null): string
     {
-        $original = self::column($columnName, $tableAlias);
+        return static::column(
+            columnName: $columnName,
+            tableAlias: $tableAlias,
+            prefixTable: true
+        );
+    }
+
+    public static function columnAs(string $columnName, string $as, ?string $tableAlias = null, bool $prefixTable = false): string
+    {
+        $original = self::column($columnName, $tableAlias, $prefixTable);
 
         return "{$original} as {$as}";
     }
 
-    public static function primaryKey(?string $tableAlias = null): string
+    public static function primaryKey(?string $tableAlias = null, bool $prefixTable = false): string
     {
-        return static::column((new static)->primaryKey, $tableAlias);
+        return static::column((new static)->primaryKey, $tableAlias, $prefixTable);
     }
 }
