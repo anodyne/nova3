e55135392fee8c45f7c780312d4365695ca4a803
David VanScott <david.vanscott@gmail.com>
2025-02-13 13:19:43 -0500

    Setup for the Khitomer seeder

--- nova/database/seeders/KhitomerSeeder.php
+++ nova/database/seeders/KhitomerSeeder.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Nova\Stories\Models\Post;
+use Nova\Stories\Models\PostAuthor;
+use Nova\Stories\Models\Story;
 
 class KhitomerSeeder extends Seeder
 {
     public function run()
     {
         activity()->disableLogging();
+
+        PostAuthor::truncate();
+        Post::truncate();
+        Story::truncate();
 
         $this->call([
             KhitomerStorySeeder::class,
