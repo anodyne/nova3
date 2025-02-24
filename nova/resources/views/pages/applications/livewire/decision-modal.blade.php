55b5008abb654053abc0a5f7b8be997f70878eed
David VanScott <david.vanscott@gmail.com>
2025-02-24 08:40:59 -0500

    Fix missing validation message

--- nova/resources/views/pages/applications/livewire/decision-modal.blade.php
+++ nova/resources/views/pages/applications/livewire/decision-modal.blade.php
@@ -84,8 +84,9 @@
                     description="This is the message that will be emailed to the applicant notifying them of your decision."
                     id="message"
                     name="message"
+                    :error="$errors->first('form.message')"
                 >
-                    <x-input.textarea rows="5" wire:model.live.debounce.500ms="form.message"></x-input.textarea>
+                    <x-input.textarea rows="5" wire:model.blur="form.message"></x-input.textarea>
                 </x-fieldset.field>
             </x-fieldset.field-group>
         </x-fieldset>
