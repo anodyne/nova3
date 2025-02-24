985913432eadc94f78805e3601d3f1b4be16db99
David VanScott <david.vanscott@gmail.com>
2025-02-24 08:41:33 -0500

    Make sure a character created by a non-admin has an application

--- nova/src/Characters/Actions/CreateCharacterManager.php
+++ nova/src/Characters/Actions/CreateCharacterManager.php
@@ -6,6 +6,8 @@ namespace Nova\Characters\Actions;
 
 use Illuminate\Support\Facades\DB;
 use Lorisleiva\Actions\Concerns\AsAction;
+use Nova\Applications\Actions\CreateApplicationManager;
+use Nova\Applications\Data\ApplicationData;
 use Nova\Characters\Data\CharacterPositionsData;
 use Nova\Characters\Models\Character;
 use Nova\Characters\Requests\StoreCharacterRequest;
@@ -61,6 +63,10 @@ class CreateCharacterManager
 
             $this->createFormSubmission($character, $request->input('characterBio', []));
 
+            if ($character->is_pending) {
+                $this->createApplication($character);
+            }
+
             SendPendingCharacterNotification::runUnless(
                 $character->is_active,
                 $character,
@@ -71,6 +77,17 @@ class CreateCharacterManager
 
             return $character->refresh();
         });
+    }
+
+    protected function createApplication(Character $character): void
+    {
+        $data = ApplicationData::from(
+            character_id: $character->id,
+            user_id: $character->activeUsers->first()->id,
+            ip_address: null,
+        );
+
+        CreateApplicationManager::run($data);
     }
 
     protected function createFormSubmission(Character $character, ?array $data = []): void
