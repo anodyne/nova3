423677ed473ee650d33b458d1dcbe2c7d8d4aa62
David VanScott <david.vanscott@gmail.com>
2025-03-17 10:23:23 -0400

    Updated setup language items

---
+++ nova/lang/en/setup.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'migrate' => [
+
+        'characters-cannot-be-disabled' => 'Character migration cannot be disabled.',
+
+        'private-messages-note' => 'Due to Nova 2 not having a way to group related private messages, we don’t recommend migrating this data to Nova 3.',
+
+        'users-cannot-be-disabled' => 'User migration cannot be disabled.',
+
+    ],
+
+];
