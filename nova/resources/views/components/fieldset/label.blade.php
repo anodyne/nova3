45f1c29eedb51ad716937a9b53483a481ea498bb
David VanScott <david.vanscott@gmail.com>
2025-03-20 14:39:42 -0400

    Fix error

--- nova/resources/views/components/fieldset/label.blade.php
+++ nova/resources/views/components/fieldset/label.blade.php
@@ -1,3 +1,7 @@
+@props([
+    'required' => false,
+])
+
 <label
     data-slot="label"
     class="block select-none text-base/6 text-gray-800 data-[disabled]:opacity-50 sm:text-sm/6 dark:text-white"
