8b6aa15bb5e1fe3cb4cefdd22c2102af5a02bf53
David VanScott <david.vanscott@gmail.com>
2025-04-08 20:53:31 -0400

    Tests

---
+++ tests/Feature/Posts/CreatePostTest.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+// A moderated user publishing a post as the only author has their post status set to pending
+// A moderated user publishing a post as one of several authors has the post status set to pending
+// A non-modereated user publishing a post that includes a moderated user has the past status set to pending
