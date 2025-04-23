d082cff87c49ff7ecdd9a3b80f13491d7d602225
David VanScott <david.vanscott@gmail.com>
2025-04-23 12:46:06 -0400

    Updated markup

--- nova/resources/views/public-components/manifest/grid-item.blade.php
+++ nova/resources/views/public-components/manifest/grid-item.blade.php
@@ -7,6 +7,7 @@
 <a class="flex rounded-lg px-4 py-2 hover:bg-gray-50 dark:hover:bg-gray-900" {{ $attributes }}>
     <div
         @class([
+            'nv-manifest-grid-item',
             'inline-flex gap-x-3',
             // 'items-center' => ! in_array('position', $options) && ! in_array('type', $options) && ! in_array('status', $options) && ! in_array('rank-image', $options),
         ])
@@ -17,15 +18,17 @@
             </div>
         @endif
 
-        <div class="flex flex-col gap-1">
-            <div class="flex items-center text-lg/7 font-medium text-gray-900 dark:text-white">
+        <div class="nv-manifest-grid-item-content flex flex-col gap-1">
+            <div
+                class="nv-manifest-grid-item-content-name flex items-center text-lg/7 font-medium text-gray-900 dark:text-white"
+            >
                 @if (filled($character))
                     {{ in_array('rank-name', $options) ? $character->display_name : $character->name }}
                 @else
                     {{ $position->name }}
                 @endif
             </div>
-            <div class="text-sm/6 text-gray-600 dark:text-gray-400">
+            <div class="nv-manifest-grid-item-content-position text-sm/6 text-gray-600 dark:text-gray-400">
                 @if (filled($character))
                     @if (in_array('position', $options))
                         {{ $position?->name ?? $character?->positions->implode('name', ' & ') }}
@@ -36,12 +39,12 @@
             </div>
 
             @if (filled($character) && in_array('rank-image', $options))
-                <div class="mt-1">
+                <div class="nv-manifest-grid-item-content-rank mt-1">
                     <x-rank :rank="$character?->rank"></x-rank>
                 </div>
             @endif
 
-            <div class="mt-1">
+            <div class="nv-manifest-grid-item-content-badges mt-1">
                 @if (filled($character))
                     @if (in_array('type', $options))
                         <x-badge :color="$character->type->getColor()">
