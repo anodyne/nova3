e85d4e4fc01a0f8cf17eb26e44053f8fb9ce9efa
David VanScott <david.vanscott@gmail.com>
2025-08-09 01:01:52 -0400

    Checkbox and radios

--- nova/resources/views/components/radio/group.blade.php
+++ nova/resources/views/components/radio/group.blade.php
@@ -1,17 +1,3 @@
-@props([
-    'error' => null,
-])
-
-<div
-    data-slot="control"
-    {{ $attributes->merge(['class' => 'space-y-3 [&_[data-slot=label]]:font-normal has-[[data-slot=description]]:space-y-6 [&_[data-slot=label]]:has-[[data-slot=description]]:font-medium']) }}
->
+<flux:radio.group {{ $attributes->merge(['data-slot' => 'control']) }}>
     {{ $slot }}
-
-    @if (filled($error))
-        <div data-slot="error" class="flex items-center gap-x-1">
-            <x-icon :name="Icon::AlertCircle" size="sm" class="text-danger-400 dark:text-danger-600"></x-icon>
-            <x-fieldset.error-message>{{ $error }}</x-fieldset.error-message>
-        </div>
-    @endif
-</div>
+</flux:radio.group>
