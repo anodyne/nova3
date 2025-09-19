6b63928b81ddd6cd609305ecabeb317ac95b5c72
David VanScott <david.vanscott@gmail.com>
2025-09-19 14:07:00 -0400

    Illustration updates

--- nova/foundation/Icons/Illustration.php
+++ nova/foundation/Icons/Illustration.php
@@ -7,14 +7,14 @@ namespace Nova\Foundation\Icons;
 enum Illustration: string
 {
     case Addons = 'empty-addons';
-    case Applications = 'empty-applications';
-    case Bans = 'empty-bans';
     case Book = 'empty-book';
     case BubbleDiscuss = 'empty-bubble-discuss';
     case ClipboardList = 'empty-clipboard-list';
     case FlowchartPaper = 'empty-flowchart-paper';
+    case GuiHologram = 'empty-gui-hologram';
     case HandpickResume = 'empty-handpick-resume';
     case Inbox = 'empty-inbox';
+    case InkPenDrawing = 'empty-ink-pen-drawing';
     case Layers = 'empty-layers';
     case LineBarChart = 'empty-line-bar-chart';
     case Megaphone = 'empty-megaphone';
@@ -23,9 +23,13 @@ enum Illustration: string
     case Padlock = 'empty-padlock';
     case PadlockShield = 'empty-padlock-shield';
     case PartyMasks = 'empty-party-masks';
+    case PenAndQuill = 'empty-pen-and-quill';
     case RocketCog = 'empty-rocket-cog';
     case SearchUsers = 'empty-search-users';
+    case SecureMonitor = 'empty-secure-monitor';
+    case SheetForm = 'empty-sheet-form';
     case User = 'empty-user';
     case Users = 'empty-users';
     case Vulcan = 'empty-vulcan';
+    case WebError = 'empty-web-error';
 }
