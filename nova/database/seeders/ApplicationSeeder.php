3dc284420fb6c0d22df2d523ca87e8d45bfc4cf9
David VanScott <david.vanscott@gmail.com>
2025-09-23 14:35:54 -0400

    Tweaks to application seeder

--- nova/database/seeders/ApplicationSeeder.php
+++ nova/database/seeders/ApplicationSeeder.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Nova\Applications\Enums\ApplicationResult;
 use Nova\Applications\Models\Application;
 use Nova\Characters\Models\Character;
 use Nova\Departments\Models\Position;
+use Nova\Discussions\Models\Discussion;
 use Nova\Forms\Actions\CreateFormSubmission;
 use Nova\Forms\Models\Form;
 use Nova\Users\Actions\PopulateAccountPreferences;
@@ -20,18 +22,22 @@ class ApplicationSeeder extends Seeder
     {
         activity()->disableLogging();
 
-        $user = User::factory()->pending()->create([
-            'name' => 'Pending user',
-            'email' => 'pending@pending.com',
-        ]);
+        $this->createAcceptedApplication();
+        $this->createDeniedApplication();
+        $this->createPendingApplication();
+
+        activity()->enableLogging();
+    }
+
+    protected function createAcceptedApplication(): void
+    {
+        $user = User::factory()->pending()->create();
         $user->addRoles(['active', 'writer']);
         PopulateAccountPreferences::run($user);
         PopulateNotificationPreferences::run($user);
         CreateFormSubmission::run(Form::key('userBio')->first(), $user);
 
-        $character = Character::factory()->primary()->pending()->create([
-            'name' => 'Jean-Luc Picard',
-        ]);
+        $character = Character::factory()->primary()->pending()->create();
         $character->users()->save($user);
         $character->positions()->save(Position::query()->inRandomOrder()->first());
         CreateFormSubmission::run(Form::key('characterBio')->first(), $character);
@@ -40,13 +46,90 @@ class ApplicationSeeder extends Seeder
             ->create([
                 'user_id' => $user,
                 'character_id' => $character,
+                'result' => ApplicationResult::Accept,
+                'decision_date' => now(),
+                'decision_message' => fake()->paragraph(),
             ]);
         CreateFormSubmission::run(Form::key('applicationInfo')->first(), $application);
 
-        $application->discussion()->create();
+        $discussion = $application->discussion()->create();
 
         $application->reviews()->attach([1, 2, 3]);
 
-        activity()->enableLogging();
+        $this->createDiscussionMessages($discussion, mt_rand(2, 10));
+    }
+
+    protected function createDeniedApplication(): void
+    {
+        $user = User::factory()->pending()->create();
+        $user->addRoles(['active', 'writer']);
+        PopulateAccountPreferences::run($user);
+        PopulateNotificationPreferences::run($user);
+        CreateFormSubmission::run(Form::key('userBio')->first(), $user);
+
+        $character = Character::factory()->primary()->pending()->create();
+        $character->users()->save($user);
+        $character->positions()->save(Position::query()->inRandomOrder()->first());
+        CreateFormSubmission::run(Form::key('characterBio')->first(), $character);
+
+        $application = Application::factory()
+            ->create([
+                'user_id' => $user,
+                'character_id' => $character,
+                'result' => ApplicationResult::Deny,
+                'decision_date' => now(),
+                'decision_message' => fake()->paragraph(),
+            ]);
+        CreateFormSubmission::run(Form::key('applicationInfo')->first(), $application);
+
+        $discussion = $application->discussion()->create();
+
+        $application->reviews()->attach([1, 2, 3]);
+
+        $this->createDiscussionMessages($discussion, mt_rand(2, 10));
+    }
+
+    protected function createPendingApplication(): void
+    {
+        $user = User::factory()->pending()->create();
+        $user->addRoles(['active', 'writer']);
+        PopulateAccountPreferences::run($user);
+        PopulateNotificationPreferences::run($user);
+        CreateFormSubmission::run(Form::key('userBio')->first(), $user);
+
+        $character = Character::factory()->primary()->pending()->create();
+        $character->users()->save($user);
+        $character->positions()->save(Position::query()->inRandomOrder()->first());
+        CreateFormSubmission::run(Form::key('characterBio')->first(), $character);
+
+        $application = Application::factory()
+            ->create([
+                'user_id' => $user,
+                'character_id' => $character,
+                'result' => ApplicationResult::Pending,
+            ]);
+        CreateFormSubmission::run(Form::key('applicationInfo')->first(), $application);
+
+        $discussion = $application->discussion()->create();
+
+        $application->reviews()->attach([1, 2, 3]);
+
+        $this->createDiscussionMessages($discussion, mt_rand(2, 10));
+    }
+
+    protected function createDiscussionMessages(Discussion $discussion, int $count = 3): void
+    {
+        $messages = [];
+
+        for ($i = 0; $i < $count; $i++) {
+            $messages[] = [
+                'discussion_id' => $discussion->id,
+                'content' => fake()->paragraph(),
+                'user_id' => mt_rand(1, 3),
+                'type' => 'text',
+            ];
+        }
+
+        $discussion->messages()->createMany($messages);
     }
 }
