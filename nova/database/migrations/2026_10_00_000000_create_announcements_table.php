45ceafc9f00647884039f0e4644c1e82bd332857
David VanScott <david.vanscott@gmail.com>
2025-11-01 10:53:26 -0400

    Updates for announcements

--- nova/database/migrations/2026_10_00_000000_create_announcements_table.php
+++ nova/database/migrations/2026_10_00_000000_create_announcements_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
             $table->prefixedId();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->nullable()->constrained();
             $table->string('title');
             $table->string('category')->nullable();
             $table->longText('content');
