dcfbf42f26bcadfe454d1f9f401f91b4f4eb0e9c
David VanScott <david.vanscott@gmail.com>
2026-08-19 00:30:55 -0400

    Rector config updates

--- rector.php
+++ rector.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\StrictArrayParamDimFetchRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -17,6 +19,8 @@ return RectorConfig::configure()
     ->withSkip([
         __DIR__.'/nova/database/migrations',
         __DIR__.'/nova/database/migrations_data',
+        RenameParamToMatchTypeRector::class,
+        StrictArrayParamDimFetchRector::class,
     ])
     ->withPreparedSets(
         deadCode: true,
