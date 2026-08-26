6bb3f6fa662bb43e52092cb122e8d3ed48f0bf8a
David VanScott <david.vanscott@gmail.com>
2026-08-26 10:32:18 -0400

    Media table updates

--- nova/database/migrations/2026_02_03_000000_create_media_table.php
+++ nova/database/migrations/2026_02_03_000000_create_media_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
         Schema::create('media', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuidMorphs('model');
-            $table->uuid('uuid')->nullable()->unique();
+            $table->uuid()->nullable()->unique();
             $table->string('collection_name');
             $table->string('name');
             $table->string('file_name');
@@ -30,10 +30,5 @@ return new class extends Migration
 
             $table->index(['model_id', 'model_type']);
         });
-    }
-
-    public function down(): void
-    {
-        Schema::dropIfExists('media');
     }
 };
