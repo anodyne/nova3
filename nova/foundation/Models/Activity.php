2aa91cc9048e269be2daf97e7339ba3a95eb84c3
David VanScott <david.vanscott@gmail.com>
2026-08-26 09:47:54 -0400

    IDE helper for Activity model

--- nova/foundation/Models/Activity.php
+++ nova/foundation/Models/Activity.php
@@ -6,6 +6,9 @@ namespace Nova\Foundation\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
+/**
+ * @mixin IdeHelperActivity
+ */
 class Activity extends \Spatie\Activitylog\Models\Activity
 {
     use HasUuids;
