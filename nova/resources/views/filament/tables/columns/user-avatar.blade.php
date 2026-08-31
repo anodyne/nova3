3ac3056ab97844ce3e35946683c4dff7b2a95e91
David VanScott <david.vanscott@gmail.com>
2026-08-31 19:48:05 -0400

    Reducing the size of table rows

--- nova/resources/views/filament/tables/columns/user-avatar.blade.php
+++ nova/resources/views/filament/tables/columns/user-avatar.blade.php
@@ -2,6 +2,6 @@
     $user = $getRecord();
 @endphp
 
-<div class="flex items-center px-3 py-3.5">
+<div class="flex items-center px-3 py-2.5">
     <x-avatar.user :$user pronouns></x-avatar.user>
 </div>
